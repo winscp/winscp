64f7ab815dc9063477a845888d44d92204974cf3
Martin Prikryl <martin@winscp.net>
2015-12-02 15:48:48 +0000

    Prevent assertion when starting on 200% Windows 10
    Source commit: 27152b734aaf322491231c85b9b8c9b650f4e9e0

--- source/components/ThemePageControl.cpp
+++ source/components/ThemePageControl.cpp
@@ -77,15 +77,22 @@ int __fastcall TThemePageControl::GetTabsHeight()
 
   // Two different ways to calculate the same, not sure which one is more reliable,
   // so we want to know in case they differ.
-  // On Windows 10 with 200% scaling, the first is 40, the second is 42.
-  // The correct size is probably 41. Will wait for final release before settling on solution.
   if (ALWAYS_TRUE(PageCount >= 0))
   {
     TRect Rect = TabRect(0);
     int Result2 = Rect.Bottom + 1;
-    if (ALWAYS_FALSE(Result != Result2))
+    if (Result != Result2)
     {
-      Result = Result2;
+      // On Windows 10 with 200% scaling, the first is 40, the second is 42.
+      // The correct size is probably 41. Will wait for final release before settling on solution.
+      if (ALWAYS_TRUE(IsWin10() && (Result - 2) == Result2))
+      {
+        Result--;
+      }
+      else
+      {
+        Result = Result2;
+      }
     }
   }
 
