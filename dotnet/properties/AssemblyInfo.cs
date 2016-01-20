a5c6ccc32f75719d56b625d4261f08f0dcd86ca8
Martin Prikryl <martin@winscp.net>
2016-01-20 13:26:09 +0000

    Bumping devel version to 5.8.2
    Source commit: de411617ab0a22cea03a70db7785bba001da9e71

--- dotnet/properties/AssemblyInfo.cs
+++ dotnet/properties/AssemblyInfo.cs
@@ -21,6 +21,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.3.1.0")]
 [assembly: AssemblyFileVersion("1.3.1.0")]
-[assembly: AssemblyInformationalVersionAttribute("5.8.1.0")]
+[assembly: AssemblyInformationalVersionAttribute("5.8.2.0")]
 
 [assembly: CLSCompliant(true)]
