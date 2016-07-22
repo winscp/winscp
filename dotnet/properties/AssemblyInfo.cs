1d35b78bf5bddcb85fc0568262763d9752e84c91
Martin Prikryl <martin@winscp.net>
2016-07-22 13:54:16 +0000

    Bumping master version to 5.9.1
    Source commit: 980be48cd70c6025b46bd90aa95e50fd8ddc8af2

--- dotnet/properties/AssemblyInfo.cs
+++ dotnet/properties/AssemblyInfo.cs
@@ -21,6 +21,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.3.3.0")]
 [assembly: AssemblyFileVersion("1.3.3.0")]
-[assembly: AssemblyInformationalVersionAttribute("5.9.0.0")]
+[assembly: AssemblyInformationalVersionAttribute("5.9.1.0")]
 
 [assembly: CLSCompliant(true)]
