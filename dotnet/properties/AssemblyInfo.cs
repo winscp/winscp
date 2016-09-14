be44a7d0898b07b7574335cd36d875fc98beeccd
Martin Prikryl <martin@winscp.net>
2016-09-14 11:28:35 +0000

    Bumping devel version to 5.9.3
    Source commit: c1b9bf42c14ca1b44bba15e8703882f0bf653264

--- dotnet/properties/AssemblyInfo.cs
+++ dotnet/properties/AssemblyInfo.cs
@@ -21,6 +21,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.3.5.0")]
 [assembly: AssemblyFileVersion("1.3.5.0")]
-[assembly: AssemblyInformationalVersionAttribute("5.9.2.0")]
+[assembly: AssemblyInformationalVersionAttribute("5.9.3.0")]
 
 [assembly: CLSCompliant(true)]
