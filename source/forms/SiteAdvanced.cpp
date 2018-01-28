winscp/winscp

commit fd8263f6ef0b52813015325bcbaa5009bff2dab0

All id_* files are included in private key files filter
Source commit: de9ac6fcb5d33bbf1242c495af1aa1be1e7638d7

source/forms/SiteAdvanced.dfm
@@ -1563,8 +1563,7 @@ object SiteAdvancedDialog: TSiteAdvancedDialog
             OnAfterDialog = PrivateKeyEdit2AfterDialog
             Filter = 
               'PuTTY Private Key Files (*.ppk)|*.ppk|All Private Key Files (*.p' +
-              'pk;*.pem;*.key;id_dsa;id_rsa)|*.ppk;*.pem;*.key;id_dsa;id_rsa|Al' +
-              'l Files (*.*)|*.*'
+              'pk;*.pem;*.key;id_*)|*.ppk;*.pem;*.key;id_*|All Files (*.*)|*.*'
             DialogOptions = [ofReadOnly, ofPathMustExist, ofFileMustExist]
             DialogTitle = 'Select private key file'
             ClickKey = 16397
@@ -2040,8 +2039,7 @@ object SiteAdvancedDialog: TSiteAdvancedDialog
             OnAfterDialog = PrivateKeyEdit2AfterDialog
             Filter = 
               'PuTTY Private Key Files (*.ppk)|*.ppk|All Private Key Files (*.p' +
-              'pk;*.pem;*.key;id_dsa;id_rsa)|*.ppk;*.pem;*.key;id_dsa;id_rsa|Al' +
-              'l Files (*.*)|*.*'
+              'pk;*.pem;*.key;id_*)|*.ppk;*.pem;*.key;id_*|All Files (*.*)|*.*'
             DialogOptions = [ofReadOnly, ofPathMustExist, ofFileMustExist]
             DialogTitle = 'Select private key file'
             ClickKey = 16397

commit 0ac05a2da0be22e58cef3c84d20ebf46086aa337

Forcing re-translation caused by fd8263f6ef (All id_* files are included in private key files filter)
Source commit: 35e71f82dfe1592186265305628080c1f7f05287

source/forms/SiteAdvanced.cpp
@@ -216,7 +216,7 @@ void __fastcall TSiteAdvancedDialog::LoadSession()
     AuthGSSAPICheck3->Checked = FSessionData->AuthGSSAPI;
     GSSAPIFwdTGTCheck->Checked = FSessionData->GSSAPIFwdTGT;
     AgentFwdCheck->Checked = FSessionData->AgentFwd;
-    PrivateKeyEdit2->Text = FSessionData->PublicKeyFile;
+    PrivateKeyEdit3->Text = FSessionData->PublicKeyFile;
 
     // SSH page
     Ssh2LegacyDESCheck->Checked = FSessionData->Ssh2DES;
@@ -372,7 +372,7 @@ void __fastcall TSiteAdvancedDialog::LoadSession()
     TunnelPortNumberEdit->AsInteger = FSessionData->TunnelPortNumber;
     TunnelHostNameEdit->Text = FSessionData->TunnelHostName;
     TunnelPasswordEdit->Text = FSessionData->TunnelPassword;
-    TunnelPrivateKeyEdit2->Text = FSessionData->TunnelPublicKeyFile;
+    TunnelPrivateKeyEdit3->Text = FSessionData->TunnelPublicKeyFile;
     if (FSessionData->TunnelAutoassignLocalPortNumber)
     {
       TunnelLocalPortNumberEdit->Text = TunnelLocalPortNumberEdit->Items->Strings[0];
@@ -437,7 +437,7 @@ void __fastcall TSiteAdvancedDialog::SaveSession()
   FSessionData->AuthGSSAPI = AuthGSSAPICheck3->Checked;
   FSessionData->GSSAPIFwdTGT = GSSAPIFwdTGTCheck->Checked;
   FSessionData->AgentFwd = AgentFwdCheck->Checked;
-  FSessionData->PublicKeyFile = PrivateKeyEdit2->Text;
+  FSessionData->PublicKeyFile = PrivateKeyEdit3->Text;
 
   // Connection page
   FSessionData->FtpPasvMode = FtpPasvModeCheck->Checked;
@@ -615,7 +615,7 @@ void __fastcall TSiteAdvancedDialog::SaveSession()
   FSessionData->TunnelPortNumber = TunnelPortNumberEdit->AsInteger;
   FSessionData->TunnelHostName = TunnelHostNameEdit->Text;
   FSessionData->TunnelPassword = TunnelPasswordEdit->Text;
-  FSessionData->TunnelPublicKeyFile = TunnelPrivateKeyEdit2->Text;
+  FSessionData->TunnelPublicKeyFile = TunnelPrivateKeyEdit3->Text;
   if (TunnelLocalPortNumberEdit->Text == TunnelLocalPortNumberEdit->Items->Strings[0])
   {
     FSessionData->TunnelLocalPortNumber = 0;
@@ -1012,8 +1012,8 @@ void __fastcall TSiteAdvancedDialog::DataChange(TObject * /*Sender*/)
 //---------------------------------------------------------------------------
 void __fastcall TSiteAdvancedDialog::FormShow(TObject * /*Sender*/)
 {
-  InstallPathWordBreakProc(PrivateKeyEdit2);
-  InstallPathWordBreakProc(TunnelPrivateKeyEdit2);
+  InstallPathWordBreakProc(PrivateKeyEdit3);
+  InstallPathWordBreakProc(TunnelPrivateKeyEdit3);
   InstallPathWordBreakProc(RemoteDirectoryEdit);
   InstallPathWordBreakProc(LocalDirectoryEdit);
   InstallPathWordBreakProc(RecycleBinPathEdit);
@@ -1240,7 +1240,7 @@ void __fastcall TSiteAdvancedDialog::HelpButtonClick(TObject * /*Sender*/)
   FormHelp(this);
 }
 //---------------------------------------------------------------------------
-void __fastcall TSiteAdvancedDialog::PrivateKeyEdit2AfterDialog(TObject * Sender,
+void __fastcall TSiteAdvancedDialog::PrivateKeyEdit3AfterDialog(TObject * Sender,
   UnicodeString & Name, bool & Action)
 {
   PathEditAfterDialog(Sender, Name, Action);
@@ -1258,9 +1258,9 @@ void __fastcall TSiteAdvancedDialog::FormCloseQuery(TObject * /*Sender*/,
   if (ModalResult == DefaultResult(this))
   {
     // StripPathQuotes should not be needed as we do not feed quotes anymore
-    VerifyKey(StripPathQuotes(PrivateKeyEdit2->Text), GetSshProt());
+    VerifyKey(StripPathQuotes(PrivateKeyEdit3->Text), GetSshProt());
     // for tunnel SSH version is not configurable
-    VerifyKey(StripPathQuotes(TunnelPrivateKeyEdit2->Text), ssh2only);
+    VerifyKey(StripPathQuotes(TunnelPrivateKeyEdit3->Text), ssh2only);
     VerifyCertificate(StripPathQuotes(TlsCertificateFileEdit->Text));
   }
 }

source/forms/SiteAdvanced.dfm
@@ -1551,16 +1551,16 @@ object SiteAdvancedDialog: TSiteAdvancedDialog
             Width = 75
             Height = 13
             Caption = 'Private &key file:'
-            FocusControl = TunnelPrivateKeyEdit2
+            FocusControl = TunnelPrivateKeyEdit3
           end
-          object TunnelPrivateKeyEdit2: TFilenameEdit
+          object TunnelPrivateKeyEdit3: TFilenameEdit
             Left = 12
             Top = 35
             Width = 370
             Height = 21
             AcceptFiles = True
             OnBeforeDialog = PathEditBeforeDialog
-            OnAfterDialog = PrivateKeyEdit2AfterDialog
+            OnAfterDialog = PrivateKeyEdit3AfterDialog
             Filter = 
               'PuTTY Private Key Files (*.ppk)|*.ppk|All Private Key Files (*.p' +
               'pk;*.pem;*.key;id_*)|*.ppk;*.pem;*.key;id_*|All Files (*.*)|*.*'
@@ -1569,7 +1569,7 @@ object SiteAdvancedDialog: TSiteAdvancedDialog
             ClickKey = 16397
             Anchors = [akLeft, akTop, akRight]
             TabOrder = 0
-            Text = 'TunnelPrivateKeyEdit2'
+            Text = 'TunnelPrivateKeyEdit3'
             OnChange = DataChange
           end
         end
@@ -2017,7 +2017,7 @@ object SiteAdvancedDialog: TSiteAdvancedDialog
             Width = 75
             Height = 13
             Caption = 'Private &key file:'
-            FocusControl = PrivateKeyEdit2
+            FocusControl = PrivateKeyEdit3
           end
           object AgentFwdCheck: TCheckBox
             Left = 12
@@ -2029,14 +2029,14 @@ object SiteAdvancedDialog: TSiteAdvancedDialog
             TabOrder = 0
             OnClick = DataChange
           end
-          object PrivateKeyEdit2: TFilenameEdit
+          object PrivateKeyEdit3: TFilenameEdit
             Left = 12
             Top = 59
             Width = 372
             Height = 21
             AcceptFiles = True
             OnBeforeDialog = PathEditBeforeDialog
-            OnAfterDialog = PrivateKeyEdit2AfterDialog
+            OnAfterDialog = PrivateKeyEdit3AfterDialog
             Filter = 
               'PuTTY Private Key Files (*.ppk)|*.ppk|All Private Key Files (*.p' +
               'pk;*.pem;*.key;id_*)|*.ppk;*.pem;*.key;id_*|All Files (*.*)|*.*'
@@ -2045,7 +2045,7 @@ object SiteAdvancedDialog: TSiteAdvancedDialog
             ClickKey = 16397
             Anchors = [akLeft, akTop, akRight]
             TabOrder = 1
-            Text = 'PrivateKeyEdit2'
+            Text = 'PrivateKeyEdit3'
             OnChange = DataChange
           end
         end

source/forms/SiteAdvanced.h
@@ -152,7 +152,7 @@ __published:
   TEdit *TunnelUserNameEdit;
   TPasswordEdit *TunnelPasswordEdit;
   TUpDownEdit *TunnelPortNumberEdit;
-  TFilenameEdit *TunnelPrivateKeyEdit2;
+  TFilenameEdit *TunnelPrivateKeyEdit3;
   TLabel *Label19;
   TComboBox *ReturnVarEdit;
   TLabel *Label20;
@@ -228,7 +228,7 @@ __published:
   TLabel *FtpAccountLabel;
   TEdit *FtpAccountEdit;
   TLabel *PrivateKeyLabel;
-  TFilenameEdit *PrivateKeyEdit2;
+  TFilenameEdit *PrivateKeyEdit3;
   TGroupBox *TunnelAuthenticationParamsGroup;
   TImageList *ColorImageList;
   TButton *ColorButton;
@@ -270,7 +270,7 @@ __published:
   void __fastcall AuthGSSAPICheck3Click(TObject *Sender);
   void __fastcall KexButtonClick(TObject *Sender);
   void __fastcall HelpButtonClick(TObject *Sender);
-  void __fastcall PrivateKeyEdit2AfterDialog(TObject *Sender,
+  void __fastcall PrivateKeyEdit3AfterDialog(TObject *Sender,
     UnicodeString &Name, bool &Action);
   void __fastcall FormCloseQuery(TObject *Sender, bool &CanClose);
   void __fastcall PathEditBeforeDialog(TObject *Sender, UnicodeString &Name,