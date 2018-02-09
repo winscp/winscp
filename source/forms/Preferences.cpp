3ba21d09f6daf1bb1de1485cdd78170998b169ae
Martin Prikryl <martin@winscp.net>
2018-02-09 18:23:21 +0000

    Typo in comment
    Source commit: af53a2a99cf1a7e77ee615caaf99b9c122b57b64

--- source/forms/Preferences.cpp
+++ source/forms/Preferences.cpp
@@ -2518,7 +2518,7 @@ void __fastcall TPreferencesDialog::AddExtension()
         if (WinSCPURL)
         {
           Url = ProgramUrl(Url);
-          // The EncodeUrlString should not be necessary, but as we get the value from regitry, let's be safe
+          // The EncodeUrlString should not be necessary, but as we get the value from registry, let's be safe
           Url = AppendUrlParams(Url, FORMAT(L"netframework=%s", (EncodeUrlString(GetNetVersionStr()))));
           Url = AppendUrlParams(Url, FORMAT(L"powershell=%s", (EncodeUrlString(GetPowerShellVersionStr()))));
           Url = AppendUrlParams(Url, FORMAT(L"windows=%s", (EncodeUrlString(WindowsVersion()))));
