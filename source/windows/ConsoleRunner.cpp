544949211a7160dfa81939d08efe740108c1fb38
Martin Prikryl <martin@winscp.net>
2018-02-15 08:35:44 +0000

    Bug 1606: Failure when processing FileTransferProgress event (2nd try)
    https://winscp.net/tracker/1606
    
    Source commit: d920bc8191bf873a36e133b6e66b8f2510fbaae3

--- source/windows/ConsoleRunner.cpp
+++ source/windows/ConsoleRunner.cpp
@@ -889,10 +889,11 @@ void __fastcall TExternalConsole::Progress(TScriptProgress & Progress)
   TConsoleCommStruct * CommStruct = GetCommStruct();
 
   typedef TConsoleCommStruct::TProgressEvent TProgressEvent;
-  TProgressEvent & ProgressEvent = CommStruct->ProgressEvent;
 
   try
   {
+    TProgressEvent & ProgressEvent = CommStruct->ProgressEvent;
+
     CommStruct->Event = TConsoleCommStruct::PROGRESS;
 
     switch (Progress.Operation)
@@ -940,7 +941,7 @@ void __fastcall TExternalConsole::Progress(TScriptProgress & Progress)
   CommStruct = GetCommStruct();
   try
   {
-    ProgressEvent = CommStruct->ProgressEvent;
+    TProgressEvent & ProgressEvent = CommStruct->ProgressEvent;
     Progress.Cancel = ProgressEvent.Cancel;
   }
   __finally
