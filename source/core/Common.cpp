winscp/winscp

commit a032aef9eede3b7e8a06f7ad0e8137e857970974

Time constraints can be set to a start of an interval
Source commit: 4d90140e321930199632c63e75f6fc0db70b092e

source/core/Common.cpp
@@ -1719,27 +1719,52 @@ bool __fastcall TryRelativeStrToDateTime(UnicodeString S, TDateTime & DateTime, 
     }
     S.Delete(1, Index - 1);
     S = S.Trim().UpperCase();
+    bool Start = !S.IsEmpty() && EndsStr(L"S", S);
+    if (Start)
+    {
+      S.SetLength(S.Length() - 1);
+    }
     DateTime = Now();
     // These may not overlap with ParseSize (K, M and G)
     if (S == "S")
     {
       DateTime = IncSecond(DateTime, Number);
+      if (Start)
+      {
+        DateTime = IncMilliSecond(DateTime, -static_cast<int>(MilliSecondOfTheSecond(DateTime)));
+      }
     }
     else if (S == "N")
     {
       DateTime = IncMinute(DateTime, Number);
+      if (Start)
+      {
+        DateTime = IncMilliSecond(DateTime, -static_cast<int>(MilliSecondOfTheMinute(DateTime)));
+      }
     }
     else if (S == "H")
     {
       DateTime = IncHour(DateTime, Number);
+      if (Start)
+      {
+        DateTime = IncMilliSecond(DateTime, -static_cast<int>(MilliSecondOfTheHour(DateTime)));
+      }
     }
     else if (S == "D")
     {
       DateTime = IncDay(DateTime, Number);
+      if (Start)
+      {
+        DateTime = IncMilliSecond(DateTime, -static_cast<int>(MilliSecondOfTheDay(DateTime)));
+      }
     }
     else if (S == "Y")
     {
       DateTime = IncYear(DateTime, Number);
+      if (Start)
+      {
+        DateTime = IncMilliSecond(DateTime, -MilliSecondOfTheYear(DateTime));
+      }
     }
     else
     {

commit ea20049836a2325d58a8a8b8d08c4fda88e037f6

"S" time unit was broken by a032aef9ee
Source commit: b4893283ce0e259b4fd9e5e3b562c5b7bd98ee65

source/core/Common.cpp
@@ -1728,7 +1728,7 @@ bool __fastcall TryRelativeStrToDateTime(UnicodeString S, TDateTime & DateTime, 
     }
     S.Delete(1, Index - 1);
     S = S.Trim().UpperCase();
-    bool Start = !S.IsEmpty() && EndsStr(L"S", S);
+    bool Start = (S.Length() == 2) && (S[2] == L'S');
     if (Start)
     {
       S.SetLength(S.Length() - 1);