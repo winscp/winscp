8d261efced4cda7b4c6a8fe547237be5d060382e
Martin Prikryl <martin@winscp.net>
2018-06-18 09:41:54 +0000

    Translating new propagation strings in the next release
    Source commit: 7ae56230b2bb7601b8e262366b517944bb84945b

--- source/windows/Setup.cpp
+++ source/windows/Setup.cpp
@@ -504,7 +504,7 @@ static void __fastcall RegisterProtocolForDefaultPrograms(HKEY RootKey, const Un
     Abort();
   }
 
-  UnicodeString Description = LoadStr(REGISTERED_APP_DESC2);
+  UnicodeString Description = LoadStr(REGISTERED_APP_DESC3);
   Registry->WriteString(L"ApplicationDescription", Description);
 
   if (!Registry->OpenKey(L"UrlAssociations", true))
