winscp/winscp

commit 44cd2388253272018d1fbbbf9c4769b87833deb2

Operation completion message after synchronization running over checklist was shown as an error
Because ESshTerminate was rethrown as EFatal.
Caused by 0de5a4124

Source commit: 3ea09e85fa0d32acdcc3b677d9158193af93a65c

source/core/Exceptions.cpp
@@ -496,9 +496,9 @@ Exception * __fastcall CloneException(Exception * E)
 void __fastcall RethrowException(Exception * E)
 {
   // this list has to be in sync with ExceptionMessage
-  if (dynamic_cast<EFatal *>(E) != NULL)
+  if (dynamic_cast<ExtException *>(E) != NULL)
   {
-    throw EFatal(E, L"");
+    dynamic_cast<ExtException *>(E)->Rethrow();
   }
   else if (dynamic_cast<ECallbackGuardAbort *>(E) != NULL)
   {
@@ -511,10 +511,6 @@ void __fastcall RethrowException(Exception * E)
   else if (WellKnownException(E, NULL, NULL, NULL, true))
   {
     // noop, should never get here
-  }
-  else if (dynamic_cast<ExtException *>(E) != NULL)
-  {
-    dynamic_cast<ExtException *>(E)->Rethrow();
   }
   else
   {

commit 8671e9768e7aeb73786a716efe2c0dd8f8c2a64b

Connections that failed to open were not closed (caused by 44cd238825)
Source commit: f2eb20f42e36e31e7bd6e11b3bf482cf20ccca54

source/core/Exceptions.cpp
@@ -442,6 +442,11 @@ ExtException * __fastcall EFatal::Clone()
   return new EFatal(this, L"");
 }
 //---------------------------------------------------------------------------
+void __fastcall EFatal::Rethrow()
+{
+  throw EFatal(this, L"");
+}
+//---------------------------------------------------------------------------
 ExtException * __fastcall ESshTerminate::Clone()
 {
   return new ESshTerminate(this, L"", Operation, TargetLocalPath, DestLocalFileName);

source/core/Exceptions.h
@@ -159,6 +159,7 @@ public:
   __property bool ReopenQueried = { read = FReopenQueried, write = FReopenQueried };
 
   virtual ExtException * __fastcall Clone();
+  virtual void __fastcall Rethrow();
 
 private:
   bool FReopenQueried;