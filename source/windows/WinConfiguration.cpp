winscp/winscp

commit be8d3acbc1445b89e3db81e710aa0a921162f8f7

In UWP, files can be downloaded directly to an external drop location even without a loaded drag&drop shell extension.
Part of "Make WinSCP available in Windows Store (UWP/AppX package)"
https://winscp.net/tracker/1639
(UWP-only subset of dev branch commit dc85563)

Counters DownloadsDragDropQueue + DownloadsDragDropFakeFile

(cherry picked from commit dc855630a2a68ba029e6ef7845471a49049dfe42)

# Conflicts:
#	source/resource/TextsWin.h
#	source/resource/TextsWin1.rc
#	translations/translate.web.ini

Source commit: 5c7069bac3e08f2f19c227d38eaffe989e73f4f8

source/windows/WinConfiguration.cpp
@@ -493,6 +493,7 @@ void __fastcall TWinConfiguration::Default()
   FDDWarnLackOfTempSpace = true;
   FDDWarnLackOfTempSpaceRatio = 1.1;
   FDDExtEnabled = DDExtInstalled;
+  FDDFakeFile = true;
   FDDExtTimeout = MSecsPerSec;
   FDeleteToRecycleBin = true;
   FSelectDirectories = false;
@@ -915,6 +916,7 @@ THierarchicalStorage * TWinConfiguration::CreateScpStorage(bool & SessionList)
     KEY(Bool,     UseSharedBookmarks); \
     KEY(Integer,  LocaleSafe); \
     KEY(Bool,     DDExtEnabled); \
+    KEY(Bool,     DDFakeFile); \
     KEY(Integer,  DDExtTimeout); \
     KEY(Bool,     DefaultDirIsHome); \
     KEY(Bool,     TemporaryDirectoryAppendSession); \
@@ -1605,6 +1607,23 @@ bool __fastcall TWinConfiguration::GetDDExtInstalled()
   return (FDDExtInstalled > 0);
 }
 //---------------------------------------------------------------------------
+bool __fastcall TWinConfiguration::IsDDExtRunning()
+{
+  bool Result;
+  if (!DDExtInstalled)
+  {
+    Result = false;
+  }
+  else
+  {
+    HANDLE H = OpenMutex(SYNCHRONIZE, False, DRAG_EXT_RUNNING_MUTEX);
+    Result = (H != NULL);
+    CloseHandle(H);
+  }
+
+  return Result;
+}
+//---------------------------------------------------------------------------
 RawByteString __fastcall TWinConfiguration::StronglyRecryptPassword(RawByteString Password, UnicodeString Key)
 {
   RawByteString Dummy;
@@ -1799,9 +1818,28 @@ void __fastcall TWinConfiguration::SetDDTemporaryDirectory(UnicodeString value)
   SET_CONFIG_PROPERTY(DDTemporaryDirectory);
 }
 //---------------------------------------------------------------------------
+bool __fastcall TWinConfiguration::GetDDExtEnabled()
+{
+  if (IsUWP())
+  {
+    return FDDFakeFile;
+  }
+  else
+  {
+    return FDDExtEnabled;
+  }
+}
+//---------------------------------------------------------------------------
 void __fastcall TWinConfiguration::SetDDExtEnabled(bool value)
 {
-  SET_CONFIG_PROPERTY(DDExtEnabled);
+  if (IsUWP())
+  {
+    SET_CONFIG_PROPERTY(DDFakeFile);
+  }
+  else
+  {
+    SET_CONFIG_PROPERTY(DDExtEnabled);
+  }
 }
 //---------------------------------------------------------------------------
 void __fastcall TWinConfiguration::SetDDExtTimeout(int value)

source/Moje.cbproj
@@ -96,7 +96,10 @@
 			<BuildOrder>3</BuildOrder>
 		</DelphiCompile>
 		<DelphiCompile Include="packages\my\CompThread.pas">
-			<BuildOrder>4</BuildOrder>
+			<BuildOrder>22</BuildOrder>
+		</DelphiCompile>
+		<DelphiCompile Include="packages\my\DirectoryMonitor.pas">
+			<BuildOrder>23</BuildOrder>
 		</DelphiCompile>
 		<ResFiles Include="packages\my\DiscMon.dcr">
 			<BuildOrder>6</BuildOrder>

source/dragext/DragExt.cpp
@@ -208,7 +208,7 @@ DllMain(HINSTANCE HInstance, DWORD Reason, LPVOID /*Reserved*/)
 
     if (GRefThisDll == 0)
     {
-      GLogMutex = CreateMutex(NULL, false, L"WinSCPDragExtLogMutex");
+      GLogMutex = CreateMutex(NULL, false, DRAG_EXT_RUNNING_MUTEX);
 
       for (int Root = 0; Root <= 1; Root++)
       {

source/dragext/DragExt.h
@@ -4,6 +4,7 @@
 //---------------------------------------------------------------------------
 #define DRAG_EXT_MAPPING L"WinSCPDragExtMapping"
 #define DRAG_EXT_MUTEX L"WinSCPDragExtMutex"
+#define DRAG_EXT_RUNNING_MUTEX L"WinSCPDragExtLogMutex"
 #define DRAG_EXT_DUMMY_DIR_PREFIX L"scp"
 #define DRAG_EXT_DUMMY_DIR_PREFIX_LEN 3
 //---------------------------------------------------------------------------

source/forms/CustomScpExplorer.cpp
@@ -35,6 +35,7 @@
 #include <Consts.hpp>
 #include <DateUtils.hpp>
 #include <TB2Common.hpp>
+#include <DirectoryMonitor.hpp>
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 #pragma link "CustomDirView"
@@ -6583,7 +6584,7 @@ void __fastcall TCustomScpExplorerForm::RemoteFileControlDDCreateDragFileList(
 
   if (WinConfiguration->DDExtEnabled)
   {
-    if (!WinConfiguration->DDExtInstalled)
+    if (!IsUWP() && !WinConfiguration->DDExtInstalled)
     {
       Configuration->Usage->Inc(L"DownloadsDragDropExternalExtNotInstalled");
       throw ExtException(NULL, LoadStr(DRAGEXT_TARGET_NOT_INSTALLED2), HELP_DRAGEXT_TARGET_NOT_INSTALLED);
@@ -6592,18 +6593,60 @@ void __fastcall TCustomScpExplorerForm::RemoteFileControlDDCreateDragFileList(
   }
 }
 //---------------------------------------------------------------------------
+void __fastcall TCustomScpExplorerForm::DDFakeCreated(TObject * /*Sender*/, const UnicodeString FileName)
+{
+  if (DebugAlwaysTrue(!FDragExtFakeDirectory.IsEmpty()) &&
+      SameText(ExtractFileName(FileName), ExtractFileName(FDragExtFakeDirectory)))
+  {
+    FFakeFileDropTarget = FileName;
+  }
+}
+//---------------------------------------------------------------------------
 void __fastcall TCustomScpExplorerForm::DDExtInitDrag(TFileList * FileList,
   bool & Created)
 {
+  FFakeFileDropTarget = UnicodeString();
   FDragExtFakeDirectory =
     ExcludeTrailingBackslash(WinConfiguration->TemporaryDir());
   if (!ForceDirectories(ApiPath(FDragExtFakeDirectory)))
   {
     throw Exception(FMTLOAD(CREATE_TEMP_DIR_ERROR, (FDragExtFakeDirectory)));
   }
+  if (IsUWP())
+  {
+    FileSetAttr(ApiPath(FDragExtFakeDirectory), faHidden);
+  }
   FileList->AddItem(NULL, FDragExtFakeDirectory);
 
   Created = true;
+
+  if (IsUWP() && !WinConfiguration->IsDDExtRunning())
+  {
+    FDragFakeMonitors = new TObjectList();
+    for (char Drive = FirstDrive; Drive <= LastDrive; Drive++)
+    {
+      std::unique_ptr<TDirectoryMonitor> Monitor(new TDirectoryMonitor(this));
+      DriveInfo->ReadDriveStatus(Drive, dsSize | dsImageIndex);
+      TDriveInfoRec * DriveInfoRec = DriveInfo->Data[Drive];
+      if (DriveInfoRec->Valid &&
+          (DriveInfoRec->DriveType != DRIVE_CDROM))
+      {
+        try
+        {
+          Monitor->Path = UnicodeString(Drive) + L":\\";
+          Monitor->WatchSubtree = true;
+          Monitor->WatchFilters = FILE_NOTIFY_CHANGE_DIR_NAME;
+          Monitor->OnCreated = DDFakeCreated;
+          Monitor->Active = true;
+          FDragFakeMonitors->Add(Monitor.release());
+        }
+        catch (Exception & E)
+        {
+          // Ignore errors watching not-ready drives
+        }
+      }
+    }
+  }
 
   FDDExtMapFile = CreateFileMappingA((HANDLE)0xFFFFFFFF, NULL, PAGE_READWRITE,
     0, sizeof(TDragExtCommStruct), AnsiString(DRAG_EXT_MAPPING).c_str());
@@ -6669,6 +6712,11 @@ void __fastcall TCustomScpExplorerForm::RemoteFileControlDDEnd(TObject * Sender)
   // Drops of local files (uploads) are handled in QueueDDProcessDropped.
   SAFE_DESTROY(FDDFileList);
 
+  if (IsUWP() && !FFakeFileDropTarget.IsEmpty())
+  {
+    RemoveDir(ApiPath(FFakeFileDropTarget));
+  }
+
   if ((FDDExtMapFile != NULL) || (FDDTargetControl == QueueView3))
   {
     try
@@ -6712,9 +6760,9 @@ void __fastcall TCustomScpExplorerForm::RemoteFileControlDDEnd(TObject * Sender)
         }
 
         TTransferOperationParam Param;
-        bool Internal;
+        UnicodeString CounterName;
         bool ForceQueue;
-        if (!DDGetTarget(Param.TargetDirectory, ForceQueue, Internal))
+        if (!DDGetTarget(Param.TargetDirectory, ForceQueue, CounterName))
         {
           // we get drInvalid both if d&d was intercepted by ddext,
           // and when users drops on no-drop location.
@@ -6731,7 +6779,7 @@ void __fastcall TCustomScpExplorerForm::RemoteFileControlDDEnd(TObject * Sender)
         }
         else
         {
-          // download using ddext
+          // download using fake file
           Param.Temp = false;
           Param.DragDrop = true;
           if (ForceQueue)
@@ -6746,8 +6794,7 @@ void __fastcall TCustomScpExplorerForm::RemoteFileControlDDEnd(TObject * Sender)
           if (RemoteFileControlFileOperation(Sender, Operation,
                 (WinConfiguration->DDTransferConfirmation == asOff), &Param))
           {
-            Configuration->Usage->Inc(
-              Internal ? L"DownloadsDragDropInternal" : L"DownloadsDragDropExternalExt");
+            Configuration->Usage->Inc(CounterName);
           }
         }
       }
@@ -6756,8 +6803,14 @@ void __fastcall TCustomScpExplorerForm::RemoteFileControlDDEnd(TObject * Sender)
     {
       CloseHandle(FDDExtMapFile);
       FDDExtMapFile = NULL;
+      if (IsUWP())
+      {
+        delete FDragFakeMonitors;
+        FDragFakeMonitors = NULL;
+      }
       RemoveDir(ApiPath(FDragExtFakeDirectory));
       FDragExtFakeDirectory = L"";
+      FFakeFileDropTarget = UnicodeString();
     }
   }
 
@@ -6789,15 +6842,22 @@ void __fastcall TCustomScpExplorerForm::RemoteFileControlDDGiveFeedback(
 }
 //---------------------------------------------------------------------------
 bool __fastcall TCustomScpExplorerForm::DDGetTarget(
-  UnicodeString & Directory, bool & ForceQueue, bool & Internal)
+  UnicodeString & Directory, bool & ForceQueue, UnicodeString & CounterName)
 {
   bool Result;
   if (FDDTargetControl == QueueView3)
   {
     Directory = DefaultDownloadTargetDirectory();
     Result = true;
-    Internal = true;
+    CounterName = L"DownloadsDragDropQueue";
     ForceQueue = true;
+  }
+  else if (IsUWP() && !FFakeFileDropTarget.IsEmpty())
+  {
+    Directory = ExcludeTrailingBackslash(ExtractFilePath(FFakeFileDropTarget));
+    Result = true;
+    ForceQueue = false;
+    CounterName = L"DownloadsDragDropFakeFile";
   }
   else
   {
@@ -6820,7 +6880,7 @@ bool __fastcall TCustomScpExplorerForm::DDGetTarget(
           if (Result)
           {
             Directory = ExtractFilePath(CommStruct->DropDest);
-            Internal = false;
+            CounterName = L"DownloadsDragDropExternalExt";
           }
           UnmapViewOfFile(CommStruct);
         }

source/forms/CustomScpExplorer.h
@@ -202,6 +202,7 @@ private:
   TStringList * FErrorList;
   HANDLE FDDExtMutex;
   UnicodeString FDragExtFakeDirectory;
+  TObjectList * FDragFakeMonitors;
   TStrings * FDelayedDeletionList;
   TTimer * FDelayedDeletionTimer;
   TStrings * FDDFileList;
@@ -327,6 +328,7 @@ protected:
   TPoint FLastContextPopupScreenPoint;
   bool FRemoteDirViewWasFocused;
   bool FDoNotIdleCurrentTerminal;
+  UnicodeString FFakeFileDropTarget;
 
   virtual bool __fastcall CopyParamDialog(TTransferDirection Direction,
     TTransferType Type, bool Temp, TStrings * FileList,
@@ -419,8 +421,9 @@ protected:
   bool __fastcall ExecuteFileOperation(TFileOperation Operation, TOperationSide Side,
     TStrings * FileList, bool NoConfirmation, void * Param);
   virtual bool __fastcall DDGetTarget(UnicodeString & Directory,
-    bool & ForceQueue, bool & Internal);
+    bool & ForceQueue, UnicodeString & CounterName);
   virtual void __fastcall DDExtInitDrag(TFileList * FileList, bool & Created);
+  void __fastcall DDFakeCreated(TObject * Sender, const UnicodeString FileName);
   virtual void __fastcall SideEnter(TOperationSide Side);
   virtual TOperationSide __fastcall GetSide(TOperationSide Side);
   TStrings * __fastcall PanelExport(TOperationSide Side, TPanelExport Export);

source/forms/Preferences.cpp
@@ -1214,8 +1214,8 @@ void __fastcall TPreferencesDialog::UpdateControls()
     }
     SetLabelHintPopup(CopyParamLabel, InfoStr);
 
-    EnableControl(DDExtEnabledButton, WinConfiguration->DDExtInstalled);
-    EnableControl(DDExtEnabledLabel, WinConfiguration->DDExtInstalled);
+    EnableControl(DDExtEnabledButton, IsUWP() || WinConfiguration->DDExtInstalled);
+    EnableControl(DDExtEnabledLabel, DDExtEnabledButton->Enabled);
     EnableControl(DDExtDisabledPanel, DDExtDisabledButton->Checked);
     EnableControl(DDTemporaryDirectoryEdit, DDCustomTemporaryDirectoryButton->Enabled &&
       DDCustomTemporaryDirectoryButton->Checked);

source/forms/Preferences.dfm
@@ -1343,7 +1343,7 @@ object PreferencesDialog: TPreferencesDialog
             Width = 364
             Height = 17
             Anchors = [akLeft, akTop, akRight]
-            Caption = 'Use &shell extension'
+            Caption = 'Determine drop target by dragging a &fake file'
             TabOrder = 1
             OnClick = ControlChange
           end
@@ -1353,7 +1353,7 @@ object PreferencesDialog: TPreferencesDialog
             Width = 356
             Height = 17
             Anchors = [akLeft, akTop, akRight]
-            Caption = 'Use &temporary folder'
+            Caption = 'Download files via &temporary folder'
             TabOrder = 2
             OnClick = ControlChange
           end

commit 485db6d2346910e57eeb93b8776a10daf24903e9

Bug fix: Orphaned hidden temporary folders were not being deleted
(caused by be8d3acbc1)

Source commit: 2521dffe05e479020a3cbf44a16bdc98f491ac33

source/windows/WinConfiguration.cpp
@@ -2364,7 +2364,7 @@ TStrings * __fastcall TWinConfiguration::FindTemporaryFolders()
     TSearchRecOwned SRec;
     UnicodeString Mask = TemporaryDir(true);
     UnicodeString Directory = ExtractFilePath(Mask);
-    if (FindFirstUnchecked(Mask, faDirectory, SRec) == 0)
+    if (FindFirstUnchecked(Mask, faDirectory | faHidden, SRec) == 0)
     {
       do
       {