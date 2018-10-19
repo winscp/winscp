winscp/winscp

commit c16fd9a54bdbc96b4b05615559ccdf8b458777a8

Function "Keep remote directory up to date" can be started in a new window
Source commit: 2cb0af9c9fd298daecd8df6096cd20536377e51f

source/core/CopyParam.h
@@ -100,7 +100,7 @@ public:
   bool __fastcall SkipTransfer(UnicodeString FileName, bool Directory) const;
 
   void __fastcall Load(THierarchicalStorage * Storage);
-  void __fastcall Save(THierarchicalStorage * Storage) const;
+  void __fastcall Save(THierarchicalStorage * Storage, const TCopyParamType * Defaults = NULL) const;
   UnicodeString __fastcall GetInfoStr(UnicodeString Separator, int Attrs) const;
   bool __fastcall AnyUsableCopyParam(int Attrs) const;
   UnicodeString __fastcall GenerateTransferCommandArgs(

source/windows/GUIConfiguration.cpp
@@ -652,6 +652,26 @@ void __fastcall TGUIConfiguration::UpdateStaticUsage()
     KEY(Integer,  SessionReopenAutoIdle); \
   ); \
 //---------------------------------------------------------------------------
+bool __fastcall TGUIConfiguration::DoSaveCopyParam(THierarchicalStorage * Storage, const TCopyParamType * CopyParam, const TCopyParamType * Defaults)
+{
+  bool Result = Storage->OpenSubKey(L"Interface\\CopyParam", true, true);
+  if (Result)
+  {
+    CopyParam->Save(Storage, Defaults);
+  }
+  return Result;
+}
+//---------------------------------------------------------------------------
+bool __fastcall TGUIConfiguration::SaveCopyParam(THierarchicalStorage * Storage, const TCopyParamType * CopyParam, const TCopyParamType * Defaults)
+{
+  bool Result = DoSaveCopyParam(Storage, CopyParam, Defaults);
+  if (Result)
+  {
+    Storage->CloseSubKey();
+  }
+  return Result;
+}
+//---------------------------------------------------------------------------
 void __fastcall TGUIConfiguration::SaveData(THierarchicalStorage * Storage, bool All)
 {
   TConfiguration::SaveData(Storage, All);
@@ -661,7 +681,7 @@ void __fastcall TGUIConfiguration::SaveData(THierarchicalStorage * Storage, bool
   REGCONFIG(true);
   #undef KEYEX
 
-  if (Storage->OpenSubKey(L"Interface\\CopyParam", true, true))
+  if (DoSaveCopyParam(Storage, &FDefaultCopyParam, NULL))
   try
   {
     FDefaultCopyParam.Save(Storage);
@@ -693,6 +713,25 @@ void __fastcall TGUIConfiguration::SaveData(THierarchicalStorage * Storage, bool
   }
 }
 //---------------------------------------------------------------------------
+bool __fastcall TGUIConfiguration::LoadCopyParam(THierarchicalStorage * Storage, TCopyParamType * CopyParam)
+{
+  bool Result =
+    Storage->OpenSubKey(L"Interface\\CopyParam", false, true);
+  if (Result)
+  {
+    try
+    {
+      CopyParam->Load(Storage);
+    }
+    catch (...)
+    {
+      Storage->CloseSubKey();
+      throw;
+    }
+  }
+  return Result;
+}
+//---------------------------------------------------------------------------
 void __fastcall TGUIConfiguration::LoadData(THierarchicalStorage * Storage)
 {
   TConfiguration::LoadData(Storage);
@@ -704,12 +743,10 @@ void __fastcall TGUIConfiguration::LoadData(THierarchicalStorage * Storage)
   #pragma warn +eas
   #undef KEYEX
 
-  if (Storage->OpenSubKey(L"Interface\\CopyParam", false, true))
+  // FDefaultCopyParam must be loaded before eventual setting defaults for CopyParamList
+  if (LoadCopyParam(Storage, &FDefaultCopyParam))
   try
   {
-    // must be loaded before eventual setting defaults for CopyParamList
-    FDefaultCopyParam.Load(Storage);
-
     int CopyParamListCount = Storage->ReadInteger(L"CopyParamList", -1);
     FCopyParamListDefaults = (CopyParamListCount < 0);
     if (!FCopyParamListDefaults)

source/windows/GUIConfiguration.h
@@ -231,12 +231,15 @@ protected:
   virtual int __fastcall GetResourceModuleCompleteness(HINSTANCE Module);
   virtual bool __fastcall IsTranslationComplete(HINSTANCE Module);
   static int __fastcall LocalesCompare(void * Item1, void * Item2);
+  bool __fastcall DoSaveCopyParam(THierarchicalStorage * Storage, const TCopyParamType * CopyParam, const TCopyParamType * Defaults);
 
 public:
   __fastcall TGUIConfiguration();
   virtual __fastcall ~TGUIConfiguration();
   virtual void __fastcall Default();
   virtual void __fastcall UpdateStaticUsage();
+  bool __fastcall SaveCopyParam(THierarchicalStorage * Storage, const TCopyParamType * CopyParam, const TCopyParamType * Defaults);
+  bool __fastcall LoadCopyParam(THierarchicalStorage * Storage, TCopyParamType * CopyParam);
 
   HANDLE __fastcall ChangeToDefaultResourceModule();
   HANDLE __fastcall ChangeResourceModule(HANDLE Instance);

source/core/Common.cpp
@@ -781,6 +781,24 @@ UnicodeString __fastcall EscapePuttyCommandParam(UnicodeString Param)
   return Param;
 }
 //---------------------------------------------------------------------------
+UnicodeString __fastcall StringsToParams(TStrings * Strings)
+{
+  UnicodeString Result;
+
+  for (int Index = 0; Index < Strings->Count; Index++)
+  {
+    UnicodeString Name = Strings->Names[Index];
+    UnicodeString Value = Strings->ValueFromIndex[Index];
+    // Do not quote if it is all-numeric
+    if (IntToStr(StrToIntDef(Value, -1)) != Value)
+    {
+      Value = FORMAT(L"\"%s\"", (EscapeParam(Value)));
+    }
+    Result += FORMAT(L" %s=%s", (Name, Value));
+  }
+  return Result;
+}
+//---------------------------------------------------------------------------
 UnicodeString __fastcall ExpandEnvironmentVariables(const UnicodeString & Str)
 {
   UnicodeString Buf;

source/core/Common.h
@@ -84,6 +84,7 @@ UnicodeString __fastcall ExpandFileNameCommand(const UnicodeString Command,
 void __fastcall ReformatFileNameCommand(UnicodeString & Command);
 UnicodeString __fastcall EscapeParam(const UnicodeString & Param);
 UnicodeString __fastcall EscapePuttyCommandParam(UnicodeString Param);
+UnicodeString __fastcall StringsToParams(TStrings * Strings);
 UnicodeString __fastcall ExpandEnvironmentVariables(const UnicodeString & Str);
 bool __fastcall SamePaths(const UnicodeString & Path1, const UnicodeString & Path2);
 bool __fastcall IsPathToSameFile(const UnicodeString & Path1, const UnicodeString & Path2);

source/core/CopyParam.cpp
@@ -822,33 +822,47 @@ void __fastcall TCopyParamType::Load(THierarchicalStorage * Storage)
   NewerOnly = Storage->ReadBool(L"NewerOnly", NewerOnly);
 }
 //---------------------------------------------------------------------------
-void __fastcall TCopyParamType::Save(THierarchicalStorage * Storage) const
+void __fastcall TCopyParamType::Save(THierarchicalStorage * Storage, const TCopyParamType * Defaults) const
 {
-  Storage->WriteBool(L"AddXToDirectories", AddXToDirectories);
-  Storage->WriteString(L"Masks", AsciiFileMask.Masks);
-  Storage->WriteInteger(L"FileNameCase", FileNameCase);
-  Storage->WriteBool(L"PreserveReadOnly", PreserveReadOnly);
-  Storage->WriteBool(L"PreserveTime", PreserveTime);
-  Storage->WriteBool(L"PreserveTimeDirs", PreserveTimeDirs);
-  Storage->WriteBool(L"PreserveRights", PreserveRights);
-  Storage->WriteBool(L"IgnorePermErrors", IgnorePermErrors);
-  Storage->WriteString(L"Text", Rights.Text);
-  Storage->WriteInteger(L"TransferMode", TransferMode);
-  Storage->WriteInteger(L"ResumeSupport", ResumeSupport);
-  Storage->WriteInt64(L"ResumeThreshold", ResumeThreshold);
-  Storage->WriteInteger(L"ReplaceInvalidChars", (unsigned int)InvalidCharsReplacement);
-  Storage->WriteString(L"LocalInvalidChars", LocalInvalidChars);
-  Storage->WriteBool(L"CalculateSize", CalculateSize);
-  Storage->WriteString(L"IncludeFileMask", IncludeFileMask.Masks);
+  // Same as in TSessionData::DoSave
+  #define WRITE_DATA_EX(TYPE, NAME, PROPERTY, CONV) \
+    if ((Defaults != NULL) && (CONV(Defaults->PROPERTY) == CONV(PROPERTY))) \
+    { \
+      Storage->DeleteValue(NAME); \
+    } \
+    else \
+    { \
+      Storage->Write ## TYPE(NAME, CONV(PROPERTY)); \
+    }
+  #define WRITE_DATA_CONV(TYPE, NAME, PROPERTY) WRITE_DATA_EX(TYPE, NAME, PROPERTY, WRITE_DATA_CONV_FUNC)
+  #define WRITE_DATA(TYPE, PROPERTY) WRITE_DATA_EX(TYPE, TEXT(#PROPERTY), PROPERTY, )
+
+  WRITE_DATA(Bool, AddXToDirectories);
+  WRITE_DATA_EX(String, L"Masks", AsciiFileMask.Masks, );
+  WRITE_DATA(Integer, FileNameCase);
+  WRITE_DATA(Bool, PreserveReadOnly);
+  WRITE_DATA(Bool, PreserveTime);
+  WRITE_DATA(Bool, PreserveTimeDirs);
+  WRITE_DATA(Bool, PreserveRights);
+  WRITE_DATA(Bool, IgnorePermErrors);
+  WRITE_DATA_EX(String, L"Text", Rights.Text, );
+  WRITE_DATA(Integer, TransferMode);
+  WRITE_DATA(Integer, ResumeSupport);
+  WRITE_DATA(Int64, ResumeThreshold);
+  #define WRITE_DATA_CONV_FUNC(X) (unsigned int)(X)
+  WRITE_DATA_CONV(Integer, L"ReplaceInvalidChars", InvalidCharsReplacement);
+  WRITE_DATA(String, LocalInvalidChars);
+  WRITE_DATA(Bool, CalculateSize);
+  WRITE_DATA_EX(String, L"IncludeFileMask", IncludeFileMask.Masks, );
   Storage->DeleteValue(L"ExcludeFileMask"); // obsolete
   Storage->DeleteValue(L"NegativeExclude"); // obsolete
   DebugAssert(FTransferSkipList.get() == NULL);
   DebugAssert(FTransferResumeFile.IsEmpty());
-  Storage->WriteBool(L"ClearArchive", ClearArchive);
-  Storage->WriteBool(L"RemoveCtrlZ", RemoveCtrlZ);
-  Storage->WriteBool(L"RemoveBOM", RemoveBOM);
-  Storage->WriteInteger(L"CPSLimit", CPSLimit);
-  Storage->WriteBool(L"NewerOnly", NewerOnly);
+  WRITE_DATA(Bool, ClearArchive);
+  WRITE_DATA(Bool, RemoveCtrlZ);
+  WRITE_DATA(Bool, RemoveBOM);
+  WRITE_DATA(Integer, CPSLimit);
+  WRITE_DATA(Bool, NewerOnly);
 }
 //---------------------------------------------------------------------------
 #define C(Property) (Property == rhp.Property)

source/core/HierarchicalStorage.cpp
@@ -1334,6 +1334,8 @@ private:
 
   bool __fastcall AllowWrite();
   void __fastcall NotImplemented();
+  bool __fastcall AllowSection(const UnicodeString & Section);
+  UnicodeString __fastcall FormatKey(const UnicodeString & Section, const UnicodeString & Ident);
 };
 //---------------------------------------------------------------------------
 __fastcall TOptionsIniFile::TOptionsIniFile(TStrings * Options, TWriteMode WriteMode, const UnicodeString & RootKey) :
@@ -1373,22 +1375,42 @@ bool __fastcall TOptionsIniFile::AllowWrite()
   }
 }
 //---------------------------------------------------------------------------
-UnicodeString __fastcall TOptionsIniFile::ReadString(const UnicodeString Section, const UnicodeString Ident, const UnicodeString Default)
+bool __fastcall TOptionsIniFile::AllowSection(const UnicodeString & Section)
 {
   UnicodeString Name = Section;
   if (!Name.IsEmpty())
   {
     Name += PathDelim;
   }
+  bool Result = SameText(Name.SubString(1, FRootKey.Length()), FRootKey);
+  return Result;
+}
+//---------------------------------------------------------------------------
+UnicodeString __fastcall TOptionsIniFile::FormatKey(const UnicodeString & Section, const UnicodeString & Ident)
+{
+  UnicodeString Result = Section;
+  if (!Result.IsEmpty())
+  {
+    Result += PathDelim;
+  }
+  Result += Ident; // Can be empty, when called from a contructor, AllowSection or ReadSection
+  if (DebugAlwaysTrue(AllowSection(Section)))
+  {
+    Result.Delete(1, FRootKey.Length());
+  }
+  return Result;
+}
+//---------------------------------------------------------------------------
+UnicodeString __fastcall TOptionsIniFile::ReadString(const UnicodeString Section, const UnicodeString Ident, const UnicodeString Default)
+{
   UnicodeString Value;
-  if (!SameText(Name.SubString(1, FRootKey.Length()), FRootKey))
+  if (!AllowSection(Section))
   {
     Value = Default;
   }
   else
   {
-    Name.Delete(1, FRootKey.Length());
-    Name += Ident;
+    UnicodeString Name = FormatKey(Section, Ident);
 
     int Index = FOptions->IndexOfName(Name);
     if (Index >= 0)
@@ -1406,25 +1428,19 @@ UnicodeString __fastcall TOptionsIniFile::ReadString(const UnicodeString Section
 void __fastcall TOptionsIniFile::WriteString(const UnicodeString Section, const UnicodeString Ident, const UnicodeString Value)
 {
   if (AllowWrite() &&
-      // Implemented for TSessionData.DoSave only
-      DebugAlwaysTrue(Section.IsEmpty() && FRootKey.IsEmpty()))
+      DebugAlwaysTrue(AllowSection(Section)))
   {
-    FOptions->Values[Ident] = Value;
+    UnicodeString Name = FormatKey(Section, Ident);
+    FOptions->Values[Name] = Value;
   }
 }
 //---------------------------------------------------------------------------
 void __fastcall TOptionsIniFile::ReadSection(const UnicodeString Section, TStrings * Strings)
 {
 
-  UnicodeString SectionPrefix = Section;
-  if (!SectionPrefix.IsEmpty())
+  if (AllowSection(Section))
   {
-    SectionPrefix += PathDelim;
-  }
-
-  if (SameText(SectionPrefix.SubString(1, FRootKey.Length()), FRootKey))
-  {
-    SectionPrefix.Delete(1, FRootKey.Length());
+    UnicodeString SectionPrefix = FormatKey(Section, UnicodeString());
 
     Strings->BeginUpdate();
     try
@@ -1486,10 +1502,11 @@ void __fastcall TOptionsIniFile::EraseSection(const UnicodeString Section)
 void __fastcall TOptionsIniFile::DeleteKey(const UnicodeString Section, const UnicodeString Ident)
 {
   if (AllowWrite() &&
-      // Implemented for TSessionData.DoSave only
-      DebugAlwaysTrue(Section.IsEmpty() && FRootKey.IsEmpty()))
+      DebugAlwaysTrue(AllowSection(Section)))
   {
-    int Index = FOptions->IndexOfName(Ident);
+    UnicodeString Name = FormatKey(Section, Ident);
+
+    int Index = FOptions->IndexOfName(Name);
     if (Index >= 0)
     {
       FOptions->Delete(Index);

source/core/SessionData.cpp
@@ -881,6 +881,7 @@ void __fastcall TSessionData::Load(THierarchicalStorage * Storage, bool PuttyImp
 void __fastcall TSessionData::DoSave(THierarchicalStorage * Storage,
   bool PuttyExport, const TSessionData * Default, bool DoNotEncryptPasswords)
 {
+  // Same as in TCopyParamType::Save
   #define WRITE_DATA_EX(TYPE, NAME, PROPERTY, CONV) \
     if ((Default != NULL) && (CONV(Default->PROPERTY) == CONV(PROPERTY))) \
     { \
@@ -3042,17 +3043,7 @@ UnicodeString __fastcall TSessionData::GenerateOpenCommandArgs(bool Rtf)
   {
     AddSwitch(Result, RawSettingsOption, Rtf);
 
-    for (int Index = 0; Index < RawSettings->Count; Index++)
-    {
-      UnicodeString Name = RawSettings->Names[Index];
-      UnicodeString Value = RawSettings->ValueFromIndex[Index];
-      // Do not quote if it is all-numeric
-      if (IntToStr(StrToIntDef(Value, -1)) != Value)
-      {
-        Value = FORMAT(L"\"%s\"", (EscapeParam(Value)));
-      }
-      Result += FORMAT(L" %s=%s", (Name, Value));
-    }
+    Result += StringsToParams(RawSettings.get());
   }
 
   return Result;

commit 3cf2bbe8b50b5ad18533a275ac0c187fc3f657c7

Bug 1690: Background transfer settings are not preserved
https://winscp.net/tracker/1690

Caused by c16fd9a54bd

Source commit: 230a0f6674b775396272bc9701eaf620472698ca

source/core/CopyParam.h
@@ -103,8 +103,8 @@ public:
     bool Directory, const TFileMasks::TParams & Params, bool Hidden) const;
   bool __fastcall SkipTransfer(UnicodeString FileName, bool Directory) const;
 
-  void __fastcall Load(THierarchicalStorage * Storage);
-  void __fastcall Save(THierarchicalStorage * Storage, const TCopyParamType * Defaults = NULL) const;
+  virtual void __fastcall Load(THierarchicalStorage * Storage);
+  virtual void __fastcall Save(THierarchicalStorage * Storage, const TCopyParamType * Defaults = NULL) const;
   UnicodeString __fastcall GetInfoStr(UnicodeString Separator, int Attrs) const;
   bool __fastcall AnyUsableCopyParam(int Attrs) const;
   UnicodeString __fastcall GenerateTransferCommandArgs(int Attrs, const UnicodeString & Link) const;

source/windows/GUIConfiguration.cpp
@@ -86,9 +86,10 @@ void __fastcall TGUICopyParamType::Load(THierarchicalStorage * Storage)
   QueueParallel = Storage->ReadBool(L"QueueParallel", QueueParallel);
 }
 //---------------------------------------------------------------------------
-void __fastcall TGUICopyParamType::Save(THierarchicalStorage * Storage)
+void __fastcall TGUICopyParamType::Save(THierarchicalStorage * Storage, const TCopyParamType * Defaults) const
 {
-  TCopyParamType::Save(Storage);
+  DebugAssert(Defaults == NULL);
+  TCopyParamType::Save(Storage, Defaults);
 
   Storage->WriteBool(L"Queue", Queue);
   Storage->WriteBool(L"QueueNoConfirmation", QueueNoConfirmation);

source/windows/GUIConfiguration.h
@@ -28,8 +28,8 @@ public:
   __fastcall TGUICopyParamType(const TCopyParamType & Source);
   __fastcall TGUICopyParamType(const TGUICopyParamType & Source);
 
-  void __fastcall Load(THierarchicalStorage * Storage);
-  void __fastcall Save(THierarchicalStorage * Storage);
+  virtual void __fastcall Load(THierarchicalStorage * Storage);
+  virtual void __fastcall Save(THierarchicalStorage * Storage, const TCopyParamType * Defaults = NULL) const;
 
   virtual void __fastcall Default();
   virtual void __fastcall Assign(const TCopyParamType * Source);