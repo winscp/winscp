7902bb2053e52b823f0aa3ee0086ed9862d3b6fb
Martin Prikryl <martin@winscp.net>
2018-12-03 09:55:20 +0000

    libs3 Commit 23a95ae8 (2018-11-29) - license changes only
    Source commit: 2f46009faae2dc456a8a385aaa51b40b0eb83528

--- libs/libs3/src/simplexml.c
+++ libs/libs3/src/simplexml.c
@@ -2,12 +2,14 @@
  * simplexml.c
  * 
  * Copyright 2008 Bryan Ischo <bryan@ischo.com>
- * 
+ *
  * This file is part of libs3.
- * 
+ *
  * libs3 is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, version 3 of the License.
+ * Software Foundation, version 3 or above of the License.  You can also
+ * redistribute and/or modify it under the terms of the GNU General Public
+ * License, version 2 or above of the License.
  *
  * In addition, as a special exception, the copyright holders give
  * permission to link the code of this library and its programs with the
@@ -20,6 +22,10 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * version 3 along with libs3, in a file named COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * You should also have received a copy of the GNU General Public License
+ * version 2 along with libs3, in a file named COPYING-GPLv2.  If not, see
  * <http://www.gnu.org/licenses/>.
  *
  ************************************************************************** **/
