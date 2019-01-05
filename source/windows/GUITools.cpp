winscp/winscp

commit 4031057b675b411166a136ce666a51afe7f58cf5

Control labels do not show keyboard accelerator cue, until Alt key is pressed
(including mechanism to inject a different control implementation + as I needed to add an header include to most form header files, I reordered include sections along a bit)

Source commit: aa2a1b6f5daf70865c0c87c0733aad1be4825b83

source/windows/GUITools.cpp
@@ -2068,3 +2068,31 @@ void __fastcall TNewRichEdit::DestroyWnd()
     FreeLibrary(FLibrary);
   }
 }
+//---------------------------------------------------------------------------
+static int HideAccelFlag(TControl * Control)
+{
+  //ask the top level window about its UI state
+  while (Control->Parent != NULL)
+  {
+    Control = Control->Parent;
+  }
+  int Result;
+  if (FLAGSET(Control->Perform(WM_QUERYUISTATE, 0, 0), UISF_HIDEACCEL))
+  {
+    Result = DT_HIDEPREFIX;
+  }
+  else
+  {
+    Result = 0;
+  }
+  return Result;
+}
+//---------------------------------------------------------------------------
+void __fastcall TUIStateAwareLabel::DoDrawText(TRect & Rect, int Flags)
+{
+  if (ShowAccelChar)
+  {
+    Flags = Flags | HideAccelFlag(this);
+  }
+  TLabel::DoDrawText(Rect, Flags);
+}

source/windows/GUITools.h
@@ -179,6 +179,30 @@ private:
   HINSTANCE FLibrary;
 };
 //---------------------------------------------------------------------------
+// Based on:
+// https://stackoverflow.com/q/6912424/850848
+// https://stackoverflow.com/q/4685863/850848
+class TUIStateAwareLabel : public TLabel
+{
+protected:
+  DYNAMIC void __fastcall DoDrawText(TRect & Rect, int Flags);
+};
+#define INTERFACE_HOOK \
+  protected: \
+    virtual void __fastcall ReadState(TReader * Reader) \
+    { \
+      Reader->OnFindComponentClass = FindComponentClass; \
+      TForm::ReadState(Reader); \
+    } \
+  \
+    void __fastcall FindComponentClass(TReader * Reader, const UnicodeString ClassName, TComponentClass & ComponentClass) \
+    { \
+      if (ComponentClass == __classid(TLabel)) \
+      { \
+        ComponentClass = __classid(TUIStateAwareLabel); \
+      } \
+    }
+//---------------------------------------------------------------------------
 extern const UnicodeString PageantTool;
 extern const UnicodeString PuttygenTool;
 //---------------------------------------------------------------------------

source/forms/Authenticate.h
@@ -116,6 +116,8 @@ private:
   int FVerticalLogPadding;
   TTextFormat FLogTextFormat;
   bool FShowNoActivate;
+
+  INTERFACE_HOOK;
 };
 //---------------------------------------------------------------------------
 #endif

source/forms/Cleanup.h
@@ -10,6 +10,7 @@
 #include <Configuration.h>
 #include <SessionData.h>
 #include <WinInterface.h>
+#include <GUITools.h>
 //---------------------------------------------------------------------
 enum TWinSCPData {wdConfiguration = 1, wdStoredSessions, wdHostKeys,
   wdConfigurationIniFile, wdRandomSeedFile, wdTemporaryFolders };
@@ -38,6 +39,9 @@ private:
   void __fastcall InitControls();
   void __fastcall UpdateControls();
   bool __fastcall GetCleanupData(TWinSCPData Data);
+
+  INTERFACE_HOOK;
+
 public:
   virtual __fastcall TCleanupDialog(TComponent* AOwner);
   __property TStoredSessionList *SessionList  = { read=FSessionList, write=FSessionList };

source/forms/Console.h
@@ -18,6 +18,7 @@
 #include "PngImageList.hpp"
 #include <Vcl.Imaging.pngimage.hpp>
 #include <System.Actions.hpp>
+#include <GUITools.h>
 //----------------------------------------------------------------------------
 class TConsoleDialog : public TForm
 {
@@ -78,6 +79,8 @@ protected:
   virtual void __fastcall CreateParams(TCreateParams & Params);
   virtual void __fastcall Dispatch(void * Message);
   void __fastcall DoAdjustWindow();
+
+  INTERFACE_HOOK;
 
 public:
   virtual __fastcall ~TConsoleDialog();

source/forms/Copy.h
@@ -76,6 +76,9 @@ protected:
   bool __fastcall RemotePaths();
   void __fastcall CopyParamListPopup(TRect R, int AdditionalOptions);
   int __fastcall ActualCopyParamAttrs();
+
+  INTERFACE_HOOK;
+
 public:
   __fastcall TCopyDialog(
     TComponent* Owner, bool ToRemote, bool Move, TStrings * FileList, int Options,

source/forms/CopyParamPreset.h
@@ -47,6 +47,8 @@ protected:
   TCopyParamRule * __fastcall GetRule();
   void __fastcall SetRuleData(const TCopyParamRuleData & Data);
 
+  INTERFACE_HOOK;
+
 public:
   __fastcall TCopyParamPresetDialog(TComponent * Owner,
     TCopyParamPresetMode Mode, TCopyParamRuleData * CurrentRuleData);

source/forms/CreateDirectory.h
@@ -11,6 +11,7 @@
 #include <Vcl.StdCtrls.hpp>
 //----------------------------------------------------------------------------
 #include <Bookmarks.h>
+#include <GUITools.h>
 #include "RightsExt.h"
 #include "Rights.h"
 //----------------------------------------------------------------------------
@@ -42,6 +43,8 @@ public:
 
 protected:
   void __fastcall UpdateControls();
+
+  INTERFACE_HOOK;
 
 private:
   int FAllowedChanges;

commit 3bedafb566d1989fbcef26fdd8ba2082f144c605

Prevent compiler warning caused by 4031057b67
Source commit: 8837e1525f060a77109385b0905b7173e5858588

source/Console.cbproj
@@ -43,7 +43,7 @@
 		<ProjectType>CppConsoleApplication</ProjectType>
 		<SanitizedProjectName>Console</SanitizedProjectName>
 		<VerInfo_IncludeVerInfo>true</VerInfo_IncludeVerInfo>
-		<VerInfo_Keys>CompanyName=Martin Prikryl;FileDescription=Console interface for WinSCP;FileVersion=5.0.1.0;InternalName=console;LegalCopyright=(c) 2000-2018 Martin Prikryl;LegalTrademarks=;OriginalFilename=winscp.com;ProductName=WinSCP;ProductVersion=5.14.3.0;ReleaseType=stable;WWW=https://winscp.net/</VerInfo_Keys>
+		<VerInfo_Keys>CompanyName=Martin Prikryl;FileDescription=Console interface for WinSCP;FileVersion=5.0.1.0;InternalName=console;LegalCopyright=(c) 2000-2019 Martin Prikryl;LegalTrademarks=;OriginalFilename=winscp.com;ProductName=WinSCP;ProductVersion=5.14.3.0;ReleaseType=stable;WWW=https://winscp.net/</VerInfo_Keys>
 		<VerInfo_Locale>1033</VerInfo_Locale>
 		<VerInfo_MajorVer>5</VerInfo_MajorVer>
 		<VerInfo_MinorVer>0</VerInfo_MinorVer>

source/DragExt.cbproj
@@ -49,7 +49,7 @@
 		<SanitizedProjectName>DragExt</SanitizedProjectName>
 		<VerInfo_DLL>true</VerInfo_DLL>
 		<VerInfo_IncludeVerInfo>true</VerInfo_IncludeVerInfo>
-		<VerInfo_Keys>CompanyName=Martin Prikryl;FileDescription=Drag&amp;Drop shell extension for WinSCP ($(Platform));FileVersion=2.0.0.0;InternalName=dragext;LegalCopyright=(c) 2000-2018 Martin Prikryl;LegalTrademarks=;OriginalFilename=dragext.dll;ProductName=WinSCP;ProductVersion=5.14.3.0;ReleaseType=stable;WWW=https://winscp.net/</VerInfo_Keys>
+		<VerInfo_Keys>CompanyName=Martin Prikryl;FileDescription=Drag&amp;Drop shell extension for WinSCP ($(Platform));FileVersion=2.0.0.0;InternalName=dragext;LegalCopyright=(c) 2000-2019 Martin Prikryl;LegalTrademarks=;OriginalFilename=dragext.dll;ProductName=WinSCP;ProductVersion=5.14.3.0;ReleaseType=stable;WWW=https://winscp.net/</VerInfo_Keys>
 		<VerInfo_Locale>1033</VerInfo_Locale>
 		<VerInfo_MajorVer>2</VerInfo_MajorVer>
 	</PropertyGroup>

source/WinSCP.cbproj
@@ -76,7 +76,7 @@
 		<SanitizedProjectName>WinSCP</SanitizedProjectName>
 		<UsingDelphiRTL>true</UsingDelphiRTL>
 		<VerInfo_IncludeVerInfo>true</VerInfo_IncludeVerInfo>
-		<VerInfo_Keys>CompanyName=Martin Prikryl;FileDescription=WinSCP: SFTP, FTP, WebDAV, S3 and SCP client;FileVersion=5.14.3.0;InternalName=winscp;LegalCopyright=(c) 2000-2018 Martin Prikryl;LegalTrademarks=;OriginalFilename=winscp.exe;ProductName=WinSCP;ProductVersion=5.14.3.0;ReleaseType=stable;WWW=https://winscp.net/</VerInfo_Keys>
+		<VerInfo_Keys>CompanyName=Martin Prikryl;FileDescription=WinSCP: SFTP, FTP, WebDAV, S3 and SCP client;FileVersion=5.14.3.0;InternalName=winscp;LegalCopyright=(c) 2000-2019 Martin Prikryl;LegalTrademarks=;OriginalFilename=winscp.exe;ProductName=WinSCP;ProductVersion=5.14.3.0;ReleaseType=stable;WWW=https://winscp.net/</VerInfo_Keys>
 		<VerInfo_Locale>1033</VerInfo_Locale>
 		<VerInfo_MajorVer>5</VerInfo_MajorVer>
 		<VerInfo_MinorVer>14</VerInfo_MinorVer>

source/windows/GUITools.cpp
@@ -2096,3 +2096,12 @@ void __fastcall TUIStateAwareLabel::DoDrawText(TRect & Rect, int Flags)
   }
   TLabel::DoDrawText(Rect, Flags);
 }
+//---------------------------------------------------------------------------
+void __fastcall FindComponentClass(
+  void *, TReader *, const UnicodeString DebugUsedArg(ClassName), TComponentClass & ComponentClass)
+{
+  if (ComponentClass == __classid(TLabel))
+  {
+    ComponentClass = __classid(TUIStateAwareLabel);
+  }
+}

source/windows/GUITools.h
@@ -187,20 +187,17 @@ class TUIStateAwareLabel : public TLabel
 protected:
   DYNAMIC void __fastcall DoDrawText(TRect & Rect, int Flags);
 };
+// FindComponentClass takes parameter by referece and as such it cannot be implemented in
+// an inline method without a compiler warning, which we cannot suppress in a macro.
+// And having the implementation in a real code (not macro) also allows us to debug the code.
+void __fastcall FindComponentClass(
+  void * Data, TReader * Reader, const UnicodeString ClassName, TComponentClass & ComponentClass);
 #define INTERFACE_HOOK \
   protected: \
     virtual void __fastcall ReadState(TReader * Reader) \
     { \
-      Reader->OnFindComponentClass = FindComponentClass; \
+      Reader->OnFindComponentClass = MakeMethod<TFindComponentClassEvent>(NULL, FindComponentClass); \
       TForm::ReadState(Reader); \
-    } \
-  \
-    void __fastcall FindComponentClass(TReader * Reader, const UnicodeString ClassName, TComponentClass & ComponentClass) \
-    { \
-      if (ComponentClass == __classid(TLabel)) \
-      { \
-        ComponentClass = __classid(TUIStateAwareLabel); \
-      } \
     }
 //---------------------------------------------------------------------------
 extern const UnicodeString PageantTool;