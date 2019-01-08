d3f40c5aa777f9f8ccf8f49bbd1e10a49deee337
Martin Prikryl <martin@winscp.net>
2019-01-08 13:57:52 +0000

    Bumping .NET assembly version number before 5.13.7 release as there were changes
    Source commit: 02eaf41eccd331674566b8d925158c3b180f30c4

--- dotnet/properties/AssemblyInfo.cs
+++ dotnet/properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ namespace WinSCP
     {
         public const string UndefinedProductVersion = "9.9.9.9";
 
-        public const string Version = "1.5.6.0";
+        public const string Version = "1.5.7.0";
         public const string ProductVersion = "5.13.7.0";
     }
 }
