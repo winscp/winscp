e689da0b44f516e69f7261934659c8fef2640519
Martin Prikryl <martin@winscp.net>
2019-04-03 16:32:15 +0000

    Bug fix: Errors while mass-modifying sites were displayed in message box instead of console window
    Source commit: f37b0c95dd58fdc65a32d5486422eb9303ecef58

--- source/windows/ConsoleRunner.cpp
+++ source/windows/ConsoleRunner.cpp
@@ -2288,57 +2288,6 @@ void __fastcall Usage(TConsole * Console)
   Console->WaitBeforeExit();
 }
 //---------------------------------------------------------------------------
-void __fastcall BatchSettings(TConsole * Console, TProgramParams * Params)
-{
-  std::unique_ptr<TStrings> Arguments(new TStringList());
-  if (DebugAlwaysTrue(Params->FindSwitch(L"batchsettings", Arguments.get())))
-  {
-    if (Arguments->Count < 1)
-    {
-      Console->PrintLine(LoadStr(BATCH_SET_NO_MASK));
-    }
-    else if (Arguments->Count < 2)
-    {
-      Console->PrintLine(LoadStr(BATCH_SET_NO_SETTINGS));
-    }
-    else
-    {
-      TFileMasks Mask(Arguments->Strings[0]);
-      Arguments->Delete(0);
-
-      std::unique_ptr<TOptionsStorage> OptionsStorage(new TOptionsStorage(Arguments.get(), false));
-
-      int Matches = 0;
-      int Changes = 0;
-
-      for (int Index = 0; Index < StoredSessions->Count; Index++)
-      {
-        TSessionData * Data = StoredSessions->Sessions[Index];
-        if (!Data->IsWorkspace &&
-            Mask.Matches(Data->Name, false, false))
-        {
-          Matches++;
-          std::unique_ptr<TSessionData> OriginalData(new TSessionData(L""));
-          OriginalData->Assign(Data);
-          Data->ApplyRawSettings(OptionsStorage.get());
-          bool Changed = !OriginalData->IsSame(Data, false);
-          if (Changed)
-          {
-            Changes++;
-          }
-          UnicodeString StateStr = LoadStr(Changed ? BATCH_SET_CHANGED : BATCH_SET_NOT_CHANGED);
-          Console->PrintLine(FORMAT(L"%s - %s", (Data->Name, StateStr)));
-        }
-      }
-
-      StoredSessions->Save(false, true); // explicit
-      Console->PrintLine(FMTLOAD(BATCH_SET_SUMMARY, (Matches, Changes)));
-    }
-
-    Console->WaitBeforeExit();
-  }
-}
-//---------------------------------------------------------------------------
 int __fastcall HandleException(TConsole * Console, Exception & E)
 {
   UnicodeString Message;
@@ -2347,6 +2296,70 @@ int __fastcall HandleException(TConsole * Console, Exception & E)
     Console->Print(Message);
   }
   return RESULT_ANY_ERROR;
+}
+//---------------------------------------------------------------------------
+int __fastcall BatchSettings(TConsole * Console, TProgramParams * Params)
+{
+  int Result = RESULT_SUCCESS;
+  try
+  {
+    std::unique_ptr<TStrings> Arguments(new TStringList());
+    if (!DebugAlwaysTrue(Params->FindSwitch(L"batchsettings", Arguments.get())))
+    {
+      Abort();
+    }
+    else
+    {
+      if (Arguments->Count < 1)
+      {
+        throw Exception(LoadStr(BATCH_SET_NO_MASK));
+      }
+      else if (Arguments->Count < 2)
+      {
+        throw Exception(LoadStr(BATCH_SET_NO_SETTINGS));
+      }
+      else
+      {
+        TFileMasks Mask(Arguments->Strings[0]);
+        Arguments->Delete(0);
+
+        std::unique_ptr<TOptionsStorage> OptionsStorage(new TOptionsStorage(Arguments.get(), false));
+
+        int Matches = 0;
+        int Changes = 0;
+
+        for (int Index = 0; Index < StoredSessions->Count; Index++)
+        {
+          TSessionData * Data = StoredSessions->Sessions[Index];
+          if (!Data->IsWorkspace &&
+              Mask.Matches(Data->Name, false, false))
+          {
+            Matches++;
+            std::unique_ptr<TSessionData> OriginalData(new TSessionData(L""));
+            OriginalData->Assign(Data);
+            Data->ApplyRawSettings(OptionsStorage.get());
+            bool Changed = !OriginalData->IsSame(Data, false);
+            if (Changed)
+            {
+              Changes++;
+            }
+            UnicodeString StateStr = LoadStr(Changed ? BATCH_SET_CHANGED : BATCH_SET_NOT_CHANGED);
+            Console->PrintLine(FORMAT(L"%s - %s", (Data->Name, StateStr)));
+          }
+        }
+
+        StoredSessions->Save(false, true); // explicit
+        Console->PrintLine(FMTLOAD(BATCH_SET_SUMMARY, (Matches, Changes)));
+      }
+    }
+  }
+  catch (Exception & E)
+  {
+    Result = HandleException(Console, E);
+  }
+
+  Console->WaitBeforeExit();
+  return Result;
 }
 //---------------------------------------------------------------------------
 bool __fastcall FindPuttygenCompatibleSwitch(
@@ -2683,7 +2696,7 @@ int __fastcall Console(TConsoleMode Mode)
       if (CheckSafe(Params))
       {
         Configuration->Usage->Inc(L"BatchSettings");
-        BatchSettings(Console, Params);
+        Result = BatchSettings(Console, Params);
       }
     }
     else if (Mode == cmKeyGen)
