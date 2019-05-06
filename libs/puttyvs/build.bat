2a23580572007bfa8006491a116bdf512624d4e2
Martin Prikryl <martin@winscp.net>
2019-05-06 21:08:04 +0000

    Sync OMF lib existence with result of COFF build
    Source commit: 49c2b4a50ae8db70a0306529700e74bd96844dd8

--- libs/puttyvs/build.bat
+++ libs/puttyvs/build.bat
@@ -4,5 +4,12 @@ if "%BUILDTOOLS_PATH%" == "" set BUILDTOOLS_PATH=..\..\buildtools
 if "%PROCESSOR_ARCHITECTURE%"=="x86" set PROGRAMFILES32=%ProgramFiles%
 if "%PROCESSOR_ARCHITECTURE%"=="AMD64" set PROGRAMFILES32=%ProgramFiles(x86)%
 
+set COFF_LIB=Release\PuTTYVS.lib
+set OMF_LIB=..\lib\PuTTYVS.lib
+if exist %COFF_LIB% del %COFF_LIB%
+if exist %OMF_LIB% del %OMF_LIB%
+
 "%PROGRAMFILES32%\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe" PuTTYVS.vcxproj /p:Configuration=Release
-%BUILDTOOLS_PATH%\tools\objconv.exe -fomf32 Release\PuTTYVS.lib ..\lib\PuTTYVS.lib
+if exist %COFF_LIB% (
+  %BUILDTOOLS_PATH%\tools\objconv.exe -v2 -fomf32 %COFF_LIB% %OMF_LIB%
+)
