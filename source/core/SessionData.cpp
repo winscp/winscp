winscp/winscp

commit 2061ad0031c17cd6946a2b2d1c66876bd7d90f14

Bug fix: It was not possible to mass-modify stored sites, when a master password was set (2nd)
Previous implementation broke a prompt for master password, when logging to a site wit encrypted passwords.

Source commit: 0cf714c96ffbdcc593c094f7a30a35664e275aff

source/core/SessionData.cpp
@@ -448,6 +448,7 @@ void __fastcall TSessionData::Assign(TPersistent * Source)
   if (Source && Source->InheritsFrom(__classid(TSessionData)))
   {
     TSessionData * SourceData = (TSessionData *)Source;
+    // Master password prompt shows implicitly here, when cloning the session data for a new terminal
     CopyData(SourceData);
     FSource = SourceData->FSource;
   }
@@ -457,9 +458,17 @@ void __fastcall TSessionData::Assign(TPersistent * Source)
   }
 }
 //---------------------------------------------------------------------
-void __fastcall TSessionData::CopyData(TSessionData * SourceData)
+void __fastcall TSessionData::DoCopyData(TSessionData * SourceData, bool NoRecrypt)
 {
-  #define PROPERTY_HANDLER(P, F) F##P = SourceData->F##P
+  #define PROPERTY_HANDLER(P, F) \
+    if (NoRecrypt) \
+    { \
+      F##P = SourceData->F##P; \
+    } \
+    else \
+    { \
+      P = SourceData->P; \
+    }
   PROPERTY(Name);
   BASE_PROPERTIES;
   ADVANCED_PROPERTIES;
@@ -468,6 +477,16 @@ void __fastcall TSessionData::CopyData(TSessionData * SourceData)
   FOverrideCachedHostKey = SourceData->FOverrideCachedHostKey;
   FModified = SourceData->Modified;
   FSaveOnly = SourceData->FSaveOnly;
+}
+//---------------------------------------------------------------------
+void __fastcall TSessionData::CopyData(TSessionData * SourceData)
+{
+  DoCopyData(SourceData, false);
+}
+//---------------------------------------------------------------------
+void __fastcall TSessionData::CopyDataNoRecrypt(TSessionData * SourceData)
+{
+  DoCopyData(SourceData, true);
 }
 //---------------------------------------------------------------------
 void __fastcall TSessionData::CopyDirectoriesStateData(TSessionData * SourceData)

source/core/SessionData.h
@@ -445,6 +445,7 @@ private:
   TStrings * __fastcall SaveToOptions(const TSessionData * Default);
   void __fastcall ApplyRawSettings(TStrings * RawSettings);
   TStrings * __fastcall GetRawSettingsForUrl();
+  void __fastcall DoCopyData(TSessionData * SourceData, bool NoRecrypt);
   template<class AlgoT>
   void __fastcall SetAlgoList(AlgoT * List, const AlgoT * DefaultList, const UnicodeString * Names,
     int Count, AlgoT WarnAlgo, UnicodeString value);
@@ -475,6 +476,7 @@ public:
   virtual void __fastcall Assign(TPersistent * Source);
   virtual int __fastcall Compare(TNamedObject * Other);
   void __fastcall CopyData(TSessionData * Source);
+  void __fastcall CopyDataNoRecrypt(TSessionData * SourceData);
   void __fastcall CopyDirectoriesStateData(TSessionData * SourceData);
   bool __fastcall ParseUrl(UnicodeString Url, TOptions * Options,
     TStoredSessionList * StoredSessions, bool & DefaultsOnly,

source/windows/ConsoleRunner.cpp
@@ -2336,7 +2336,7 @@ int __fastcall BatchSettings(TConsole * Console, TProgramParams * Params)
           {
             Matches++;
             std::unique_ptr<TSessionData> OriginalData(new TSessionData(L""));
-            OriginalData->Assign(Data);
+            OriginalData->CopyDataNoRecrypt(Data);
             Data->ApplyRawSettings(OptionsStorage.get());
             bool Changed = !OriginalData->IsSame(Data, false);
             if (Changed)

commit b53ebad49491fc9c39310319f0cb85f16f6f4b9d

Bug fix: When default session setting has any password stored, Login dialog always opens on NewSite page
Caused by 2061ad00
https://winscp.net/forum/viewtopic.php?t=28005

Source commit: 7b949cc22ef25c0e56ea6c333481555ae0f8e581

source/core/SessionData.cpp
@@ -518,11 +518,13 @@ void __fastcall TSessionData::CopyNonCoreData(TSessionData * SourceData)
   Note = SourceData->Note;
 }
 //---------------------------------------------------------------------
-bool __fastcall TSessionData::IsSame(const TSessionData * Default, bool AdvancedOnly, TStrings * DifferentProperties)
+bool __fastcall TSessionData::IsSame(
+  const TSessionData * Default, bool AdvancedOnly, TStrings * DifferentProperties, bool Decrypted)
 {
   bool Result = true;
   #define PROPERTY_HANDLER(P, F) \
-    if (F##P != Default->F##P) \
+    if ((Decrypted && (P != Default->P)) || \
+        (!Decrypted && (F##P != Default->F##P))) \
     { \
       Result = false; \
       if (DifferentProperties != NULL) \
@@ -547,7 +549,12 @@ bool __fastcall TSessionData::IsSame(const TSessionData * Default, bool Advanced
 //---------------------------------------------------------------------
 bool __fastcall TSessionData::IsSame(const TSessionData * Default, bool AdvancedOnly)
 {
-  return IsSame(Default, AdvancedOnly, NULL);
+  return IsSame(Default, AdvancedOnly, NULL, false);
+}
+//---------------------------------------------------------------------
+bool __fastcall TSessionData::IsSameDecrypted(const TSessionData * Default)
+{
+  return IsSame(Default, false, NULL, true);
 }
 //---------------------------------------------------------------------
 TFSProtocol NormalizeFSProtocol(TFSProtocol FSProtocol)
@@ -4610,9 +4617,9 @@ void __fastcall TStoredSessionList::UpdateStaticUsage()
         Note++;
       }
 
-      // this effectively does not take passwords (proxy + tunnel) into account,
-      // when master password is set, as master password handler in not set up yet
-      if (!Data->IsSame(FactoryDefaults.get(), true, DifferentAdvancedProperties.get()))
+      // This would not work for passwords, as they are compared in their encrypted form.
+      // But there are no passwords set in factory defaults anyway.
+      if (!Data->IsSame(FactoryDefaults.get(), true, DifferentAdvancedProperties.get(), false))
       {
         Advanced++;
       }

source/core/SessionData.h
@@ -414,7 +414,7 @@ private:
   _di_IXMLNode __fastcall FindSettingsNode(_di_IXMLNode Node, const UnicodeString & Name);
   UnicodeString __fastcall ReadSettingsNode(_di_IXMLNode Node, const UnicodeString & Name, const UnicodeString & Default);
   int __fastcall ReadSettingsNode(_di_IXMLNode Node, const UnicodeString & Name, int Default);
-  bool __fastcall IsSame(const TSessionData * Default, bool AdvancedOnly, TStrings * DifferentProperties);
+  bool __fastcall IsSame(const TSessionData * Default, bool AdvancedOnly, TStrings * DifferentProperties, bool Decrypted);
   UnicodeString __fastcall GetNameWithoutHiddenPrefix();
   bool __fastcall HasStateData();
   void __fastcall CopyStateData(TSessionData * SourceData);
@@ -487,6 +487,7 @@ public:
   void __fastcall ExpandEnvironmentVariables();
   void __fastcall DisableAuthentationsExceptPassword();
   bool __fastcall IsSame(const TSessionData * Default, bool AdvancedOnly);
+  bool __fastcall IsSameDecrypted(const TSessionData * Default);
   bool __fastcall IsSameSite(const TSessionData * Default);
   bool __fastcall IsInFolderOrWorkspace(UnicodeString Name);
   UnicodeString __fastcall GenerateSessionUrl(unsigned int Flags);

source/forms/Login.cpp
@@ -1270,12 +1270,11 @@ bool __fastcall TLoginDialog::Execute(TList * DataList)
       FNewSiteData->CopyData(SessionData);
       FNewSiteData->Special = false;
 
-      // This is actualy bit pointless, as we focus the last selected site anyway
-      // in LoadState(). As of now, we hardly get any useful data
-      // in ad-hoc DataList anyway, so it is not a big deal
+      // This is actualy bit pointless.
+      // As of now, we hardly ever get any useful data in ad-hoc DataList.
       // (this was implemented for support taking session url from clipboard instead
       // of command-line, but without autoconnect, but this functionality was cancelled)
-      if (!FNewSiteData->IsSame(StoredSessions->DefaultSettings, false))
+      if (!FNewSiteData->IsSameDecrypted(StoredSessions->DefaultSettings))
       {
         // we want to start with new site page
         FForceNewSite = true;