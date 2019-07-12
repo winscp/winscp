winscp/winscp

commit 0ccd2defd36413be0ad2ff4064704367a5da1f1a

SSH password change + When opening secondary shell session fails, the main session is not Closed + Session.ExecuteCommand unit tests
ChangePassword + PasswordChangesFailed counters
task_change_password help keyword

Source commit: 335f4de0796d0367a8ed427790cf1407607cc0d8

source/core/SessionData.cpp
@@ -104,6 +104,8 @@ void __fastcall TSessionData::Default()
   PortNumber = SshPortNumber;
   UserName = L"";
   Password = L"";
+  NewPassword = L"";
+  ChangePassword = false;
   PingInterval = 30;
   PingType = ptOff;
   Timeout = 15;
@@ -277,6 +279,8 @@ void __fastcall TSessionData::NonPersistant()
   PROPERTY(Note);
 //---------------------------------------------------------------------
 #define ADVANCED_PROPERTIES \
+  PROPERTY(NewPassword); \
+  PROPERTY(ChangePassword); \
   PROPERTY(PingInterval); \
   PROPERTY(PingType); \
   PROPERTY(Timeout); \
@@ -1420,6 +1424,7 @@ void __fastcall TSessionData::SavePasswords(THierarchicalStorage * Storage, bool
 void __fastcall TSessionData::RecryptPasswords()
 {
   Password = Password;
+  NewPassword = NewPassword;
   ProxyPassword = ProxyPassword;
   TunnelPassword = TunnelPassword;
   Passphrase = Passphrase;
@@ -1437,12 +1442,17 @@ bool __fastcall TSessionData::HasAnySessionPassword()
 //---------------------------------------------------------------------
 bool __fastcall TSessionData::HasAnyPassword()
 {
-  return HasAnySessionPassword() || !FProxyPassword.IsEmpty();
+  return
+    HasAnySessionPassword() ||
+    !FProxyPassword.IsEmpty() ||
+    // will probably be never used
+    FNewPassword.IsEmpty();
 }
 //---------------------------------------------------------------------
 void __fastcall TSessionData::ClearSessionPasswords()
 {
   FPassword = L"";
+  FNewPassword = L"";
   FTunnelPassword = L"";
 }
 //---------------------------------------------------------------------
@@ -1560,7 +1570,9 @@ bool __fastcall TSessionData::IsProtocolUrl(
 //---------------------------------------------------------------------
 bool __fastcall TSessionData::IsSensitiveOption(const UnicodeString & Option)
 {
-  return SameText(Option, PassphraseOption);
+  return
+    SameText(Option, PassphraseOption) ||
+    SameText(Option, NEWPASSWORD_SWITCH);
 }
 //---------------------------------------------------------------------
 bool __fastcall TSessionData::ParseUrl(UnicodeString Url, TOptions * Options,
@@ -1869,6 +1881,11 @@ bool __fastcall TSessionData::ParseUrl(UnicodeString Url, TOptions * Options,
     {
       Name = Value;
     }
+    if (Options->FindSwitch(NEWPASSWORD_SWITCH, Value))
+    {
+      ChangePassword = true;
+      NewPassword = Value;
+    }
     if (Options->FindSwitch(L"privatekey", Value))
     {
       PublicKeyFile = Value;
@@ -2077,6 +2094,7 @@ void __fastcall TSessionData::SetHostName(UnicodeString value)
   {
     // HostName is key for password encryption
     UnicodeString XPassword = Password;
+    UnicodeString XNewPassword = Password;
 
     // This is now hardly used as hostname is parsed directly on login dialog.
     // But can be used when importing sites from PuTTY, as it allows same format too.
@@ -2090,7 +2108,9 @@ void __fastcall TSessionData::SetHostName(UnicodeString value)
     Modify();
 
     Password = XPassword;
+    NewPassword = XNewPassword;
     Shred(XPassword);
+    Shred(XNewPassword);
   }
 }
 //---------------------------------------------------------------------
@@ -2141,9 +2161,12 @@ void __fastcall TSessionData::SetUserName(UnicodeString value)
   {
     // UserName is key for password encryption
     UnicodeString XPassword = Password;
+    UnicodeString XNewPassword = NewPassword;
     SET_SESSION_PROPERTY(UserName);
     Password = XPassword;
+    NewPassword = XNewPassword;
     Shred(XPassword);
+    Shred(XNewPassword);
   }
 }
 //---------------------------------------------------------------------
@@ -2161,6 +2184,22 @@ void __fastcall TSessionData::SetPassword(UnicodeString avalue)
 UnicodeString __fastcall TSessionData::GetPassword() const
 {
   return DecryptPassword(FPassword, UserName+HostName);
+}
+//---------------------------------------------------------------------
+void __fastcall TSessionData::SetNewPassword(UnicodeString avalue)
+{
+  RawByteString value = EncryptPassword(avalue, UserName+HostName);
+  SET_SESSION_PROPERTY(NewPassword);
+}
+//---------------------------------------------------------------------
+UnicodeString __fastcall TSessionData::GetNewPassword() const
+{
+  return DecryptPassword(FNewPassword, UserName+HostName);
+}
+//---------------------------------------------------------------------
+void __fastcall TSessionData::SetChangePassword(bool value)
+{
+  SET_SESSION_PROPERTY(ChangePassword);
 }
 //---------------------------------------------------------------------
 void __fastcall TSessionData::SetPingInterval(int value)
@@ -3645,6 +3684,19 @@ UnicodeString __fastcall TSessionData::ComposePath(
   const UnicodeString & Path, const UnicodeString & Name)
 {
   return UnixIncludeTrailingBackslash(Path) + Name;
+}
+//---------------------------------------------------------------------
+void __fastcall TSessionData::DisableAuthentationsExceptPassword()
+{
+  SshNoUserAuth = false;
+  AuthTIS = false;
+  AuthKI = false;
+  AuthKIPassword = false;
+  AuthGSSAPI = false;
+  PublicKeyFile = L"";
+  TlsCertificateFile = L"";
+  Passphrase = L"";
+  TryAgent = false;
 }
 //=== TStoredSessionList ----------------------------------------------
 __fastcall TStoredSessionList::TStoredSessionList(bool aReadOnly):

dotnet/GlobalSuppressions.cs
@@ -148,7 +148,7 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Scope = "member", Target = "WinSCP.Session.#TestHandlesClosedInternal")]
 [assembly: SuppressMessage("Microsoft.Security", "CA2122:DoNotIndirectlyExposeMethodsWithLinkDemands", Scope = "member", Target = "WinSCP.ExeSessionProcess.#TryCreateEvent(System.String,System.Threading.EventWaitHandle&)")]
 [assembly: SuppressMessage("Microsoft.Reliability", "CA2001:AvoidCallingProblematicMethods", MessageId = "System.Runtime.InteropServices.SafeHandle.DangerousGetHandle", Scope = "member", Target = "WinSCP.ExeSessionProcess.#TryCreateEvent(System.String,System.Threading.EventWaitHandle&)")]
-[assembly: SuppressMessage("Microsoft.Security", "CA2122:DoNotIndirectlyExposeMethodsWithLinkDemands", Scope = "member", Target = "WinSCP.SessionOptions.#GetPassword()")]
+[assembly: SuppressMessage("Microsoft.Security", "CA2122:DoNotIndirectlyExposeMethodsWithLinkDemands", Scope = "member", Target = "WinSCP.SessionOptions.#GetPassword(System.Security.SecureString)")]
 [assembly: SuppressMessage("Microsoft.Design", "CA1054:UriParametersShouldNotBeStrings", MessageId = "0#", Scope = "member", Target = "WinSCP.SessionOptions.#ParseUrl(System.String)")]
 [assembly: SuppressMessage("Microsoft.Security", "CA2122:DoNotIndirectlyExposeMethodsWithLinkDemands", Scope = "member", Target = "WinSCP.ExeSessionProcess+NoopSafeHandle.#.ctor(System.IntPtr)")]
 [assembly: SuppressMessage("Microsoft.Security", "CA2122:DoNotIndirectlyExposeMethodsWithLinkDemands", Scope = "member", Target = "WinSCP.ExeSessionProcess.#CreateFileMapping(System.String)")]
@@ -158,6 +158,6 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope", Scope = "member", Target = "WinSCP.ExeSessionProcess.#CreateFileMapping(System.String)")]
 [assembly: SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope", Scope = "member", Target = "WinSCP.Logger.#CreateCounters()")]
 [assembly: SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope", Scope = "member", Target = "WinSCP.SessionLogReader.#LogContents()")]
-[assembly: SuppressMessage("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity", Scope = "member", Target = "WinSCP.Session.#SessionOptionsToSwitches(WinSCP.SessionOptions,System.Boolean)")]
+[assembly: SuppressMessage("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity", Scope = "member", Target = "WinSCP.Session.#SessionOptionsToSwitches(WinSCP.SessionOptions,System.Boolean,System.String&,System.String&)")]
 [assembly: SuppressMessage("Microsoft.Security", "CA5122:PInvokesShouldNotBeSafeCriticalFxCopRule", Scope = "member", Target = "WinSCP.UnsafeNativeMethods.#RegGetValue(System.UIntPtr,System.String,System.String,WinSCP.RegistryFlags,WinSCP.RegistryType&,System.IntPtr,System.UInt32&)")]
 [assembly: SuppressMessage("Microsoft.Security", "CA2122:DoNotIndirectlyExposeMethodsWithLinkDemands", Scope = "member", Target = "WinSCP.ExeSessionProcess.#GetInstallationPath(Microsoft.Win32.RegistryHive)")]

dotnet/Session.cs
@@ -1463,22 +1463,27 @@ namespace WinSCP
                 url += tail;
                 logUrl += tail;
 
-                string arguments = SessionOptionsToSwitches(sessionOptions, scanFingerprint);
+                string arguments;
+                string logArguments;
+                SessionOptionsToSwitches(sessionOptions, scanFingerprint, out arguments, out logArguments);
 
+                const string switchName = "-rawsettings";
                 Tools.AddRawParameters(ref arguments, sessionOptions.RawSettings, "-rawsettings");
+                Tools.AddRawParameters(ref logArguments, sessionOptions.RawSettings, switchName);
 
                 if (!string.IsNullOrEmpty(arguments))
                 {
                     arguments = " " + arguments;
+                    logArguments = " " + logArguments;
                 }
 
                 // Switches should (and particularly the -rawsettings MUST) come after the URL
                 command = "\"" + Tools.ArgumentEscape(url) + "\"" + arguments;
-                log = "\"" + Tools.ArgumentEscape(logUrl) + "\"" + arguments;
+                log = "\"" + Tools.ArgumentEscape(logUrl) + "\"" + logArguments;
             }
         }
 
-        private string SessionOptionsToSwitches(SessionOptions sessionOptions, bool scanFingerprint)
+        private void SessionOptionsToSwitches(SessionOptions sessionOptions, bool scanFingerprint, out string arguments, out string logArguments)
         {
             using (Logger.CreateCallstack())
             {
@@ -1580,7 +1585,20 @@ namespace WinSCP
 
                 switches.Add(FormatSwitch("timeout", (int)sessionOptions.Timeout.TotalSeconds));
 
-                return string.Join(" ", switches.ToArray());
+                List<string> logSwitches = new List<string>(switches);
+
+                if ((sessionOptions.SecureNewPassword != null) && !scanFingerprint)
+                {
+                    if (sessionOptions.SecurePassword == null)
+                    {
+                        throw new ArgumentException("SessionOptions.SecureNewPassword is set, but SessionOptions.SecurePassword is not.");
+                    }
+                    switches.Add(FormatSwitch("newpassword", sessionOptions.NewPassword));
+                    logSwitches.Add(FormatSwitch("newpassword", "***"));
+                }
+
+                arguments = string.Join(" ", switches.ToArray());
+                logArguments = string.Join(" ", logSwitches.ToArray());
             }
         }
 

dotnet/SessionOptions.cs
@@ -49,8 +49,10 @@ namespace WinSCP
         public string HostName { get; set; }
         public int PortNumber { get { return _portNumber; } set { SetPortNumber(value); } }
         public string UserName { get; set; }
-        public string Password { get { return GetPassword(); } set { SetPassword(value); } }
-        public SecureString SecurePassword { get; set; }
+        public string Password { get { return GetPassword(_securePassword); } set { SetPassword(ref _securePassword, value); } }
+        public SecureString SecurePassword { get { return _securePassword; } set { _securePassword = value; } }
+        public string NewPassword { get { return GetPassword(_secureNewPassword); } set { SetPassword(ref _secureNewPassword, value); } }
+        public SecureString SecureNewPassword { get { return _secureNewPassword; } set { _secureNewPassword = value; } }
         public TimeSpan Timeout { get { return _timeout; } set { SetTimeout(value); } }
         public int TimeoutInMilliseconds { get { return Tools.TimeSpanToMilliseconds(Timeout); } set { Timeout = Tools.MillisecondsToTimeSpan(value); } }
         public string PrivateKeyPassphrase { get; set; }
@@ -364,25 +366,25 @@ namespace WinSCP
             _webdavRoot = value;
         }
 
-        private void SetPassword(string value)
+        private static void SetPassword(ref SecureString securePassword, string value)
         {
             if (value == null)
             {
-                SecurePassword = null;
+                securePassword = null;
             }
             else
             {
-                SecurePassword = new SecureString();
+                securePassword = new SecureString();
                 foreach (char c in value)
                 {
-                    SecurePassword.AppendChar(c);
+                    securePassword.AppendChar(c);
                 }
             }
         }
 
-        private string GetPassword()
+        private static string GetPassword(SecureString securePassword)
         {
-            if (SecurePassword == null)
+            if (securePassword == null)
             {
                 return null;
             }
@@ -391,7 +393,7 @@ namespace WinSCP
                 IntPtr ptr = IntPtr.Zero;
                 try
                 {
-                    ptr = Marshal.SecureStringToGlobalAllocUnicode(SecurePassword);
+                    ptr = Marshal.SecureStringToGlobalAllocUnicode(securePassword);
                     return Marshal.PtrToStringUni(ptr);
                 }
                 finally
@@ -401,6 +403,8 @@ namespace WinSCP
             }
         }
 
+        private SecureString _securePassword;
+        private SecureString _secureNewPassword;
         private string _sshHostKeyFingerprint;
         private string _tlsHostCertificateFingerprint;
         private TimeSpan _timeout;

source/core/FtpFileSystem.cpp
@@ -2351,6 +2351,7 @@ bool __fastcall TFTPFileSystem::IsCapable(int Capability) const
     case fcLocking:
     case fcPreservingTimestampDirs:
     case fcResumeSupport:
+    case fcChangePassword:
       return false;
 
     default:

source/core/Interface.h
@@ -9,6 +9,7 @@
 #define SCRIPT_SWITCH "script"
 #define COMMAND_SWITCH L"Command"
 #define SESSIONNAME_SWICH L"SessionName"
+#define NEWPASSWORD_SWITCH L"newpassword"
 #define INI_NUL L"nul"
 #define PRESERVETIME_SWITCH L"preservetime"
 #define PRESERVETIMEDIRS_SWITCH_VALUE L"all"

source/core/ScpFileSystem.cpp
@@ -464,6 +464,9 @@ bool __fastcall TSCPFileSystem::IsCapable(int Capability) const
     case fcResumeSupport:
       return false;
 
+    case fcChangePassword:
+      return FSecureShell->CanChangePassword();
+
     default:
       DebugFail();
       return false;

source/core/Script.cpp
@@ -2059,7 +2059,7 @@ __fastcall TManagementScript::TManagementScript(TStoredSessionList * StoredSessi
 
   FCommands->Register(L"exit", SCRIPT_EXIT_DESC, SCRIPT_EXIT_HELP, &ExitProc, 0, 0, false);
   FCommands->Register(L"bye", 0, SCRIPT_EXIT_HELP, &ExitProc, 0, 0, false);
-  FCommands->Register(L"open", SCRIPT_OPEN_DESC, SCRIPT_OPEN_HELP8, &OpenProc, 0, -1, true);
+  FCommands->Register(L"open", SCRIPT_OPEN_DESC, SCRIPT_OPEN_HELP9, &OpenProc, 0, -1, true);
   FCommands->Register(L"close", SCRIPT_CLOSE_DESC, SCRIPT_CLOSE_HELP, &CloseProc, 0, 1, false);
   FCommands->Register(L"session", SCRIPT_SESSION_DESC, SCRIPT_SESSION_HELP, &SessionProc, 0, 1, false);
   FCommands->Register(L"lpwd", SCRIPT_LPWD_DESC, SCRIPT_LPWD_HELP, &LPwdProc, 0, 0, false);

commit 384fdf63bb94c7809fc5a170f11025ffcb5dbea0

When saving an edited site or default settings, WinSCP prompted for the master password, even when the site does not have any password to protect
Caused by 0ccd2def.

Source commit: d6f120c5c92a4a16310a746efda75dbe40dfdcdf

source/core/SessionData.cpp
@@ -1596,7 +1596,7 @@ bool __fastcall TSessionData::HasAnyPassword()
     HasAnySessionPassword() ||
     !FProxyPassword.IsEmpty() ||
     // will probably be never used
-    FNewPassword.IsEmpty();
+    !FNewPassword.IsEmpty();
 }
 //---------------------------------------------------------------------
 void __fastcall TSessionData::ClearSessionPasswords()
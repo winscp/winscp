384fdf63bb94c7809fc5a170f11025ffcb5dbea0
Martin Prikryl <martin@winscp.net>
2019-07-12 11:16:16 +0000

    When saving an edited site or default settings, WinSCP prompted for the master password, even when the site does not have any password to protect
    Caused by 0ccd2def.
    
    Source commit: d6f120c5c92a4a16310a746efda75dbe40dfdcdf

--- source/core/SessionData.cpp
+++ source/core/SessionData.cpp
@@ -1596,7 +1596,7 @@ bool __fastcall TSessionData::HasAnyPassword()
     HasAnySessionPassword() ||
     !FProxyPassword.IsEmpty() ||
     // will probably be never used
-    FNewPassword.IsEmpty();
+    !FNewPassword.IsEmpty();
 }
 //---------------------------------------------------------------------
 void __fastcall TSessionData::ClearSessionPasswords()
