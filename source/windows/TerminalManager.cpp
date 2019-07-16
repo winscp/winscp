winscp/winscp

commit ea24ee3405e8d94135f96c66096629af2bf3e746

Disconnect/Reconnect Session commands + Tab of a session that fails to connect/reconnect is not closed
Source commit: 546c99291ae6bc7a109fb54d172f2937d2f58630

source/windows/TerminalManager.cpp
@@ -31,7 +31,7 @@ __fastcall TManagedTerminal::TManagedTerminal(TSessionData * SessionData,
   TConfiguration * Configuration) :
   TTerminal(SessionData, Configuration),
   LocalExplorerState(NULL), RemoteExplorerState(NULL),
-  ReopenStart(0), DirectoryLoaded(Now()), TerminalThread(NULL)
+  ReopenStart(0), DirectoryLoaded(Now()), TerminalThread(NULL), Disconnected(false)
 {
   StateData = new TSessionData(L"");
   StateData->Assign(SessionData);
@@ -249,6 +249,7 @@ void __fastcall TTerminalManager::DoConnectTerminal(TTerminal * Terminal, bool R
           ManagedTerminal->ReopenStart = Now();
         }
 
+        ManagedTerminal->Disconnected = false;
         DebugAssert(ManagedTerminal->TerminalThread == NULL);
         ManagedTerminal->TerminalThread = TerminalThread;
       }
@@ -391,7 +392,7 @@ bool __fastcall TTerminalManager::ConnectActiveTerminalImpl(bool Reopen)
 
   if (Action == tpFree)
   {
-    FreeActiveTerminal();
+    DisconnectActiveTerminal();
   }
 
   return Result;
@@ -447,6 +448,10 @@ bool __fastcall TTerminalManager::ConnectActiveTerminal()
 void __fastcall TTerminalManager::DisconnectActiveTerminal()
 {
   DebugAssert(ActiveTerminal);
+  if (ActiveTerminal->Active)
+  {
+    ActiveTerminal->Close();
+  }
 
   int Index = IndexOf(ActiveTerminal);
 
@@ -457,6 +462,15 @@ void __fastcall TTerminalManager::DisconnectActiveTerminal()
   FQueues->Items[Index] = NewQueue;
   ScpExplorer->Queue = NewQueue;
   delete OldQueue;
+
+  dynamic_cast<TManagedTerminal *>(ActiveTerminal)->Disconnected = true;
+  if (ScpExplorer != NULL)
+  {
+    TerminalReady(); // in case it was never connected
+    ScpExplorer->TerminalDisconnected();
+  }
+  // disconnecting duplidate session removes need to distinguish the only connected session with short path
+  DoTerminalListChanged();
 }
 //---------------------------------------------------------------------------
 void __fastcall TTerminalManager::ReconnectActiveTerminal()
@@ -484,7 +498,7 @@ void __fastcall TTerminalManager::ReconnectActiveTerminal()
   }
   catch(...)
   {
-    FreeActiveTerminal();
+    DisconnectActiveTerminal();
     throw;
   }
 }
@@ -611,7 +625,7 @@ void __fastcall TTerminalManager::DoSetActiveTerminal(TTerminal * value, bool Au
     UpdateAppTitle();
     if (ScpExplorer)
     {
-      if (ActiveTerminal && (ActiveTerminal->Status == ssOpened))
+      if (ActiveTerminal && ((ActiveTerminal->Status == ssOpened) || dynamic_cast<TManagedTerminal *>(ActiveTerminal)->Disconnected))
       {
         TerminalReady();
       }
@@ -662,7 +676,9 @@ void __fastcall TTerminalManager::DoSetActiveTerminal(TTerminal * value, bool Au
       }
     }
 
-    if ((ActiveTerminal != NULL) && !ActiveTerminal->Active)
+
+    if ((ActiveTerminal != NULL) && !ActiveTerminal->Active &&
+        !dynamic_cast<TManagedTerminal *>(ActiveTerminal)->Disconnected)
     {
       ConnectActiveTerminal();
     }
@@ -1320,13 +1336,14 @@ UnicodeString __fastcall TTerminalManager::GetTerminalTitle(TTerminal * Terminal
   if (Unique)
   {
     int Index = IndexOf(Terminal);
-    // not for background transfer sessions
-    if (Index >= 0)
+    // not for background transfer sessions and disconnected sessions
+    if ((Index >= 0) && Terminal->Active)
     {
       for (int Index2 = 0; Index2 < Count; Index2++)
       {
         UnicodeString Name = Terminals[Index2]->SessionData->SessionName;
         if ((Terminals[Index2] != Terminal) &&
+            Terminals[Index2]->Active &&
             SameText(Name, Result))
         {
           UnicodeString Path = GetTerminalShortPath(Terminal);

source/windows/TerminalManager.h
@@ -28,6 +28,9 @@ public:
   TDateTime DirectoryLoaded;
   TTerminalThread * TerminalThread;
   TDateTime QueueOperationStart;
+  // To distinguish sessions that were explicitly disconnected and
+  // should not be reconnected when their tab is activated.
+  bool Disconnected;
 };
 //---------------------------------------------------------------------------
 class TTerminalManager : public TTerminalList

source/windows/UserInterface.cpp
@@ -228,6 +228,7 @@ void __fastcall ShowExtendedExceptionEx(TTerminal * Terminal,
     {
       if (ForActiveTerminal)
       {
+        DebugAssert(!Terminal->Active);
         Manager->DisconnectActiveTerminal();
       }
 
@@ -355,7 +356,7 @@ void __fastcall ShowExtendedExceptionEx(TTerminal * Terminal,
     {
       if (ForActiveTerminal)
       {
-        Manager->FreeActiveTerminal();
+        Manager->DisconnectActiveTerminal();
       }
     }
   }

source/components/ThemePageControl.cpp
@@ -122,7 +122,7 @@ void __fastcall TThemePageControl::PaintWindow(HDC DC)
   ::SendMessage(Handle, TCM_ADJUSTRECT, FALSE, (LPARAM)&PageRect);
 
   ClientRect.Top = PageRect.Top - 2;
-  DrawThemesXpTabItem(DC, -1, ClientRect, true, 0);
+  DrawThemesXpTabItem(DC, -1, ClientRect, true, 0, false);
 
   // 2nd paint the inactive tabs
 
@@ -165,12 +165,12 @@ void __fastcall TThemePageControl::DrawThemesXpTab(HDC DC, int Tab)
   {
     State = TIS_SELECTED;
   }
-  DrawThemesXpTabItem(DC, Tab, Rect, false, State);
+  DrawThemesXpTabItem(DC, Tab, Rect, false, State, Shadowed);
 }
 //----------------------------------------------------------------------------------------------------------
 // This function draws Themes Tab control parts: a) Tab-Body and b) Tab-tabs
 void __fastcall TThemePageControl::DrawThemesXpTabItem(HDC DC, int Item,
-  const TRect & Rect, bool Body, int State)
+  const TRect & Rect, bool Body, int State, bool Shadowed)
 {
   TSize Size = Rect.Size;
 
@@ -210,7 +210,7 @@ void __fastcall TThemePageControl::DrawThemesXpTabItem(HDC DC, int Item,
 
   if (!Body && (Item >= 0))
   {
-    DrawTabItem(DCMem, Item, Rect, RectItemMem, (State == TIS_SELECTED), (State == TIS_DISABLED));
+    DrawTabItem(DCMem, Item, Rect, RectItemMem, (State == TIS_SELECTED), Shadowed);
   }
 
   // Blit image to the screen

source/components/ThemePageControl.h
@@ -48,7 +48,7 @@ protected:
 
 private:
   void __fastcall DrawThemesXpTab(HDC DC, int Tab);
-  void __fastcall DrawThemesXpTabItem(HDC DC, int Item, const TRect & Rect, bool Body, int State);
+  void __fastcall DrawThemesXpTabItem(HDC DC, int Item, const TRect & Rect, bool Body, int State, bool Selected);
   void __fastcall DrawTabItem(HDC DC, int Item, TRect TabRect, TRect Rect, bool Selected, bool Shadowed);
   void __fastcall DrawThemesPart(HDC DC, int PartId, int StateId, LPCWSTR PartNameID, LPRECT Rect);
   void __fastcall InvalidateTab(int Index);

source/components/UnixDirView.cpp
@@ -507,7 +507,8 @@ void __fastcall TUnixDirView::SetDriveView(TCustomUnixDriveView * Value)
 void __fastcall TUnixDirView::DoSetTerminal(TTerminal * value, bool Replace)
 {
   DebugUsedParam(Replace);
-  if (FTerminal != value)
+  if ((FTerminal != value) ||
+      ((FTerminal != NULL) && !FTerminal->Active)) // Abused by TCustomScpExplorerForm::DisconnectSession
   {
     if (FTerminal)
     {
@@ -549,6 +550,10 @@ void __fastcall TUnixDirView::DoSetTerminal(TTerminal * value, bool Replace)
         DoStartReadDirectory(FTerminal); // just for style and the assertions
         DoReadDirectoryImpl(FTerminal, false);
       }
+      else
+      {
+        PathChanged(); // To clear path combo box
+      }
     }
     UpdatePathLabel();
   }
@@ -586,10 +591,9 @@ void __fastcall TUnixDirView::DoReadDirectoryImpl(TObject * /*Sender*/, bool Rel
   FLoading = false;
 
 #ifndef DESIGN_ONLY
+  CancelEdit();
   if (Terminal->Active)
   {
-    CancelEdit();
-
     if (ReloadOnly)
     {
       Reload(false);

source/components/UnixDriveView.cpp
@@ -74,11 +74,11 @@ void __fastcall TCustomUnixDriveView::CreateWnd()
 //---------------------------------------------------------------------------
 void __fastcall TCustomUnixDriveView::SetTerminal(TTerminal * value)
 {
-  if (FTerminal != value)
+  #ifndef DESIGN_ONLY
+  if ((FTerminal != value) || ((FTerminal != NULL) && !FTerminal->Active)) // Abused by TCustomScpExplorerForm::DisconnectSession
   {
     FTerminal = value;
     Items->Clear();
-    #ifndef DESIGN_ONLY
     // If terminal is not active initially, we will never load fixed paths, when it become active.
     // But actually terminal is not active here, only when we are replacing an abandoned terminal
     // with a dummy one (which will never become active)
@@ -93,8 +93,10 @@ void __fastcall TCustomUnixDriveView::SetTerminal(TTerminal * value)
         }
       }
     }
-    #endif
   }
+  #else
+  DebugUsedParam(value);
+  #endif
 }
 //---------------------------------------------------------------------------
 void __fastcall TCustomUnixDriveView::SetDirView(TUnixDirView * Value)

source/forms/CustomScpExplorer.cpp
@@ -612,7 +612,7 @@ void __fastcall TCustomScpExplorerForm::TerminalChanged(bool Replaced)
       }
     }
 
-    if (!Replaced)
+    if (!Replaced && Terminal->Active)
     {
       InitStatusBar();
     }
@@ -1675,7 +1675,7 @@ bool __fastcall TCustomScpExplorerForm::DirViewEnabled(TOperationSide Side)
 {
   DebugAssert(GetSide(Side) == osRemote);
   DebugUsedParam(Side);
-  return (Terminal != NULL);
+  return (Terminal != NULL) && Terminal->Active;
 }
 //---------------------------------------------------------------------------
 bool __fastcall TCustomScpExplorerForm::GetEnableFocusedOperation(
@@ -4837,6 +4837,30 @@ void __fastcall TCustomScpExplorerForm::CloseSession()
   }
 }
 //---------------------------------------------------------------------------
+void __fastcall TCustomScpExplorerForm::DisconnectSession()
+{
+  if (CanCloseQueue())
+  {
+    TManagedTerminal * ManagedTerminal = dynamic_cast<TManagedTerminal *>(Terminal);
+    TTerminalManager::Instance()->DisconnectActiveTerminal();
+  }
+}
+//---------------------------------------------------------------------------
+void __fastcall TCustomScpExplorerForm::TerminalDisconnected()
+{
+  RemoteDirView->Terminal = Terminal;
+  UpdateRemotePathComboBox(false);
+  UpdateControls();
+}
+//---------------------------------------------------------------------------
+void __fastcall TCustomScpExplorerForm::ReconnectSession()
+{
+  if (DebugAlwaysTrue(!Terminal->Active))
+  {
+    TTerminalManager::Instance()->ReconnectActiveTerminal();
+  }
+}
+//---------------------------------------------------------------------------
 void __fastcall TCustomScpExplorerForm::OpenStoredSession(TSessionData * Data)
 {
   if (OpenInNewWindow())
@@ -6544,6 +6568,7 @@ void __fastcall TCustomScpExplorerForm::DoTerminalListChanged()
       {
         TabSheet->ImageIndex = FNewSessionTabImageIndex;
         TabSheet->Tag = 0; // not really needed
+        TabSheet->Shadowed = false;
         // We know that we are at the last page, otherwise we could not call this (it assumes that new session tab is the last one)
         UpdateNewSessionTab();
       }
@@ -7001,6 +7026,10 @@ void __fastcall TCustomScpExplorerForm::ShowExtendedException(
 //---------------------------------------------------------------------------
 void __fastcall TCustomScpExplorerForm::TerminalReady()
 {
+  if (Terminal->Active)
+  {
+    InitStatusBar();
+  }
   // cannot rely on active page being page for active terminal,
   // as it can happen that active page is the "new session" page
   // (e.g. when reconnecting active terminal, while login dialog
@@ -8259,7 +8288,7 @@ UnicodeString __fastcall TCustomScpExplorerForm::FileStatusBarText(
   {
     Result = FormatIncrementalSearchStatus(FIncrementalSearch, FIncrementalSearchHaveNext);
   }
-  else if (!IsSideLocalBrowser(Side) && (Terminal == NULL))
+  else if (!IsSideLocalBrowser(Side) && ((Terminal == NULL) || (dynamic_cast<TManagedTerminal *>(Terminal)->Disconnected)))
   {
     // noop
   }
@@ -8414,15 +8443,24 @@ void __fastcall TCustomScpExplorerForm::UpdateControls()
   // See also EnableControl
   if (Showing)
   {
-    bool HasTerminal = (Terminal != NULL) && Terminal->Active;
-    if (HasTerminal)
+    if (Terminal != NULL)
     {
       // Update path when it changes
       if ((SessionsPageControl->ActivePage != NULL) && (GetSessionTabTerminal(SessionsPageControl->ActivePage) == Terminal))
       {
         UpdateSessionTab(SessionsPageControl->ActivePage);
       }
+    }
 
+    NonVisualDataModule->ReconnectSessionAction->Update();
+    ReconnectToolbar->Visible = NonVisualDataModule->ReconnectSessionAction->Visible;
+    // ReconnectSessionAction is hidden when disabled, so enabling it actualy resizes the toolbar
+    CenterReconnectToolbar();
+
+    bool HasTerminal = (Terminal != NULL) && Terminal->Active;
+
+    if (HasTerminal)
+    {
       // TODO needed yet with second local browser?
       if (!RemoteDirView->Enabled)
       {
@@ -8970,6 +9008,12 @@ void __fastcall TCustomScpExplorerForm::CMShowingChanged(TMessage & Message)
   }
 }
 //---------------------------------------------------------------------------
+void __fastcall TCustomScpExplorerForm::CenterReconnectToolbar()
+{
+  ReconnectToolbar->Left = (ReconnectToolbar->Parent->ClientWidth - ReconnectToolbar->Width) / 2;
+  ReconnectToolbar->Top = (ReconnectToolbar->Parent->ClientHeight - ReconnectToolbar->Height) / 2;
+}
+//---------------------------------------------------------------------------
 void __fastcall TCustomScpExplorerForm::FormConstrainedResize(
   TObject * /*Sender*/, int & MinWidth, int & MinHeight, int & MaxWidth,
   int & MaxHeight)
@@ -9309,7 +9353,7 @@ void __fastcall TCustomScpExplorerForm::UpdateRemotePathComboBox(bool TextOnly)
     try
     {
       Items->Clear();
-      if (Terminal != NULL)
+      if ((Terminal != NULL) && !RemoteDirView->Path.IsEmpty())
       {
         UnicodeString APath = UnixExcludeTrailingBackslash(RemoteDirView->Path);
         while (!IsUnixRootPath(APath))
@@ -10552,5 +10596,10 @@ void __fastcall TCustomScpExplorerForm::DirViewChangeFocus(TObject *, TListItem 
 void __fastcall TCustomScpExplorerForm::RemoteStatusBarMouseDown(TObject *, TMouseButton, TShiftState, int, int)
 {
   CountClicksForWindowPrint(this);
+}
+//---------------------------------------------------------------------------
+void __fastcall TCustomScpExplorerForm::RemoteDirViewResize(TObject *)
+{
+  CenterReconnectToolbar();
 }
 //---------------------------------------------------------------------------

commit f3d511f41a01dfff24ec438a92707eadef7b9033

When initial connection fails, Login dialog was not reopened (regression by ea24ee34)
Source commit: f01a899fbd642b29d64f67fb237f2d5433c9cdbf

source/windows/TerminalManager.cpp
@@ -416,17 +416,22 @@ bool __fastcall TTerminalManager::ConnectActiveTerminalImpl(bool Reopen)
 
   if (Action == tpFree)
   {
-    if (ActiveTerminal->Permanent)
-    {
-      DisconnectActiveTerminal();
-    }
-    else
-    {
-      FreeActiveTerminal();
-    }
+    DisconnectActiveTerminalIfPermanentFreeOtherwise();
   }
 
   return Result;
+}
+//---------------------------------------------------------------------------
+void __fastcall TTerminalManager::DisconnectActiveTerminalIfPermanentFreeOtherwise()
+{
+  if (ActiveTerminal->Permanent)
+  {
+    DisconnectActiveTerminal();
+  }
+  else
+  {
+    FreeActiveTerminal();
+  }
 }
 //---------------------------------------------------------------------------
 bool __fastcall TTerminalManager::ConnectActiveTerminal()

source/windows/TerminalManager.h
@@ -49,6 +49,7 @@ public:
   TManagedTerminal * __fastcall NewTerminals(TList * DataList);
   virtual void __fastcall FreeTerminal(TTerminal * Terminal);
   void __fastcall Move(TTerminal * Source, TTerminal * Target);
+  void __fastcall DisconnectActiveTerminalIfPermanentFreeOtherwise();
   void __fastcall DisconnectActiveTerminal();
   void __fastcall ReconnectActiveTerminal();
   void __fastcall FreeActiveTerminal();

source/windows/UserInterface.cpp
@@ -355,7 +355,7 @@ void __fastcall ShowExtendedExceptionEx(TTerminal * Terminal,
     {
       if (ForActiveTerminal)
       {
-        Manager->DisconnectActiveTerminal();
+        Manager->DisconnectActiveTerminalIfPermanentFreeOtherwise();
       }
     }
   }