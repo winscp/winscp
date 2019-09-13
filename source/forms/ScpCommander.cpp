winscp/winscp

commit f2873a7c27fe84fa680dd10541e0264ba9c3c5f6

Switch /defaults accepts an optional delay in seconds to start the operation
Source commit: 312b3a3d9921c60a25172d756ae72949f1182766

source/forms/ScpCommander.cpp
@@ -1007,7 +1007,7 @@ void __fastcall TScpCommanderForm::SynchronizeDirectories()
 {
   UnicodeString LocalDirectory = LocalDirView->PathName;
   UnicodeString RemoteDirectory = RemoteDirView->PathName;
-  DoSynchronizeDirectories(LocalDirectory, RemoteDirectory, false);
+  DoSynchronizeDirectories(LocalDirectory, RemoteDirectory, -1);
 }
 //---------------------------------------------------------------------------
 void __fastcall TScpCommanderForm::FullSynchronizeDirectories()

source/forms/ScpExplorer.cpp
@@ -227,7 +227,7 @@ void __fastcall TScpExplorerForm::SynchronizeDirectories()
 {
   UnicodeString LocalDirectory = WinConfiguration->ScpExplorer.LastLocalTargetDirectory;
   UnicodeString RemoteDirectory = RemoteDirView->PathName;
-  if (DoSynchronizeDirectories(LocalDirectory, RemoteDirectory, false))
+  if (DoSynchronizeDirectories(LocalDirectory, RemoteDirectory, -1))
   {
     WinConfiguration->ScpExplorer.LastLocalTargetDirectory = LocalDirectory;
   }

source/forms/Copy.cpp
@@ -21,10 +21,10 @@
 #pragma resource "*.dfm"
 #endif
 //---------------------------------------------------------------------------
-bool __fastcall DoCopyDialog(bool ToRemote,
-  bool Move, TStrings * FileList, UnicodeString & TargetDirectory,
+bool __fastcall DoCopyDialog(
+  bool ToRemote, bool Move, TStrings * FileList, UnicodeString & TargetDirectory,
   TGUICopyParamType * Params, int Options, int CopyParamAttrs, TSessionData * SessionData,
-  int * OutputOptions)
+  int * OutputOptions, int AutoSubmit)
 {
   bool Result;
   TCopyDialog *CopyDialog = new TCopyDialog(Application, ToRemote, Move, FileList, Options, CopyParamAttrs, SessionData);
@@ -42,6 +42,10 @@ bool __fastcall DoCopyDialog(bool ToRemote,
     }
     CopyDialog->Directory = TargetDirectory;
     CopyDialog->Params = *Params;
+    if (AutoSubmit > 0)
+    {
+      InitiateDialogTimeout(CopyDialog, AutoSubmit * MSecsPerSec, CopyDialog->OkButton);
+    }
     Result = CopyDialog->Execute();
     if (Result)
     {

source/forms/CustomScpExplorer.cpp
@@ -1125,7 +1125,7 @@ bool __fastcall TCustomScpExplorerForm::CopyParamDialog(
     std::unique_ptr<TSessionData> SessionData(SessionDataForCode());
     FlashOnBackground(); // Particularly when called from ClipboardFakeCreated
     Result = DoCopyDialog(Direction == tdToRemote, Type == ttMove,
-      FileList, TargetDirectory, &CopyParam, Options, CopyParamAttrs, SessionData.get(), &OutputOptions);
+      FileList, TargetDirectory, &CopyParam, Options, CopyParamAttrs, SessionData.get(), &OutputOptions, -1);
 
     if (Result)
     {
@@ -5267,7 +5267,7 @@ void __fastcall TCustomScpExplorerForm::SynchronizeDirectories()
 }
 //---------------------------------------------------------------------------
 bool __fastcall TCustomScpExplorerForm::DoSynchronizeDirectories(
-  UnicodeString & LocalDirectory, UnicodeString & RemoteDirectory, bool UseDefaults)
+  UnicodeString & LocalDirectory, UnicodeString & RemoteDirectory, int UseDefaults)
 {
   TSynchronizeParamType Params;
   Params.LocalDirectory = LocalDirectory;
@@ -5744,7 +5744,7 @@ void __fastcall TCustomScpExplorerForm::FullSynchronizeInNewWindow(
 //---------------------------------------------------------------------------
 int __fastcall TCustomScpExplorerForm::DoFullSynchronizeDirectories(
   UnicodeString & LocalDirectory, UnicodeString & RemoteDirectory,
-  TSynchronizeMode & Mode, int Params, bool & SaveMode, bool UseDefaults)
+  TSynchronizeMode & Mode, int Params, bool & SaveMode, int UseDefaults)
 {
   int Result;
 
@@ -5755,9 +5755,9 @@ int __fastcall TCustomScpExplorerForm::DoFullSynchronizeDirectories(
   TCopyParamType CopyParam = GUIConfiguration->CurrentCopyParam;
   TUsableCopyParamAttrs CopyParamAttrs = Terminal->UsableCopyParamAttrs(0);
   bool Continue =
-    UseDefaults ||
+    (UseDefaults == 0) ||
     DoFullSynchronizeDialog(Mode, Params, LocalDirectory, RemoteDirectory,
-      &CopyParam, SaveSettings, SaveMode, Options, CopyParamAttrs, FullSynchronizeInNewWindow);
+      &CopyParam, SaveSettings, SaveMode, Options, CopyParamAttrs, FullSynchronizeInNewWindow, UseDefaults);
   if (Continue)
   {
     Configuration->Usage->Inc(L"Synchronizations");

source/forms/CustomScpExplorer.h
@@ -774,11 +774,11 @@ public:
   void __fastcall TerminalReady();
   void __fastcall QueueEvent(TManagedTerminal * Terminal, TTerminalQueue * Queue, TQueueEvent Event);
   void __fastcall QueueEmptyNoteClicked(TObject * Sender);
-  bool __fastcall DoSynchronizeDirectories(UnicodeString & LocalDirectory,
-    UnicodeString & RemoteDirectory, bool UseDefaults);
-  int __fastcall DoFullSynchronizeDirectories(UnicodeString & LocalDirectory,
-    UnicodeString & RemoteDirectory, TSynchronizeMode & Mode, int Params, bool & SaveMode,
-    bool UseDefaults);
+  bool __fastcall DoSynchronizeDirectories(
+    UnicodeString & LocalDirectory, UnicodeString & RemoteDirectory, int UseDefaults);
+  int __fastcall DoFullSynchronizeDirectories(
+    UnicodeString & LocalDirectory, UnicodeString & RemoteDirectory, TSynchronizeMode & Mode,
+    int Params, bool & SaveMode, int UseDefaults);
   void __fastcall StandaloneEdit(const UnicodeString & FileName);
   bool __fastcall CanPasteFromClipBoard();
   virtual void __fastcall PasteFromClipBoard();

source/forms/FullSynchronize.cpp
@@ -27,7 +27,8 @@
 bool __fastcall DoFullSynchronizeDialog(TSynchronizeMode & Mode, int & Params,
   UnicodeString & LocalDirectory, UnicodeString & RemoteDirectory,
   TCopyParamType * CopyParams, bool & SaveSettings, bool & SaveMode, int Options,
-  const TUsableCopyParamAttrs & CopyParamAttrs, TFullSynchronizeInNewWindow OnFullSynchronizeInNewWindow)
+  const TUsableCopyParamAttrs & CopyParamAttrs, TFullSynchronizeInNewWindow OnFullSynchronizeInNewWindow,
+  int AutoSubmit)
 {
   bool Result;
   TFullSynchronizeDialog * Dialog = SafeFormCreate<TFullSynchronizeDialog>();
@@ -41,6 +42,10 @@ bool __fastcall DoFullSynchronizeDialog(TSynchronizeMode & Mode, int & Params,
     Dialog->CopyParams = *CopyParams;
     Dialog->SaveSettings = SaveSettings;
     Dialog->SaveMode = SaveMode;
+    if (AutoSubmit > 0)
+    {
+      InitiateDialogTimeout(Dialog, AutoSubmit * MSecsPerSec, Dialog->OkButton);
+    }
     Result = Dialog->Execute();
     if (Result)
     {

source/forms/Synchronize.cpp
@@ -35,12 +35,12 @@ bool __fastcall DoSynchronizeDialog(TSynchronizeParamType & Params,
   TSynchronizeSessionLog OnSynchronizeSessionLog,
   TFeedSynchronizeError & OnFeedSynchronizeError,
   TSynchronizeInNewWindow OnSynchronizeInNewWindow,
-  bool Start)
+  int AutoSubmit)
 {
   bool Result;
   TSynchronizeDialog * Dialog = SafeFormCreate<TSynchronizeDialog>(Application);
 
-  Dialog->Init(OnStartStop, OnGetOptions, OnSynchronizeSessionLog, OnFeedSynchronizeError, OnSynchronizeInNewWindow, Start);
+  Dialog->Init(OnStartStop, OnGetOptions, OnSynchronizeSessionLog, OnFeedSynchronizeError, OnSynchronizeInNewWindow, AutoSubmit);
 
   try
   {
@@ -97,7 +97,7 @@ void __fastcall TSynchronizeDialog::Init(TSynchronizeStartStopEvent OnStartStop,
   TSynchronizeSessionLog OnSynchronizeSessionLog,
   TFeedSynchronizeError & OnFeedSynchronizeError,
   TSynchronizeInNewWindow OnSynchronizeInNewWindow,
-  bool StartImmediately)
+  int AutoSubmit)
 {
   FOnStartStop = OnStartStop;
   FOnGetOptions = OnGetOptions;
@@ -105,7 +105,19 @@ void __fastcall TSynchronizeDialog::Init(TSynchronizeStartStopEvent OnStartStop,
   FOnFeedSynchronizeError = &OnFeedSynchronizeError;
   DebugAssert(OnSynchronizeInNewWindow != NULL);
   FOnSynchronizeInNewWindow = OnSynchronizeInNewWindow;
-  FStartImmediately = StartImmediately;
+  if (AutoSubmit == 0)
+  {
+    FStartImmediately = true;
+  }
+  else
+  {
+    FStartImmediately = false;
+
+    if (AutoSubmit > 0)
+    {
+      InitiateDialogTimeout(this, AutoSubmit * MSecsPerSec, StartButton);
+    }
+  }
 }
 //---------------------------------------------------------------------------
 __fastcall TSynchronizeDialog::~TSynchronizeDialog()

source/forms/Synchronize.h
@@ -142,7 +142,7 @@ public:
     TGetSynchronizeOptionsEvent OnGetOptions,
     TSynchronizeSessionLog OnSynchronizeSessionLog,
     TFeedSynchronizeError & OnFeedSynchronizeError, TSynchronizeInNewWindow OnSynchronizeInNewWindow,
-    bool StartImmediately);
+    int AutoSubmit);
   virtual __fastcall ~TSynchronizeDialog();
 
   bool __fastcall Execute();

commit 381f396d0fa61aa6f581e22bc5e58b487054c2d2

Synchronization dialog was not shown when a synchronization was started from the main window (caused by f2873a7c)
Source commit: e1cc9da90f3753c6e0d3289b3a4c51e9d3cb42fc

source/forms/ScpCommander.cpp
@@ -1020,7 +1020,7 @@ void __fastcall TScpCommanderForm::FullSynchronizeDirectories()
     (SaveMode ? (TSynchronizeMode)GUIConfiguration->SynchronizeModeAuto :
       ((FCurrentSide == osLocal) ? smRemote : smLocal));
   int Params = GUIConfiguration->SynchronizeParams;
-  if (DoFullSynchronizeDirectories(LocalDirectory, RemoteDirectory, Mode, Params, SaveMode, false) >= 0)
+  if (DoFullSynchronizeDirectories(LocalDirectory, RemoteDirectory, Mode, Params, SaveMode, -1) >= 0)
   {
     if (SaveMode)
     {

source/forms/ScpExplorer.cpp
@@ -240,7 +240,7 @@ void __fastcall TScpExplorerForm::FullSynchronizeDirectories()
   bool SaveMode = true;
   TSynchronizeMode Mode = (TSynchronizeMode)GUIConfiguration->SynchronizeMode;
   int Params = GUIConfiguration->SynchronizeParams;
-  if (DoFullSynchronizeDirectories(LocalDirectory, RemoteDirectory, Mode, Params, SaveMode, false) >= 0)
+  if (DoFullSynchronizeDirectories(LocalDirectory, RemoteDirectory, Mode, Params, SaveMode, -1) >= 0)
   {
     WinConfiguration->ScpExplorer.LastLocalTargetDirectory = LocalDirectory;
     if (SaveMode)