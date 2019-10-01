winscp/winscp

commit a62f0274e4590ef73b2bd43492923d8fda60aad8

Estimating time to finish directory comparison before synchronization based on previous comparisons of the same folders
Source commit: 01df1bed1a6df6d014df2151b6c9c08d3d860eb8

source/forms/CustomScpExplorer.cpp
@@ -850,28 +850,35 @@ void __fastcall TCustomScpExplorerForm::SetTaskbarListProgressState(TBPFLAG Flag
   FTaskbarList->SetProgressState(GetMainForm()->Handle, Flags);
 }
 //---------------------------------------------------------------------------
-void __fastcall TCustomScpExplorerForm::SetTaskbarListProgressValue(TFileOperationProgressType * ProgressData)
+void __fastcall TCustomScpExplorerForm::SetTaskbarListProgressValue(int Progress)
 {
-  if (!TFileOperationProgressType::IsIndeterminateOperation(ProgressData->Operation))
+  if (Progress >= 0)
   {
-    int OverallProgress;
-    // FProgressForm is null when this is called from SetQueueProgress
-    if ((FProgressForm != NULL) && (FProgressForm->SynchronizeProgress != NULL))
-    {
-      OverallProgress = FProgressForm->SynchronizeProgress->Progress(ProgressData);
-    }
-    else
-    {
-      OverallProgress = ProgressData->OverallProgress();
-    }
-
-    // implies TBPF_NORMAL
-    FTaskbarList->SetProgressValue(GetMainForm()->Handle, OverallProgress, 100);
+    FTaskbarList->SetProgressValue(GetMainForm()->Handle, Progress, 100);
   }
   else
   {
     SetTaskbarListProgressState(TBPF_INDETERMINATE);
   }
+}
+//---------------------------------------------------------------------------
+void __fastcall TCustomScpExplorerForm::SetTaskbarListProgressValue(TFileOperationProgressType * ProgressData)
+{
+  int OverallProgress;
+  // FProgressForm is null when this is called from SetQueueProgress
+  if ((FProgressForm != NULL) && (FProgressForm->SynchronizeProgress != NULL))
+  {
+    OverallProgress = FProgressForm->SynchronizeProgress->Progress(ProgressData);
+  }
+  else if (!TFileOperationProgressType::IsIndeterminateOperation(ProgressData->Operation))
+  {
+    OverallProgress = ProgressData->OverallProgress();
+  }
+  else
+  {
+    OverallProgress = -1;
+  }
+  SetTaskbarListProgressValue(OverallProgress);
 }
 //---------------------------------------------------------------------------
 void __fastcall TCustomScpExplorerForm::SetQueueProgress()
@@ -5417,7 +5424,7 @@ void __fastcall TCustomScpExplorerForm::Synchronize(const UnicodeString LocalDir
   TSynchronizeChecklist * AChecklist = NULL;
   try
   {
-    FSynchronizeProgressForm = new TSynchronizeProgressForm(Application, true);
+    FSynchronizeProgressForm = new TSynchronizeProgressForm(Application, true, -1);
     if (FLAGCLEAR(Params, TTerminal::spDelayProgress))
     {
       FSynchronizeProgressForm->Start();
@@ -5771,13 +5778,32 @@ int __fastcall TCustomScpExplorerForm::DoFullSynchronizeDirectories(
 
       try
       {
-        FSynchronizeProgressForm = new TSynchronizeProgressForm(Application, true);
+        UnicodeString SessionKey = Terminal->SessionData->SessionKey;
+        std::unique_ptr<TStrings> DataList(Configuration->LoadDirectoryStatisticsCache(SessionKey, RemoteDirectory, CopyParam));
+
+        int Files = -1;
+        if (DataList->Count >= 1)
+        {
+          Files = StrToIntDef(DataList->Strings[0], Files);
+        }
+        else
+        {
+          DataList->Add(UnicodeString());
+        }
+
+        FSynchronizeProgressForm = new TSynchronizeProgressForm(Application, true, Files);
         FSynchronizeProgressForm->Start();
 
         Checklist = Terminal->SynchronizeCollect(LocalDirectory, RemoteDirectory,
           static_cast<TTerminal::TSynchronizeMode>(Mode),
           &CopyParam, Params | TTerminal::spNoConfirmation, TerminalSynchronizeDirectory,
           &SynchronizeOptions);
+
+        if (Terminal->SessionData->CacheDirectories)
+        {
+          DataList->Strings[0] = IntToStr(SynchronizeOptions.Files);
+          Configuration->SaveDirectoryStatisticsCache(SessionKey, RemoteDirectory, CopyParam, DataList.get());
+        }
       }
       __finally
       {
@@ -5834,8 +5860,8 @@ int __fastcall TCustomScpExplorerForm::DoFullSynchronizeDirectories(
 }
 //---------------------------------------------------------------------------
 void __fastcall TCustomScpExplorerForm::TerminalSynchronizeDirectory(
-  const UnicodeString LocalDirectory, const UnicodeString RemoteDirectory,
-  bool & Continue, bool Collect)
+  const UnicodeString & LocalDirectory, const UnicodeString & RemoteDirectory,
+  bool & Continue, bool Collect, const TSynchronizeOptions * Options)
 {
   if (Collect)
   {
@@ -5844,7 +5870,8 @@ void __fastcall TCustomScpExplorerForm::TerminalSynchronizeDirectory(
     {
       FSynchronizeProgressForm->Start();
     }
-    FSynchronizeProgressForm->SetData(LocalDirectory, RemoteDirectory, Continue);
+    int CompareProgress = FSynchronizeProgressForm->SetData(LocalDirectory, RemoteDirectory, Options->Files, Continue);
+    SetTaskbarListProgressValue(CompareProgress);
   }
   else
   {

source/core/Configuration.cpp
@@ -13,6 +13,7 @@
 #include "CoreMain.h"
 #include "Security.h"
 #include "FileMasks.h"
+#include "CopyParam.h"
 #include <shlobj.h>
 #include <System.IOUtils.hpp>
 #include <System.StrUtils.hpp>
@@ -1441,6 +1442,64 @@ void __fastcall TConfiguration::SetRandomSeedFile(UnicodeString value)
       // ignore any error
       DeleteFile(ApiPath(PrevRandomSeedFileName));
     }
+  }
+}
+//---------------------------------------------------------------------------
+UnicodeString __fastcall TConfiguration::GetDirectoryStatisticsCacheKey(
+  const UnicodeString & SessionKey, const UnicodeString & Path, const TCopyParamType & CopyParam)
+{
+  std::unique_ptr<TStringList> RawOptions(new TStringList());
+  RawOptions->Add(SessionKey);
+  RawOptions->Add(UnixExcludeTrailingBackslash(Path));
+
+  TCopyParamType Defaults;
+  TCopyParamType FilterCopyParam;
+  FilterCopyParam.IncludeFileMask = CopyParam.IncludeFileMask;
+  FilterCopyParam.ExcludeHiddenFiles = CopyParam.ExcludeHiddenFiles;
+  FilterCopyParam.ExcludeEmptyDirectories = CopyParam.ExcludeEmptyDirectories;
+
+  std::unique_ptr<TOptionsStorage> OptionsStorage(new TOptionsStorage(RawOptions.get(), true));
+  FilterCopyParam.Save(OptionsStorage.get(), &Defaults);
+
+  UTF8String RawOptionsBuf(RawOptions->CommaText.LowerCase());
+  UnicodeString Result = Sha256(RawOptionsBuf.c_str(), RawOptionsBuf.Length());
+  return Result;
+}
+//---------------------------------------------------------------------------
+THierarchicalStorage * TConfiguration::OpenDirectoryStatisticsCache(bool CanCreate)
+{
+  std::unique_ptr<THierarchicalStorage> Storage(Configuration->CreateConfigStorage());
+  Storage->AccessMode = CanCreate ? smReadWrite : smRead;
+  if (!Storage->OpenSubKey(L"DirectoryStatisticsCache", CanCreate))
+  {
+    Storage.reset(NULL);
+  }
+  return Storage.release();
+}
+//---------------------------------------------------------------------------
+TStrings * __fastcall TConfiguration::LoadDirectoryStatisticsCache(
+  const UnicodeString & SessionKey, const UnicodeString & Path, const TCopyParamType & CopyParam)
+{
+  std::unique_ptr<THierarchicalStorage> Storage(OpenDirectoryStatisticsCache(false));
+  std::unique_ptr<TStringList> Result(new TStringList());
+  if (Storage.get() != NULL)
+  {
+    UnicodeString Key = GetDirectoryStatisticsCacheKey(SessionKey, Path, CopyParam);
+    UnicodeString Buf = Storage->ReadString(Key, UnicodeString());
+    Result->CommaText = Buf;
+  }
+  return Result.release();
+}
+//---------------------------------------------------------------------------
+void __fastcall TConfiguration::SaveDirectoryStatisticsCache(
+  const UnicodeString & SessionKey, const UnicodeString & Path, const TCopyParamType & CopyParam, TStrings * DataList)
+{
+  std::unique_ptr<THierarchicalStorage> Storage(OpenDirectoryStatisticsCache(true));
+  if (Storage.get() != NULL)
+  {
+    UnicodeString Key = GetDirectoryStatisticsCacheKey(SessionKey, Path, CopyParam);
+    UnicodeString Buf = DataList->CommaText;
+    Storage->WriteString(Key, Buf);
   }
 }
 //---------------------------------------------------------------------

source/core/Configuration.h
@@ -18,6 +18,7 @@ extern const wchar_t * NotAutoSwitchNames;
 enum TAutoSwitch { asOn, asOff, asAuto }; // Has to match PuTTY FORCE_ON, FORCE_OFF, AUTO
 //---------------------------------------------------------------------------
 class TStoredSessionList;
+class TCopyParamType;
 //---------------------------------------------------------------------------
 class TConfiguration : public TObject
 {
@@ -169,6 +170,9 @@ protected:
   static UnicodeString __fastcall PropertyToKey(const UnicodeString & Property);
   virtual void __fastcall DoSave(bool All, bool Explicit);
   UnicodeString __fastcall FormatFingerprintKey(const UnicodeString & SiteKey, const UnicodeString & FingerprintType);
+  THierarchicalStorage * OpenDirectoryStatisticsCache(bool CanCreate);
+  UnicodeString __fastcall GetDirectoryStatisticsCacheKey(
+    const UnicodeString & SessionKey, const UnicodeString & Path, const TCopyParamType & CopyParam);
 
   virtual bool __fastcall GetConfirmOverwriting();
   virtual void __fastcall SetConfirmOverwriting(bool value);
@@ -227,6 +231,10 @@ public:
     TRemoteDirectoryChangesCache * DirectoryChangesCache);
   void __fastcall SaveDirectoryChangesCache(const UnicodeString SessionKey,
     TRemoteDirectoryChangesCache * DirectoryChangesCache);
+  TStrings * __fastcall LoadDirectoryStatisticsCache(
+    const UnicodeString & SessionKey, const UnicodeString & Path, const TCopyParamType & CopyParam);
+  void __fastcall SaveDirectoryStatisticsCache(
+    const UnicodeString & SessionKey, const UnicodeString & Path, const TCopyParamType & CopyParam, TStrings * DataList);
   bool __fastcall ShowBanner(const UnicodeString & SessionKey, const UnicodeString & Banner, unsigned int & Params);
   void __fastcall NeverShowBanner(const UnicodeString & SessionKey, const UnicodeString & Banner);
   void __fastcall SetBannerParams(const UnicodeString & SessionKey, unsigned int Params);

source/core/Script.cpp
@@ -2394,8 +2394,8 @@ void __fastcall TManagementScript::TerminalOperationFinished(
 }
 //---------------------------------------------------------------------------
 void __fastcall TManagementScript::TerminalSynchronizeDirectory(
-  const UnicodeString LocalDirectory, const UnicodeString RemoteDirectory,
-  bool & Continue, bool Collect)
+  const UnicodeString & LocalDirectory, const UnicodeString & RemoteDirectory,
+  bool & Continue, bool Collect, const TSynchronizeOptions *)
 {
   int SynchronizeMode = FSynchronizeMode;
   if (FKeepingUpToDate)

source/core/Script.h
@@ -236,8 +236,9 @@ protected:
   void __fastcall FreeTerminal(TTerminal * Terminal);
   void __fastcall PrintProgress(bool First, const UnicodeString Str);
   bool __fastcall QueryCancel();
-  void __fastcall TerminalSynchronizeDirectory(const UnicodeString LocalDirectory,
-    const UnicodeString RemoteDirectory, bool & Continue, bool Collect);
+  void __fastcall TerminalSynchronizeDirectory(
+    const UnicodeString & LocalDirectory, const UnicodeString & RemoteDirectory,
+    bool & Continue, bool Collect, const TSynchronizeOptions * Options);
   void __fastcall DoChangeLocalDirectory(UnicodeString Directory);
   void __fastcall DoClose(TTerminal * Terminal);
   virtual bool __fastcall HandleExtendedException(Exception * E,

source/core/Terminal.cpp
@@ -5760,6 +5760,11 @@ void __fastcall TTerminal::DoSynchronizeCollectFile(const UnicodeString FileName
 {
   TSynchronizeData * Data = static_cast<TSynchronizeData *>(Param);
 
+  if (Data->Options != NULL)
+  {
+    Data->Options->Files++;
+  }
+
   UnicodeString LocalFileName = ChangeFileName(Data->CopyParam, File->FileName, osRemote, false);
   UnicodeString FullRemoteFileName = UnixExcludeTrailingBackslash(File->FullFileName);
   if (DoAllowRemoteFileTransfer(File, Data->CopyParam, true) &&
@@ -6211,8 +6216,8 @@ void __fastcall TTerminal::DoSynchronizeProgress(const TSynchronizeData & Data,
   if (Data.OnSynchronizeDirectory != NULL)
   {
     bool Continue = true;
-    Data.OnSynchronizeDirectory(Data.LocalDirectory, Data.RemoteDirectory,
-      Continue, Collect);
+    Data.OnSynchronizeDirectory(
+      Data.LocalDirectory, Data.RemoteDirectory, Continue, Collect, Data.Options);
 
     if (!Continue)
     {

source/core/Terminal.h
@@ -53,8 +53,8 @@ typedef void __fastcall (__closure *TProcessFileEventEx)
 typedef int __fastcall (__closure *TFileOperationEvent)
   (void * Param1, void * Param2);
 typedef void __fastcall (__closure *TSynchronizeDirectory)
-  (const UnicodeString LocalDirectory, const UnicodeString RemoteDirectory,
-   bool & Continue, bool Collect);
+  (const UnicodeString & LocalDirectory, const UnicodeString & RemoteDirectory,
+   bool & Continue, bool Collect, const TSynchronizeOptions * Options);
 typedef void __fastcall (__closure *TUpdatedSynchronizationChecklistItems)(
   const TSynchronizeChecklist::TItemList & Items);
 typedef void __fastcall (__closure *TProcessedSynchronizationChecklistItem)(
@@ -763,6 +763,7 @@ struct TSynchronizeOptions
   ~TSynchronizeOptions();
 
   TStringList * Filter;
+  int Files;
 
   bool __fastcall FilterFind(const UnicodeString & FileName);
   bool __fastcall MatchesFilter(const UnicodeString & FileName);

source/forms/CustomScpExplorer.h
@@ -438,8 +438,9 @@ protected:
   DYNAMIC void __fastcall DoShow();
   TStrings * __fastcall CreateVisitedDirectories(TOperationSide Side);
   void __fastcall HandleErrorList(TStringList *& ErrorList);
-  void __fastcall TerminalSynchronizeDirectory(const UnicodeString LocalDirectory,
-    const UnicodeString RemoteDirectory, bool & Continue, bool Collect);
+  void __fastcall TerminalSynchronizeDirectory(
+    const UnicodeString & LocalDirectory, const UnicodeString & RemoteDirectory,
+    bool & Continue, bool Collect, const TSynchronizeOptions * Options);
   void __fastcall DoSynchronize(TSynchronizeController * Sender,
     const UnicodeString LocalDirectory, const UnicodeString RemoteDirectory,
     const TCopyParamType & CopyParam, const TSynchronizeParamType & Params,
@@ -623,6 +624,7 @@ protected:
   void __fastcall SetQueueProgress();
   void __fastcall UpdateQueueLabel();
   void __fastcall SetTaskbarListProgressState(TBPFLAG Flags);
+  void __fastcall SetTaskbarListProgressValue(int Progress);
   void __fastcall SetTaskbarListProgressValue(TFileOperationProgressType * ProgressData);
   TTerminal * __fastcall GetSessionTabTerminal(TTabSheet * TabSheet);
   bool __fastcall SessionTabSwitched();

commit c35c15b43c2c11adbfcd4ec6d9351671e452439d

Bug fix: After directory comparison before synchronization finishes, task bar button still shows a progress bar (caused by a62f0274)
Source commit: 1d1026970136204873f0c3188fb502162837edc4

source/forms/CustomScpExplorer.cpp
@@ -1414,6 +1414,7 @@ void __fastcall TCustomScpExplorerForm::CreateProgressForm(TSynchronizeProgress 
   if (FTaskbarList != NULL)
   {
     // Actually, do not know what hides the progress once the operation finishes
+    // (it is possibly the SetQueueProgress - and we should not rely on that)
     SetTaskbarListProgressState(TBPF_NORMAL);
   }
 }
@@ -5449,6 +5450,7 @@ void __fastcall TCustomScpExplorerForm::Synchronize(const UnicodeString LocalDir
       &CopyParam, Params | TTerminal::spNoConfirmation, TerminalSynchronizeDirectory,
       Options);
 
+    SetTaskbarListProgressState(TBPF_NOPROGRESS);
     SAFE_DESTROY(FSynchronizeProgressForm);
 
     AnyOperation = (AChecklist->CheckedCount > 0);
@@ -5851,6 +5853,7 @@ int __fastcall TCustomScpExplorerForm::DoFullSynchronizeDirectories(
       __finally
       {
         FAutoOperation = false;
+        SetTaskbarListProgressState(TBPF_NOPROGRESS);
         SAFE_DESTROY(FSynchronizeProgressForm);
       }
 