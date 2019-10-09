9835a892d282a9cf23499ddffcfa18c861da3d17
Martin Prikryl <martin@winscp.net>
2019-10-09 13:24:46 +0000

    Redundant code
    Source commit: 9fd31f5f134ec0bf934cc9b352138f5eb933785d

--- source/putty/windows/winstore.c
+++ source/putty/windows/winstore.c
@@ -10,7 +10,6 @@
 #pragma option push -w-dup
 #define RegOpenKey reg_open_winscp_key
 #define RegCreateKey reg_create_winscp_key
-#define RegCreateKey reg_create_winscp_key
 #define RegQueryValueEx reg_query_winscp_value_ex
 #define RegSetValueEx reg_set_winscp_value_ex
 #define RegCloseKey reg_close_winscp_key
