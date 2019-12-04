7e083c0fd9690076e921b855a0bc33b7d50f64dc
Martin Prikryl <martin@winscp.net>
2019-12-04 16:21:46 +0000

    Bumping devel version to 5.16.5
    Source commit: a96f5f7581012e70feae35686aba8eb11095d947

--- dotnet/properties/AssemblyInfo.cs
+++ dotnet/properties/AssemblyInfo.cs
@@ -36,6 +36,6 @@ namespace WinSCP
         public const string UndefinedProductVersion = "9.9.9.9";
 
         public const string Version = "1.7.1.0";
-        public const string ProductVersion = "5.16.4.0";
+        public const string ProductVersion = "5.16.5.0";
     }
 }
