a9fd620201650ac7db402d6b76de60045160c689
Martin Prikryl <martin@winscp.net>
2020-04-03 14:20:12 +0000

    Ignoring even EExternalException after too big file is loaded into the internal editor
    (p=98185)
    
    Source commit: 2b45ee0092438f60ec72fbac1bf7a6aa74ef53ad

--- source/forms/Editor.cpp
+++ source/forms/Editor.cpp
@@ -1422,6 +1422,7 @@ void __fastcall TEditorForm::CheckFileSize()
       // Those are actually nearly all internal exceptions we ever practically get
       IgnoreException(typeid(EOutOfMemory));
       IgnoreException(typeid(EAccessViolation));
+      IgnoreException(typeid(EExternalException));
     }
   }
 }
