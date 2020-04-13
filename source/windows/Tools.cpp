534ba37578fb559e555291125e29d9c9cf4e94d6
Martin Prikryl <martin@winscp.net>
2020-04-13 16:29:20 +0000

    Link update in comments
    Source commit: 3ac69c841fa0f96f21aa566bd7a684d8b244951e

--- source/windows/Tools.cpp
+++ source/windows/Tools.cpp
@@ -110,7 +110,7 @@ TColor __fastcall GetWindowTextColor(TColor BackgroundColor, TColor Color)
 {
   if (Color == TColor(0))
   {
-    // Could use current theme TMT_TEXTCOLOR - see https://github.com/ysc3839/win32-darkmode/
+    // Could use current theme TMT_TEXTCOLOR - see https://github.com/ysc3839/win32-darkmode
     Color = (IsDarkColor(BackgroundColor) ? clWhite : clWindowText);
     SetContrast(Color, BackgroundColor, 180);
   }
@@ -121,7 +121,7 @@ TColor __fastcall GetWindowColor(TColor Color)
 {
   if (Color == TColor(0))
   {
-    // Could use current theme TMT_FILLCOLOR - see https://github.com/ysc3839/win32-darkmode/
+    // Could use current theme TMT_FILLCOLOR - see https://github.com/ysc3839/win32-darkmode
     Color = (WinConfiguration->UseDarkTheme() ? static_cast<TColor>(RGB(0x20, 0x20, 0x20)) : clWindow);
   }
   return Color;
