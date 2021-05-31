winscp/winscp

commit 5dfcf1eb9f16119f6dbc13a0ea4a3f69f0433ae2

Removing support for SSH-1
Source commit: 75607516cc12c02235795c32cd3f9e67d920bade

source/core/PuttyIntf.cpp
@@ -956,11 +956,6 @@ UnicodeString __fastcall ParseOpenSshPubLine(const UnicodeString & Line, const s
   return Result;
 }
 //---------------------------------------------------------------------------
-UnicodeString __fastcall GetSsh1KeyType()
-{
-  return UnicodeString(ssh_rsa.cache_id);
-}
-//---------------------------------------------------------------------------
 UnicodeString __fastcall GetKeyTypeHuman(const UnicodeString & KeyType)
 {
   UnicodeString Result;

source/core/PuttyTools.h
@@ -37,7 +37,6 @@ void __fastcall DllHijackingProtection();
 UnicodeString __fastcall ParseOpenSshPubLine(const UnicodeString & Line, const struct ssh_keyalg *& Algorithm);
 //---------------------------------------------------------------------------
 UnicodeString __fastcall GetKeyTypeHuman(const UnicodeString & KeyType);
-UnicodeString __fastcall GetSsh1KeyType();
 //---------------------------------------------------------------------------
 bool IsOpenSSH(const UnicodeString & SshImplementation);
 //---------------------------------------------------------------------------

source/windows/Tools.cpp
@@ -1285,8 +1285,7 @@ static void __fastcall ConvertKey(UnicodeString & FileName, TKeyType Type)
   }
 }
 //---------------------------------------------------------------------------
-static void __fastcall DoVerifyKey(
-  UnicodeString & FileName, TSshProt SshProt, bool Convert, bool CanIgnore)
+static void __fastcall DoVerifyKey(UnicodeString & FileName, bool Convert, bool CanIgnore)
 {
   if (!FileName.Trim().IsEmpty())
   {
@@ -1330,14 +1329,7 @@ static void __fastcall DoVerifyKey(
         break;
 
       case ktSSH1:
-      case ktSSH2:
-        if ((Type == ktSSH1) != (SshProt == ssh1only))
-        {
-          Message =
-            MainInstructions(
-              FMTLOAD(KEY_TYPE_DIFFERENT_SSH,
-                (FileName, (Type == ktSSH1 ? L"SSH-1" : L"PuTTY SSH-2"))));
-        }
+        Message = MainInstructions(LoadStr(KEY_TYPE_SSH1));
         break;
 
       case ktSSH1Public:
@@ -1375,14 +1367,15 @@ static void __fastcall DoVerifyKey(
   }
 }
 //---------------------------------------------------------------------------
-void __fastcall VerifyAndConvertKey(UnicodeString & FileName, TSshProt SshProt, bool CanIgnore)
+void __fastcall VerifyAndConvertKey(UnicodeString & FileName, bool CanIgnore)
 {
-  DoVerifyKey(FileName, SshProt, true, CanIgnore);
+  DoVerifyKey(FileName, true, CanIgnore);
 }
 //---------------------------------------------------------------------------
-void __fastcall VerifyKey(UnicodeString FileName, TSshProt SshProt)
+void __fastcall VerifyKey(const UnicodeString & FileName)
 {
-  DoVerifyKey(FileName, SshProt, false, true);
+  UnicodeString AFileName(FileName);
+  DoVerifyKey(AFileName, false, true);
 }
 //---------------------------------------------------------------------------
 void __fastcall VerifyCertificate(const UnicodeString & FileName)

source/Putty.cbproj
@@ -170,21 +170,6 @@
 			<BuildOrder>23</BuildOrder>
 			<BuildOrder>19</BuildOrder>
 		</CppCompile>
-		<CppCompile Include="putty\ssh1bpp.c">
-			<BuildOrder>53</BuildOrder>
-		</CppCompile>
-		<CppCompile Include="putty\ssh1censor.c">
-			<BuildOrder>57</BuildOrder>
-		</CppCompile>
-		<CppCompile Include="putty\ssh1connection.c">
-			<BuildOrder>64</BuildOrder>
-		</CppCompile>
-		<CppCompile Include="putty\ssh1connection-client.c">
-			<BuildOrder>71</BuildOrder>
-		</CppCompile>
-		<CppCompile Include="putty\ssh1login.c">
-			<BuildOrder>65</BuildOrder>
-		</CppCompile>
 		<CppCompile Include="putty\ssh2bpp.c">
 			<BuildOrder>54</BuildOrder>
 		</CppCompile>
@@ -244,14 +229,6 @@
 		</CppCompile>
 		<CppCompile Include="putty\sshcommon.c">
 			<BuildOrder>62</BuildOrder>
-		</CppCompile>
-		<CppCompile Include="putty\sshcrc.c">
-			<BuildOrder>38</BuildOrder>
-			<BuildOrder>36</BuildOrder>
-		</CppCompile>
-		<CppCompile Include="putty\sshcrcda.c">
-			<BuildOrder>41</BuildOrder>
-			<BuildOrder>35</BuildOrder>
 		</CppCompile>
 		<CppCompile Include="putty\sshdes.c">
 			<BuildOrder>44</BuildOrder>

source/core/SecureShell.cpp
@@ -123,27 +123,16 @@ inline void __fastcall TSecureShell::UpdateSessionInfo()
 {
   if (!FSessionInfoValid)
   {
-    FSshVersion = get_ssh_version(FBackendHandle);
+    DebugAssert(get_ssh_version(FBackendHandle) == 2);
     FSessionInfo.ProtocolBaseName = L"SSH";
     FSessionInfo.ProtocolName =
       FORMAT(L"%s-%d", (FSessionInfo.ProtocolBaseName, get_ssh_version(FBackendHandle)));
     FSessionInfo.SecurityProtocolName = FSessionInfo.ProtocolName;
 
-    if (FSshVersion == 1)
-    {
-      FSessionInfo.CSCipher = GetCipherName(get_cipher(FBackendHandle));
-      FSessionInfo.SCCipher = FSessionInfo.CSCipher;
-      // Retrieval of compression is not implemented for SSH-1
-      FSessionInfo.CSCompression = UnicodeString();
-      FSessionInfo.SCCompression = UnicodeString();
-    }
-    else
-    {
-      FSessionInfo.CSCipher = GetCipherName(get_cscipher(FBackendHandle));
-      FSessionInfo.SCCipher = GetCipherName(get_sccipher(FBackendHandle));
-      FSessionInfo.CSCompression = GetCompressorName(get_cscomp(FBackendHandle));
-      FSessionInfo.SCCompression = GetDecompressorName(get_sccomp(FBackendHandle));
-    }
+    FSessionInfo.CSCipher = GetCipherName(get_cscipher(FBackendHandle));
+    FSessionInfo.SCCipher = GetCipherName(get_sccipher(FBackendHandle));
+    FSessionInfo.CSCompression = GetCompressorName(get_cscomp(FBackendHandle));
+    FSessionInfo.SCCompression = GetDecompressorName(get_sccomp(FBackendHandle));
 
     FSessionInfoValid = true;
   }
@@ -259,10 +248,8 @@ Conf * __fastcall TSecureShell::StoreToConfig(TSessionData * Data, bool Simple)
   conf_set_filename(conf, CONF_keyfile, KeyFileFileName);
   filename_free(KeyFileFileName);
 
-  conf_set_int(conf, CONF_sshprot, Data->SshProt);
   conf_set_bool(conf, CONF_ssh2_des_cbc, Data->Ssh2DES);
   conf_set_bool(conf, CONF_ssh_no_userauth, Data->SshNoUserAuth);
-  conf_set_bool(conf, CONF_try_tis_auth, Data->AuthTIS);
   conf_set_bool(conf, CONF_try_ki_auth, Data->AuthKI);
   conf_set_bool(conf, CONF_try_gssapi_auth, Data->AuthGSSAPI);
   conf_set_bool(conf, CONF_try_gssapi_kex, Data->AuthGSSAPIKEX);
@@ -285,9 +272,6 @@ Conf * __fastcall TSecureShell::StoreToConfig(TSessionData * Data, bool Simple)
   conf_set_int(conf, CONF_proxy_dns, Data->ProxyDNS);
   conf_set_bool(conf, CONF_even_proxy_localhost, Data->ProxyLocalhost);
 
-  conf_set_int(conf, CONF_sshbug_ignore1, Data->Bug[sbIgnore1]);
-  conf_set_int(conf, CONF_sshbug_plainpw1, Data->Bug[sbPlainPW1]);
-  conf_set_int(conf, CONF_sshbug_rsa1, Data->Bug[sbRSA1]);
   conf_set_int(conf, CONF_sshbug_hmac2, Data->Bug[sbHMAC2]);
   conf_set_int(conf, CONF_sshbug_derivekey2, Data->Bug[sbDeriveKey2]);
   conf_set_int(conf, CONF_sshbug_rsapad2, Data->Bug[sbRSAPad2]);
@@ -2176,14 +2160,7 @@ unsigned long __fastcall TSecureShell::MaxPacketSize()
     UpdateSessionInfo();
   }
 
-  if (FSshVersion == 1)
-  {
-    return 0;
-  }
-  else
-  {
-    return winscp_query(FBackendHandle, WINSCP_QUERY_REMMAXPKT);
-  }
+  return winscp_query(FBackendHandle, WINSCP_QUERY_REMMAXPKT);
 }
 //---------------------------------------------------------------------------
 UnicodeString __fastcall TSecureShell::FormatKeyStr(UnicodeString KeyStr)
@@ -2416,16 +2393,8 @@ UnicodeString TSecureShell::StoreHostKey(
 void TSecureShell::ParseFingerprint(const UnicodeString & Fingerprint, UnicodeString & SignKeyType, UnicodeString & Hash)
 {
   UnicodeString Buf = Fingerprint;
-  UnicodeString SignKeyAlg, SignKeySize;
-  if (get_ssh_version(FBackendHandle) == 1)
-  {
-    SignKeyAlg = GetSsh1KeyType();
-  }
-  else
-  {
-    SignKeyAlg = CutToChar(Buf, L' ', false);
-  }
-  SignKeySize = CutToChar(Buf, L' ', false);
+  UnicodeString SignKeyAlg = CutToChar(Buf, L' ', false);
+  UnicodeString SignKeySize = CutToChar(Buf, L' ', false);
   SignKeyType = SignKeyAlg + L' ' + SignKeySize;
   Hash = Buf;
 }
@@ -2448,21 +2417,16 @@ void __fastcall TSecureShell::VerifyHostKey(
 
   UnicodeString SignKeyType, MD5, SHA256;
   ParseFingerprint(AFingerprintMD5, SignKeyType, MD5);
-  if (get_ssh_version(FBackendHandle) == 1)
+
+  DebugAssert(get_ssh_version(FBackendHandle) == 2);
+  UnicodeString SignKeyTypeSHA256;
+  ParseFingerprint(AFingerprintSHA256, SignKeyTypeSHA256, SHA256);
+  DebugAssert(SignKeyTypeSHA256 == SignKeyType);
+  if (DebugAlwaysTrue(StartsText(L"SHA256:", SHA256)))
   {
-    DebugAssert(AFingerprintSHA256.IsEmpty());
-    SHA256 = L"-";
+    CutToChar(SHA256, L':', false);
   }
-  else
-  {
-    UnicodeString SignKeyTypeSHA256;
-    ParseFingerprint(AFingerprintSHA256, SignKeyTypeSHA256, SHA256);
-    DebugAssert(SignKeyTypeSHA256 == SignKeyType);
-    if (DebugAlwaysTrue(StartsText(L"SHA256:", SHA256)))
-    {
-      CutToChar(SHA256, L':', false);
-    }
-  }
+
   UnicodeString FingerprintMD5 = SignKeyType + L' ' + MD5;
   DebugAssert(AFingerprintMD5 == FingerprintMD5);
   UnicodeString FingerprintSHA256 = SignKeyType + L' ' + SHA256;
@@ -2755,14 +2719,7 @@ void __fastcall TSecureShell::CollectUsage()
     Configuration->Usage->Inc(L"OpenedSessionsPrivateKey2");
   }
 
-  if (FSshVersion == 1)
-  {
-    Configuration->Usage->Inc(L"OpenedSessionsSSH1");
-  }
-  else if (FSshVersion == 2)
-  {
-    Configuration->Usage->Inc(L"OpenedSessionsSSH2");
-  }
+  Configuration->Usage->Inc(L"OpenedSessionsSSH2");
 
   if (SshImplementation == sshiOpenSSH)
   {

source/core/SecureShell.h
@@ -45,7 +45,6 @@ private:
   bool FStoredPasswordTried;
   bool FStoredPasswordTriedForKI;
   bool FStoredPassphraseTried;
-  int FSshVersion;
   bool FOpened;
   bool FClosed;
   int FWaiting;

source/core/SessionData.cpp
@@ -34,7 +34,6 @@ const UnicodeString CipherNames[CIPHER_COUNT] = {L"WARN", L"3des", L"blowfish", 
 const UnicodeString KexNames[KEX_COUNT] = {L"WARN", L"dh-group1-sha1", L"dh-group14-sha1", L"dh-gex-sha1", L"rsa", L"ecdh"};
 const UnicodeString HostKeyNames[HOSTKEY_COUNT] = {L"WARN", L"rsa", L"dsa", L"ecdsa", L"ed25519", L"ed448"};
 const UnicodeString GssLibNames[GSSLIB_COUNT] = {L"gssapi32", L"sspi", L"custom"};
-const wchar_t SshProtList[][10] = {L"1", L"1>2", L"2>1", L"2"};
 // Update also order in Ssh2CipherList()
 const TCipher DefaultCipherList[CIPHER_COUNT] =
   { cipAES, cipChaCha20, cipBlowfish, cip3DES, cipWarn, cipArcfour, cipDES };
@@ -129,7 +128,6 @@ void __fastcall TSessionData::DefaultSettings()
   Timeout = 15;
   TryAgent = true;
   AgentFwd = false;
-  AuthTIS = false;
   AuthKI = true;
   AuthKIPassword = true;
   AuthGSSAPI = true;
@@ -138,7 +136,6 @@ void __fastcall TSessionData::DefaultSettings()
   LogicalHostName = L"";
   ChangeUsername = false;
   Compression = false;
-  SshProt = ssh2only;
   Ssh2DES = false;
   SshNoUserAuth = false;
   for (int Index = 0; Index < CIPHER_COUNT; Index++)
@@ -338,11 +335,9 @@ void __fastcall TSessionData::NonPersistant()
   PROPERTY(Timeout); \
   PROPERTY(TryAgent); \
   PROPERTY(AgentFwd); \
-  PROPERTY(AuthTIS); \
   PROPERTY(LogicalHostName); \
   PROPERTY(ChangeUsername); \
   PROPERTY(Compression); \
-  PROPERTY(SshProt); \
   PROPERTY(Ssh2DES); \
   PROPERTY(SshNoUserAuth); \
   PROPERTY(CipherList); \
@@ -661,7 +656,6 @@ void __fastcall TSessionData::DoLoad(THierarchicalStorage * Storage, bool PuttyI
   Timeout = Storage->ReadInteger(L"Timeout", Timeout);
   TryAgent = Storage->ReadBool(L"TryAgent", TryAgent);
   AgentFwd = Storage->ReadBool(L"AgentFwd", AgentFwd);
-  AuthTIS = Storage->ReadBool(L"AuthTIS", AuthTIS);
   AuthKI = Storage->ReadBool(L"AuthKI", AuthKI);
   AuthKIPassword = Storage->ReadBool(L"AuthKIPassword", AuthKIPassword);
   // Continue to use setting keys of previous kerberos implementation (vaclav tomec),
@@ -676,17 +670,6 @@ void __fastcall TSessionData::DoLoad(THierarchicalStorage * Storage, bool PuttyI
   LogicalHostName = Storage->ReadString(L"LogicalHostName", Storage->ReadString(L"GSSAPIServerRealm", Storage->ReadString(L"KerbPrincipal", LogicalHostName)));
   ChangeUsername = Storage->ReadBool(L"ChangeUsername", ChangeUsername);
   Compression = Storage->ReadBool(L"Compression", Compression);
-  TSshProt ASshProt = (TSshProt)Storage->ReadInteger(L"SshProt", SshProt);
-  // Old sessions may contain the values correponding to the fallbacks we used to allow; migrate them
-  if (ASshProt == ssh2deprecated)
-  {
-    ASshProt = ssh2only;
-  }
-  else if (ASshProt == ssh1deprecated)
-  {
-    ASshProt = ssh1only;
-  }
-  SshProt = ASshProt;
   Ssh2DES = Storage->ReadBool(L"Ssh2DES", Ssh2DES);
   SshNoUserAuth = Storage->ReadBool(L"SshNoUserAuth", SshNoUserAuth);
   CipherList = Storage->ReadString(L"Cipher", CipherList);
@@ -799,9 +782,6 @@ void __fastcall TSessionData::DoLoad(THierarchicalStorage * Storage, bool PuttyI
   #define READ_BUG(BUG) \
     Bug[sb##BUG] = TAutoSwitch(2 - Storage->ReadInteger(L"Bug"#BUG, \
       2 - Bug[sb##BUG]));
-  READ_BUG(Ignore1);
-  READ_BUG(PlainPW1);
-  READ_BUG(RSA1);
   READ_BUG(HMAC2);
   READ_BUG(DeriveKey2);
   READ_BUG(RSAPad2);
@@ -1040,7 +1020,6 @@ void __fastcall TSessionData::DoSave(THierarchicalStorage * Storage,
   WRITE_DATA(Integer, Timeout);
   WRITE_DATA(Bool, TryAgent);
   WRITE_DATA(Bool, AgentFwd);
-  WRITE_DATA(Bool, AuthTIS);
   WRITE_DATA(Bool, AuthKI);
   WRITE_DATA(Bool, AuthKIPassword);
   WRITE_DATA(String, Note);
@@ -1065,7 +1044,6 @@ void __fastcall TSessionData::DoSave(THierarchicalStorage * Storage,
 
   WRITE_DATA(Bool, ChangeUsername);
   WRITE_DATA(Bool, Compression);
-  WRITE_DATA(Integer, SshProt);
   WRITE_DATA(Bool, Ssh2DES);
   WRITE_DATA(Bool, SshNoUserAuth);
   WRITE_DATA_EX(String, L"Cipher", CipherList, );
@@ -1173,9 +1151,6 @@ void __fastcall TSessionData::DoSave(THierarchicalStorage * Storage,
 
   #define WRITE_DATA_CONV_FUNC(X) (2 - (X))
   #define WRITE_BUG(BUG) WRITE_DATA_CONV(Integer, L"Bug" #BUG, Bug[sb##BUG]);
-  WRITE_BUG(Ignore1);
-  WRITE_BUG(PlainPW1);
-  WRITE_BUG(RSA1);
   WRITE_BUG(HMAC2);
   WRITE_BUG(DeriveKey2);
   WRITE_BUG(RSAPad2);
@@ -2653,11 +2628,6 @@ void __fastcall TSessionData::SetAgentFwd(bool value)
   SET_SESSION_PROPERTY(AgentFwd);
 }
 //---------------------------------------------------------------------
-void __fastcall TSessionData::SetAuthTIS(bool value)
-{
-  SET_SESSION_PROPERTY(AuthTIS);
-}
-//---------------------------------------------------------------------
 void __fastcall TSessionData::SetAuthKI(bool value)
 {
   SET_SESSION_PROPERTY(AuthKI);
@@ -2693,11 +2663,6 @@ void __fastcall TSessionData::SetCompression(bool value)
   SET_SESSION_PROPERTY(Compression);
 }
 //---------------------------------------------------------------------
-void __fastcall TSessionData::SetSshProt(TSshProt value)
-{
-  SET_SESSION_PROPERTY(SshProt);
-}
-//---------------------------------------------------------------------
 void __fastcall TSessionData::SetSsh2DES(bool value)
 {
   SET_SESSION_PROPERTY(Ssh2DES);
@@ -2706,11 +2671,6 @@ void __fastcall TSessionData::SetSsh2DES(bool value)
 void __fastcall TSessionData::SetSshNoUserAuth(bool value)
 {
   SET_SESSION_PROPERTY(SshNoUserAuth);
-}
-//---------------------------------------------------------------------
-UnicodeString __fastcall TSessionData::GetSshProtStr()
-{
-  return SshProtList[FSshProt];
 }
 //---------------------------------------------------------------------
 bool __fastcall TSessionData::GetUsesSsh()
@@ -4346,7 +4306,6 @@ UnicodeString __fastcall TSessionData::ComposePath(
 void __fastcall TSessionData::DisableAuthentationsExceptPassword()
 {
   SshNoUserAuth = false;
-  AuthTIS = false;
   AuthKI = false;
   AuthKIPassword = false;
   AuthGSSAPI = false;

source/core/SessionData.h
@@ -17,7 +17,6 @@ enum TFSProtocol { fsSCPonly = 0, fsSFTP = 1, fsSFTPonly = 2, fsFTP = 5, fsWebDA
 #define FSPROTOCOL_COUNT (fsS3+1)
 extern const wchar_t * ProxyMethodNames;
 enum TProxyMethod { pmNone, pmSocks4, pmSocks5, pmHTTP, pmTelnet, pmCmd };
-enum TSshProt { ssh1only, ssh1deprecated, ssh2deprecated, ssh2only };
 enum TKex { kexWarn, kexDHGroup1, kexDHGroup14, kexDHGEx, kexRSA, kexECDH };
 #define KEX_COUNT (kexECDH+1)
 enum THostKey { hkWarn, hkRSA, hkDSA, hkECDSA, hkED25519, hkED448, hkMax };
@@ -25,7 +24,7 @@ enum THostKey { hkWarn, hkRSA, hkDSA, hkECDSA, hkED25519, hkED448, hkMax };
 enum TGssLib { gssGssApi32, gssSspi, gssCustom };
 #define GSSLIB_COUNT (gssCustom+1)
 // names have to match PuTTY registry entries (see settings.c)
-enum TSshBug { sbIgnore1, sbPlainPW1, sbRSA1, sbHMAC2, sbDeriveKey2, sbRSAPad2,
+enum TSshBug { sbHMAC2, sbDeriveKey2, sbRSAPad2,
   sbPKSessID2, sbRekey2, sbMaxPkt2, sbIgnore2, sbOldGex2, sbWinAdj, sbChanReq };
 #define BUG_COUNT (sbChanReq+1)
 enum TSftpBug { sbSymlink, sbSignedTS };
@@ -114,7 +113,6 @@ private:
   bool FTryAgent;
   bool FAgentFwd;
   UnicodeString FListingCommand;
-  bool FAuthTIS;
   bool FAuthKI;
   bool FAuthKIPassword;
   bool FAuthGSSAPI;
@@ -122,7 +120,6 @@ private:
   bool FGSSAPIFwdTGT;
   bool FChangeUsername;
   bool FCompression;
-  TSshProt FSshProt;
   bool FSsh2DES;
   bool FSshNoUserAuth;
   TCipher FCiphers[CIPHER_COUNT];
@@ -263,7 +260,6 @@ private:
   void __fastcall SetPingInterval(int value);
   void __fastcall SetTryAgent(bool value);
   void __fastcall SetAgentFwd(bool value);
-  void __fastcall SetAuthTIS(bool value);
   void __fastcall SetAuthKI(bool value);
   void __fastcall SetAuthKIPassword(bool value);
   void __fastcall SetAuthGSSAPI(bool value);
@@ -271,7 +267,6 @@ private:
   void __fastcall SetGSSAPIFwdTGT(bool value);
   void __fastcall SetChangeUsername(bool value);
   void __fastcall SetCompression(bool value);
-  void __fastcall SetSshProt(TSshProt value);
   void __fastcall SetSsh2DES(bool value);
   void __fastcall SetSshNoUserAuth(bool value);
   void __fastcall SetCipher(int Index, TCipher value);
@@ -337,7 +332,6 @@ private:
   void __fastcall SetSourceAddress(const UnicodeString & value);
   void __fastcall SetProtocolFeatures(const UnicodeString & value);
   void __fastcall SetSshSimple(bool value);
-  UnicodeString __fastcall GetSshProtStr();
   bool __fastcall GetUsesSsh();
   void __fastcall SetCipherList(UnicodeString value);
   UnicodeString __fastcall GetCipherList() const;
@@ -557,7 +551,6 @@ public:
   __property bool TryAgent  = { read=FTryAgent, write=SetTryAgent };
   __property bool AgentFwd  = { read=FAgentFwd, write=SetAgentFwd };
   __property UnicodeString ListingCommand = { read = FListingCommand, write = SetListingCommand };
-  __property bool AuthTIS  = { read=FAuthTIS, write=SetAuthTIS };
   __property bool AuthKI  = { read=FAuthKI, write=SetAuthKI };
   __property bool AuthKIPassword  = { read=FAuthKIPassword, write=SetAuthKIPassword };
   __property bool AuthGSSAPI  = { read=FAuthGSSAPI, write=SetAuthGSSAPI };
@@ -565,7 +558,6 @@ public:
   __property bool GSSAPIFwdTGT = { read=FGSSAPIFwdTGT, write=SetGSSAPIFwdTGT };
   __property bool ChangeUsername  = { read=FChangeUsername, write=SetChangeUsername };
   __property bool Compression  = { read=FCompression, write=SetCompression };
-  __property TSshProt SshProt  = { read=FSshProt, write=SetSshProt };
   __property bool UsesSsh = { read = GetUsesSsh };
   __property bool Ssh2DES  = { read=FSsh2DES, write=SetSsh2DES };
   __property bool SshNoUserAuth  = { read=FSshNoUserAuth, write=SetSshNoUserAuth };
@@ -623,7 +615,6 @@ public:
   __property UnicodeString SourceAddress = { read=FSourceAddress, write=SetSourceAddress };
   __property UnicodeString ProtocolFeatures = { read=FProtocolFeatures, write=SetProtocolFeatures };
   __property bool SshSimple  = { read=FSshSimple, write=SetSshSimple };
-  __property UnicodeString SshProtStr  = { read=GetSshProtStr };
   __property UnicodeString CipherList  = { read=GetCipherList, write=SetCipherList };
   __property UnicodeString KexList  = { read=GetKexList, write=SetKexList };
   __property UnicodeString HostKeyList  = { read=GetHostKeyList, write=SetHostKeyList };

commit 2a355b95b18e5897754788273585743821df5b22

Warning when selecting too new key file + Fixing selecting PPK files and error message when selecting SSH-1 keys, both broken by 5dfcf1eb
Source commit: 7df0f518396d27d37ec0755c97b7c96e24f56205

source/core/PuttyIntf.cpp
@@ -651,7 +651,7 @@ bool IsKeyEncrypted(TKeyType KeyType, const UnicodeString & FileName, UnicodeStr
   return Result;
 }
 //---------------------------------------------------------------------------
-TPrivateKey * LoadKey(TKeyType KeyType, const UnicodeString & FileName, const UnicodeString & Passphrase)
+TPrivateKey * LoadKey(TKeyType KeyType, const UnicodeString & FileName, const UnicodeString & Passphrase, UnicodeString & Error)
 {
   UTF8String UtfFileName = UTF8String(FileName);
   Filename * KeyFile = filename_from_str(UtfFileName.c_str());
@@ -685,19 +685,48 @@ TPrivateKey * LoadKey(TKeyType KeyType, const UnicodeString & FileName, const Un
 
   if (Ssh2Key == NULL)
   {
-    UnicodeString Error = AnsiString(ErrorStr);
     // While theoretically we may get "unable to open key file" and
     // so we should check system error code,
     // we actully never get here unless we call KeyType previously
     // and handle ktUnopenable accordingly.
-    throw Exception(Error);
+    Error = AnsiString(ErrorStr);
   }
   else if (Ssh2Key == SSH2_WRONG_PASSPHRASE)
   {
-    throw Exception(LoadStr(AUTH_TRANSL_WRONG_PASSPHRASE));
+    Error = EmptyStr;
+    Ssh2Key = NULL;
   }
 
   return reinterpret_cast<TPrivateKey *>(Ssh2Key);
+}
+//---------------------------------------------------------------------------
+TPrivateKey * LoadKey(TKeyType KeyType, const UnicodeString & FileName, const UnicodeString & Passphrase)
+{
+  UnicodeString Error;
+  TPrivateKey * Result = LoadKey(KeyType, FileName, Passphrase, Error);
+  if (Result == NULL)
+  {
+    if (!Error.IsEmpty())
+    {
+      throw Exception(Error);
+    }
+    else
+    {
+      throw Exception(LoadStr(AUTH_TRANSL_WRONG_PASSPHRASE));
+    }
+  }
+  return Result;
+}
+//---------------------------------------------------------------------------
+UnicodeString TestKey(TKeyType KeyType, const UnicodeString & FileName)
+{
+  UnicodeString Result;
+  TPrivateKey * Key = LoadKey(KeyType, FileName, EmptyStr, Result);
+  if (Key != NULL)
+  {
+    FreeKey(Key);
+  }
+  return Result;
 }
 //---------------------------------------------------------------------------
 void ChangeKeyComment(TPrivateKey * PrivateKey, const UnicodeString & Comment)

source/core/PuttyTools.h
@@ -13,6 +13,7 @@ TKeyType KeyType(UnicodeString FileName);
 bool IsKeyEncrypted(TKeyType KeyType, const UnicodeString & FileName, UnicodeString & Comment);
 struct TPrivateKey;
 TPrivateKey * LoadKey(TKeyType KeyType, const UnicodeString & FileName, const UnicodeString & Passphrase);
+UnicodeString TestKey(TKeyType KeyType, const UnicodeString & FileName);
 void ChangeKeyComment(TPrivateKey * PrivateKey, const UnicodeString & Comment);
 void SaveKey(TKeyType KeyType, const UnicodeString & FileName,
   const UnicodeString & Passphrase, TPrivateKey * PrivateKey);

source/windows/Tools.cpp
@@ -1285,7 +1285,7 @@ static void __fastcall ConvertKey(UnicodeString & FileName, TKeyType Type)
   }
 }
 //---------------------------------------------------------------------------
-static void __fastcall DoVerifyKey(UnicodeString & FileName, bool Convert, bool CanIgnore)
+void DoVerifyKey(UnicodeString & FileName, bool Convert, UnicodeString & Message, TStrings *& MoreMessages, UnicodeString & HelpKeyword)
 {
   if (!FileName.Trim().IsEmpty())
   {
@@ -1293,10 +1293,9 @@ static void __fastcall DoVerifyKey(UnicodeString & FileName, bool Convert, bool 
     TKeyType Type = KeyType(FileName);
     // reason _wfopen failed
     int Error = errno;
-    UnicodeString Message;
-    UnicodeString HelpKeyword = HELP_LOGIN_KEY_TYPE;
+    HelpKeyword = HELP_LOGIN_KEY_TYPE;
     UnicodeString PuttygenPath;
-    std::unique_ptr<TStrings> MoreMessages;
+    std::unique_ptr<TStrings> AMoreMessages;
     switch (Type)
     {
       case ktOpenSSHPEM:
@@ -1310,7 +1309,7 @@ static void __fastcall DoVerifyKey(UnicodeString & FileName, bool Convert, bool 
           {
             Configuration->Usage->Inc(L"PrivateKeyConvertSuggestionsNative");
             UnicodeString ConvertMessage = FMTLOAD(KEY_TYPE_CONVERT3, (TypeName, RemoveMainInstructionsTag(Message)));
-            Message = UnicodeString();
+            Message = EmptyStr;
             if (MoreMessageDialog(ConvertMessage, NULL, qtConfirmation, qaOK | qaCancel, HelpKeyword) == qaOK)
             {
               ConvertKey(FileName, Type);
@@ -1329,7 +1328,11 @@ static void __fastcall DoVerifyKey(UnicodeString & FileName, bool Convert, bool 
         break;
 
       case ktSSH1:
-        Message = MainInstructions(LoadStr(KEY_TYPE_SSH1));
+        Message = MainInstructions(FMTLOAD(KEY_TYPE_SSH1, (FileName)));
+        break;
+
+      case ktSSH2:
+        Message = TestKey(Type, FileName);
         break;
 
       case ktSSH1Public:
@@ -1342,7 +1345,7 @@ static void __fastcall DoVerifyKey(UnicodeString & FileName, bool Convert, bool 
         Message = MainInstructions(FMTLOAD(KEY_TYPE_UNOPENABLE, (FileName)));
         if (Error != ERROR_SUCCESS)
         {
-          MoreMessages.reset(TextToStringList(SysErrorMessageForError(Error)));
+          AMoreMessages.reset(TextToStringList(SysErrorMessageForError(Error)));
         }
         break;
 
@@ -1354,14 +1357,25 @@ static void __fastcall DoVerifyKey(UnicodeString & FileName, bool Convert, bool 
         break;
     }
 
-    if (!Message.IsEmpty())
+    MoreMessages = AMoreMessages.release();
+  }
+}
+
+//---------------------------------------------------------------------------
+static void __fastcall DoVerifyKey(UnicodeString & FileName, bool Convert, bool CanIgnore)
+{
+  TStrings * AMoreMessages;
+  UnicodeString Message;
+  UnicodeString HelpKeyword;
+  DoVerifyKey(FileName, Convert, Message, AMoreMessages, HelpKeyword);
+  std::unique_ptr<TStrings> MoreMessages(AMoreMessages);
+  if (!Message.IsEmpty())
+  {
+    Configuration->Usage->Inc(L"PrivateKeySelectErrors");
+    unsigned int Answers = (CanIgnore ? (qaIgnore | qaAbort) : qaOK);
+    if (MoreMessageDialog(Message, MoreMessages.get(), qtWarning, Answers, HelpKeyword) != qaIgnore)
     {
-      Configuration->Usage->Inc(L"PrivateKeySelectErrors");
-      unsigned int Answers = (CanIgnore ? (qaIgnore | qaAbort) : qaOK);
-      if (MoreMessageDialog(Message, MoreMessages.get(), qtWarning, Answers, HelpKeyword) != qaIgnore)
-      {
-        Abort();
-      }
+      Abort();
     }
   }
 }