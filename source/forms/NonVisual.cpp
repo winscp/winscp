winscp/winscp

commit c2c2596dc487e5edb70a8021ad695f73d24020d9

Queue panel is always hidden in the local-local mode
Part of Bug 1893

Source commit: ae286a612042338a8cd02452a4b6c786b28170bf

source/forms/NonVisual.cpp
@@ -482,9 +482,11 @@ void __fastcall TNonVisualDataModule::ExplorerActionsUpdate(
     QueueItemSpeedAction->Text = SetSpeedLimit(reinterpret_cast<unsigned long>(AuxVoidPtr)),
     QueueItemSpeedAction->Text = L"")
   UPDACT(QueueToggleShowAction,
-    Action->Checked = ScpExplorer->ComponentVisible[fcQueueView])
+    Action->Checked = ScpExplorer->ComponentVisible[fcQueueView];
+    Action->Enabled = !ScpExplorer->IsLocalBrowserMode())
   #define QUEUEACTION(SHOW) UPDACT(Queue ## SHOW ## Action, \
-    Action->Checked = WinConfiguration->QueueView.Show == qv ## SHOW)
+    Action->Checked = WinConfiguration->QueueView.Show == qv ## SHOW; \
+    Action->Enabled = !ScpExplorer->IsLocalBrowserMode())
   QUEUEACTION(Show)
   QUEUEACTION(HideWhenEmpty)
   QUEUEACTION(Hide)

source/forms/CustomScpExplorer.cpp
@@ -745,9 +745,10 @@ UnicodeString __fastcall TCustomScpExplorerForm::GetQueueProgressTitle()
 void __fastcall TCustomScpExplorerForm::UpdateQueueView()
 {
   ComponentVisible[fcQueueView] =
-    (WinConfiguration->QueueView.Show == qvShow) ||
-    ((WinConfiguration->QueueView.Show == qvHideWhenEmpty) &&
-     (FQueueStatus != NULL) && (FQueueStatus->Count > 0));
+    !IsLocalBrowserMode() &&
+    ((WinConfiguration->QueueView.Show == qvShow) ||
+     ((WinConfiguration->QueueView.Show == qvHideWhenEmpty) &&
+      (FQueueStatus != NULL) && (FQueueStatus->Count > 0)));
 }
 //---------------------------------------------------------------------------
 void __fastcall TCustomScpExplorerForm::QueueChanged()

commit 3ea7f33f1680b247a009f63852343721fab79d86

High CPU load when idle in local-local mode
Caused by c2c2596d (Queue panel is always hidden in the local-local mode)

Source commit: dc1427068dfe91f025ef9f005254e4661ad50489

source/forms/NonVisual.cpp
@@ -487,12 +487,9 @@ void __fastcall TNonVisualDataModule::ExplorerActionsUpdate(
   UPDEX(QueueItemSpeedAction, ScpExplorer->AllowQueueOperation(qoItemSpeed, &AuxVoidPtr),
     QueueItemSpeedAction->Text = SetSpeedLimit(reinterpret_cast<unsigned long>(AuxVoidPtr)),
     QueueItemSpeedAction->Text = L"")
-  UPDACT(QueueToggleShowAction,
-    Action->Checked = ScpExplorer->ComponentVisible[fcQueueView];
-    Action->Enabled = !ScpExplorer->IsLocalBrowserMode())
-  #define QUEUEACTION(SHOW) UPDACT(Queue ## SHOW ## Action, \
-    Action->Checked = WinConfiguration->QueueView.Show == qv ## SHOW; \
-    Action->Enabled = !ScpExplorer->IsLocalBrowserMode())
+  UPDEX1(QueueToggleShowAction, !ScpExplorer->IsLocalBrowserMode(), Action->Checked = ScpExplorer->ComponentVisible[fcQueueView])
+  #define QUEUEACTION(SHOW) \
+    UPDEX1(Queue ## SHOW ## Action, !ScpExplorer->IsLocalBrowserMode(), Action->Checked = WinConfiguration->QueueView.Show == qv ## SHOW)
   QUEUEACTION(Show)
   QUEUEACTION(HideWhenEmpty)
   QUEUEACTION(Hide)