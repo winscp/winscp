winscp/winscp

commit 268f63557e7b3e796cec361d26c44c9316fe2d4d

Proper TLS shutdown
Source commit: 86a380532057c0d913678b9ca910cb319a8f422c

source/filezilla/AsyncSslSocketLayer.cpp
@@ -606,6 +606,14 @@ int CAsyncSslSocketLayer::Receive(void* lpBuf, int nBufLen, int nFlags)
 
 void CAsyncSslSocketLayer::Close()
 {
+  if (!m_nShutDown)
+  {
+    ShutDown();
+    while (!ShutDownComplete())
+    {
+      OnSend(0);
+    }
+  }
   m_nShutDown = 0;
   m_onCloseCalled = false;
   ResetSslSession();

commit 2c0b486de5ba36f7ffed4d5753c60ed64d7192cd

Deadlock when closing connection when TLS was never established (caused by 268f6355)
Source commit: 24d6adda3af26be8871a9973338011cfe8d0c86c

source/filezilla/AsyncSslSocketLayer.cpp
@@ -606,7 +606,7 @@ int CAsyncSslSocketLayer::Receive(void* lpBuf, int nBufLen, int nFlags)
 
 void CAsyncSslSocketLayer::Close()
 {
-  if (!m_nShutDown)
+  if (!m_nShutDown && m_bSslEstablished)
   {
     ShutDown();
     while (!ShutDownComplete())