winscp/winscp

commit 268f63557e7b3e796cec361d26c44c9316fe2d4d

Proper TLS shutdown
Source commit: 86a380532057c0d913678b9ca910cb319a8f422c

source/filezilla/AsyncSslSocketLayer.cpp
@@ -606,6 +606,14 @@ int CAsyncSslSocketLayer::Receive(void* lpBuf, int nBufLen, int nFlags)
 
 void CAsyncSslSocketLayer::Close()
 {
+  if (!m_nShutDown)
+  {
+    ShutDown();
+    while (!ShutDownComplete())
+    {
+      OnSend(0);
+    }
+  }
   m_nShutDown = 0;
   m_onCloseCalled = false;
   ResetSslSession();

commit aff5cba787eef3dbad88f075bb7a4986922fbee8

Prevent hang when closing TLS connection after network failure
Related to 2c0b486d, 5d68c005. Caused by 268f6355.

Source commit: 095c15c2ed15e8cfd517a3aebf1665d60d6b5977

source/filezilla/AsyncSslSocketLayer.cpp
@@ -606,10 +606,10 @@ int CAsyncSslSocketLayer::Receive(void* lpBuf, int nBufLen, int nFlags)
 
 void CAsyncSslSocketLayer::Close()
 {
-  if (!m_nShutDown && m_bSslEstablished && !m_nCriticalError)
+  if (!m_nShutDown && m_bSslEstablished && !m_nNetworkError && !m_nCriticalError)
   {
     ShutDown();
-    while (!ShutDownComplete())
+    while (!ShutDownComplete() && !m_nNetworkError && !m_nCriticalError)
     {
       OnSend(0);
     }