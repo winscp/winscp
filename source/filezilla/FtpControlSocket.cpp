winscp/winscp

commit cee1be5d6226da526fbd6956839b9a341d9c5433

Bug 2077: Improve error message when FTP server returns malformed response
https://winscp.net/tracker/2077

Source commit: fcf4ac3e67bfc4f877167881157278f1e28650db

source/filezilla/FtpControlSocket.cpp
@@ -1532,10 +1532,16 @@ int CFtpControlSocket::GetReplyCode()
   if (m_RecvBuffer.empty())
     return 0;
   CStringA str = m_RecvBuffer.front();
-  if (str == "")
+  if ((str == "") || (str[0] < '1') || (str[0] > '9'))
+  {
+    UnicodeString Error = FMTLOAD(FTP_MALFORMED_RESPONSE, (UnicodeString(str)));
+    LogMessageRaw(FZ_LOG_WARNING, Error.c_str());
     return 0;
+  }
   else
+  {
     return str[0]-'0';
+  }
 }
 
 void CFtpControlSocket::DoClose(int nError /*=0*/)

source/resource/TextsCore.h
@@ -278,6 +278,7 @@
 #define CREATE_TEMP_DIR_ERROR   754
 #define OPENSSH_CONFIG_NOT_FOUND 755
 #define OPENSSH_CONFIG_NO_SITES 756
+#define FTP_MALFORMED_RESPONSE  757
 
 #define CORE_CONFIRMATION_STRINGS 300
 #define CONFIRM_PROLONG_TIMEOUT3 301

source/resource/TextsCore1.rc
@@ -250,6 +250,7 @@ BEGIN
   CREATE_TEMP_DIR_ERROR, "Cannot create temporary directory '%s'. You may change root directory to store temporary files in Preferences."
   OPENSSH_CONFIG_NOT_FOUND, "OpenSSH config file not found."
   OPENSSH_CONFIG_NO_SITES, "No Host directives for specific hosts found in OpenSSH config file."
+  FTP_MALFORMED_RESPONSE, "FTP server returned invalid response '%s'."
 
   CORE_CONFIRMATION_STRINGS, "CORE_CONFIRMATION"
   CONFIRM_PROLONG_TIMEOUT3, "Host is not communicating for %d seconds.\n\nWait for another %0:d seconds?"

commit b62d433c40fb35bcb41f607db098d997de302b56

Invalid FTP response was incorrectly logged when closing transfer connection (caused by cee1be5d)
Source commit: 058b686e4addddc869a112d728f849b9c151fafd

source/filezilla/FtpControlSocket.cpp
@@ -1527,12 +1527,16 @@ BOOL CFtpControlSocket::Send(CString str)
   return TRUE;
 }
 
-int CFtpControlSocket::GetReplyCode()
+int CFtpControlSocket::TryGetReplyCode()
 {
   if (m_RecvBuffer.empty())
     return 0;
   CStringA str = m_RecvBuffer.front();
-  if ((str == "") || (str[0] < '1') || (str[0] > '9'))
+  if (str == "")
+  {
+    return -1;
+  }
+  else if ((str[0] < '1') || (str[0] > '9'))
   {
     UnicodeString Error = FMTLOAD(FTP_MALFORMED_RESPONSE, (UnicodeString(str)));
     LogMessageRaw(FZ_LOG_WARNING, Error.c_str());
@@ -1542,6 +1546,18 @@ int CFtpControlSocket::GetReplyCode()
   {
     return str[0]-'0';
   }
+}
+
+int CFtpControlSocket::GetReplyCode()
+{
+  int Result = TryGetReplyCode();
+  if (Result < 0)
+  {
+    UnicodeString Error = FMTLOAD(FTP_MALFORMED_RESPONSE, (UnicodeString()));
+    LogMessageRaw(FZ_LOG_WARNING, Error.c_str());
+    Result = 0;
+  }
+  return Result;
 }
 
 void CFtpControlSocket::DoClose(int nError /*=0*/)
@@ -2922,7 +2938,13 @@ void CFtpControlSocket::FileTransfer(t_transferfile *transferfile/*=0*/,BOOL bFi
     ///////////
     //Replies//
     ///////////
-    int code = GetReplyCode();
+    int code = TryGetReplyCode();
+    // We do not always expect a response here, particularly when closing transfer connection (FILETRANSFER_WAITFINISH).
+    // The normalization to 0 is probably not needed.
+    if (code < 0)
+    {
+      code = 0;
+    }
     switch(m_Operation.nOpState)
     {
     case FILETRANSFER_PWD:

source/filezilla/FtpControlSocket.h
@@ -119,6 +119,7 @@ protected:
   void ResetTransferSocket(int Error);
 
   void DoClose(int nError = 0);
+  int TryGetReplyCode();
   int GetReplyCode();
   CString GetReply();
   void LogOnToServer(BOOL bSkipReply = FALSE);