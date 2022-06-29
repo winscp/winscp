winscp/winscp

commit 10ba09efe70378ae52ca9024d68e3a9e91b0f45e

Making sure that "hidden files showing toggled" message is shown promptly
(cherry picked from commit d951f87ce8d74cc9e369ec8623b2abc921d33b62)

Source commit: 49c3e1684244576b77ec0c00d464850982c0fcbc

source/forms/CustomScpExplorer.cpp
@@ -9446,10 +9446,11 @@ void __fastcall TCustomScpExplorerForm::SynchronizeBrowsingChanged()
 //---------------------------------------------------------------------------
 void __fastcall TCustomScpExplorerForm::ToggleShowHiddenFiles()
 {
-  WinConfiguration->ShowHiddenFiles = !WinConfiguration->ShowHiddenFiles;
   PostNote(FORMAT(LoadStrPart(SHOW_HIDDEN_FILES_TOGGLE, 1),
     (LoadStrPart(SHOW_HIDDEN_FILES_TOGGLE,
       (WinConfiguration->ShowHiddenFiles ? 2 : 3)))), 0, NULL, NULL);
+  GetComponent(fcStatusBar)->Repaint(); // toggling ShowHiddenFiles takes time, force repaint beforehand
+  WinConfiguration->ShowHiddenFiles = !WinConfiguration->ShowHiddenFiles;
 }
 //---------------------------------------------------------------------------
 void __fastcall TCustomScpExplorerForm::SetFormatSizeBytes(TFormatBytesStyle Style)

commit 40397d215da10bf25bd1230d278b928af5a263f1

Bug fix: "Hidden files showing toggled" message displays opposite status
Caused by 10ba09ef

Source commit: e0b1a5432ed01250d54015a6dddcb9a7cbbfe8b6

source/forms/CustomScpExplorer.cpp
@@ -9812,11 +9812,12 @@ void __fastcall TCustomScpExplorerForm::SynchronizeBrowsingChanged()
 //---------------------------------------------------------------------------
 void __fastcall TCustomScpExplorerForm::ToggleShowHiddenFiles()
 {
-  PostNote(FORMAT(LoadStrPart(SHOW_HIDDEN_FILES_TOGGLE, 1),
-    (LoadStrPart(SHOW_HIDDEN_FILES_TOGGLE,
-      (WinConfiguration->ShowHiddenFiles ? 2 : 3)))), 0, NULL, NULL);
+  bool NewShowHiddenFiles = !WinConfiguration->ShowHiddenFiles;
+  UnicodeString Note =
+    FORMAT(LoadStrPart(SHOW_HIDDEN_FILES_TOGGLE, 1), (LoadStrPart(SHOW_HIDDEN_FILES_TOGGLE, (NewShowHiddenFiles ? 2 : 3))));
+  PostNote(Note, 0, NULL, NULL);
   GetComponent(fcStatusBar)->Repaint(); // toggling ShowHiddenFiles takes time, force repaint beforehand
-  WinConfiguration->ShowHiddenFiles = !WinConfiguration->ShowHiddenFiles;
+  WinConfiguration->ShowHiddenFiles = NewShowHiddenFiles;
 }
 //---------------------------------------------------------------------------
 void __fastcall TCustomScpExplorerForm::SetFormatSizeBytes(TFormatBytesStyle Style)