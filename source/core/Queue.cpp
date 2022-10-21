winscp/winscp

commit 348396c188f0caedbc7d9d109a0eb8db3a381235

StartupSecondsLastReal statistics
Source commit: e2ab2a3a8091faf710078834d0621a1237f187ee

source/windows/WinMain.cpp
@@ -412,6 +412,51 @@ static UnicodeString ColorToRGBStr(TColor Color)
   return Result;
 }
 //---------------------------------------------------------------------------
+class TStartupThread : public TSimpleThread
+{
+public:
+  TStartupThread();
+
+  int GetStartupSeconds();
+
+  virtual void __fastcall Terminate();
+
+protected:
+  virtual void __fastcall Execute();
+
+  int FMilliseconds;
+  bool FStop;
+};
+//---------------------------------------------------------------------------
+TStartupThread::TStartupThread()
+{
+  FMilliseconds = 0;
+  FStop = false;
+  Start();
+}
+//---------------------------------------------------------------------------
+int TStartupThread::GetStartupSeconds()
+{
+  DebugAssert(!FStop);
+  return FMilliseconds / 1000;
+}
+//---------------------------------------------------------------------------
+void __fastcall TStartupThread::Terminate()
+{
+  FStop = true;
+}
+//---------------------------------------------------------------------------
+void __fastcall TStartupThread::Execute()
+{
+  while (!FStop)
+  {
+    const int Step = 250;
+    Sleep(Step);
+    FMilliseconds += Step;
+  }
+}
+//---------------------------------------------------------------------------
+TStartupThread * StartupThread(new TStartupThread());
 TDateTime Started(Now());
 TDateTime LastStartupStartupSequence(Now());
 UnicodeString StartupSequence;
@@ -420,6 +465,7 @@ int LifetimeRuns = -1;
 void InterfaceStartDontMeasure()
 {
   Started = TDateTime();
+  StartupThread->Terminate();
 }
 //---------------------------------------------------------------------------
 void AddStartupSequence(const UnicodeString & Tag)
@@ -435,6 +481,7 @@ void InterfaceStarted()
   {
     // deliberate downcast
     int StartupSeconds = static_cast<int>(SecondsBetween(Now(), Started));
+    int StartupSecondsReal = DebugNotNull(StartupThread)->GetStartupSeconds();
     if (LifetimeRuns == 1)
     {
       Configuration->Usage->Set(L"StartupSeconds1", StartupSeconds);
@@ -444,9 +491,11 @@ void InterfaceStarted()
       Configuration->Usage->Set(L"StartupSeconds2", StartupSeconds);
     }
     Configuration->Usage->Set(L"StartupSecondsLast", StartupSeconds);
+    Configuration->Usage->Set(L"StartupSecondsLastReal", StartupSecondsReal);
     AddStartupSequence(L"I");
     Configuration->Usage->Set(L"StartupSequenceLast", StartupSequence);
   }
+  StartupThread->Terminate();
 }
 //---------------------------------------------------------------------------
 void __fastcall UpdateStaticUsage()
@@ -578,6 +627,7 @@ void __fastcall UpdateFinalStaticUsage()
 void __fastcall MaintenanceTask()
 {
   CoreMaintenanceTask();
+  InterfaceStartDontMeasure();
 }
 //---------------------------------------------------------------------------
 typedef std::vector<HWND> THandles;
@@ -903,6 +953,7 @@ int __fastcall Execute()
 
   if (Mode != cmNone)
   {
+    InterfaceStartDontMeasure();
     return Console(Mode);
   }
 
@@ -1249,6 +1300,7 @@ int __fastcall Execute()
                   {
                     Configuration->Usage->Inc(L"CommandLineOperation");
                     ScpExplorer->StandaloneOperation = true;
+                    InterfaceStartDontMeasure();
                   }
 
                   if (ParamCommand == pcUpload)
@@ -1330,6 +1382,7 @@ int __fastcall Execute()
     GlyphsModule = NULL;
     TTerminalManager::DestroyInstance();
     delete CommandParams;
+    delete StartupThread;
   }
 
   return 0;

commit 01c3658c005e3ab69d2dfdc27ab191f174d77cae

Pure virtual function call caused by 348396c1
Source commit: ed456191dc0c86ca12ab3387359c06f0ba794d46

source/core/Queue.cpp
@@ -306,6 +306,7 @@ __fastcall TSimpleThread::TSimpleThread() :
 //---------------------------------------------------------------------------
 __fastcall TSimpleThread::~TSimpleThread()
 {
+  // This is turn calls pure virtual Terminate, what does not work as intended, do not rely on it and remove the call eventually
   Close();
 
   if (FThread != NULL)

source/windows/WinMain.cpp
@@ -416,6 +416,7 @@ class TStartupThread : public TSimpleThread
 {
 public:
   TStartupThread();
+  virtual __fastcall ~TStartupThread();
 
   int GetStartupSeconds();
 
@@ -433,6 +434,11 @@ TStartupThread::TStartupThread()
   FMilliseconds = 0;
   FStop = false;
   Start();
+}
+//---------------------------------------------------------------------------
+__fastcall TStartupThread::~TStartupThread()
+{
+//  Close();
 }
 //---------------------------------------------------------------------------
 int TStartupThread::GetStartupSeconds()