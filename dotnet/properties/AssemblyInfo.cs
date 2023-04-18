3de5a5f4a25775a3351e6a168d9565aa4b1f7a05
Martin Prikryl <martin@winscp.net>
2023-04-18 10:05:55 +0000

    Bumping .NET assembly version number before 6.0.2 release as there were changes
    Source commit: 8fa7c0f81672182037ec72f0a06799dd57c628bd

--- dotnet/properties/AssemblyInfo.cs
+++ dotnet/properties/AssemblyInfo.cs
@@ -35,7 +35,7 @@ namespace WinSCP
     {
         public const string UndefinedProductVersion = "9.9.9.9";
 
-        public const string Version = "1.13.0.0";
+        public const string Version = "1.14.0.0";
         public const string ProductVersion = "6.0.2.0";
     }
 }
