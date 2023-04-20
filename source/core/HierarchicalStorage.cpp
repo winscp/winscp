winscp/winscp

commit 71ea2a977ab6209ce754c829362168fa30418d53

Bug 2163: Workaround for using /ini=nul on systems when reading "nul" file fails
https://winscp.net/tracker/2163

Source commit: 2a222baa93187e05a2b7c286015bb1ede9146fa1

source/core/HierarchicalStorage.cpp
@@ -1626,6 +1626,14 @@ TIniFileStorage * __fastcall TIniFileStorage::CreateFromPath(const UnicodeString
   return new TIniFileStorage(AStorage, IniFile);
 }
 //---------------------------------------------------------------------------
+TIniFileStorage * __fastcall TIniFileStorage::CreateNul()
+{
+  // Before we passed "nul", but we have report of a system, where opening "nul" file fails.
+  // Passing an empty string is even more efficient, as it does not even try to read the file.
+  TMemIniFile * IniFile = new TMemIniFile(EmptyStr);
+  return new TIniFileStorage(INI_NUL, IniFile);
+}
+//---------------------------------------------------------------------------
 __fastcall TIniFileStorage::TIniFileStorage(const UnicodeString & AStorage, TCustomIniFile * IniFile):
   TCustomIniFileStorage(AStorage, IniFile)
 {
@@ -1640,7 +1648,7 @@ void __fastcall TIniFileStorage::Flush()
   {
     FMasterStorage->Flush();
   }
-  if (FOriginal != NULL)
+  if ((FOriginal != NULL) && !FIniFile->FileName.IsEmpty())
   {
     std::unique_ptr<TStrings> Strings(new TStringList);
     std::unique_ptr<TStrings> Original(FOriginal);

source/core/Configuration.cpp
@@ -192,7 +192,7 @@ THierarchicalStorage * TConfiguration::CreateScpStorage(bool & SessionList)
   }
   else if (Storage == stNul)
   {
-    Result = TIniFileStorage::CreateFromPath(INI_NUL);
+    Result = TIniFileStorage::CreateNul();
   }
   else
   {

source/core/HierarchicalStorage.h
@@ -259,6 +259,7 @@ class TIniFileStorage : public TCustomIniFileStorage
 {
 public:
   static TIniFileStorage * __fastcall CreateFromPath(const UnicodeString & AStorage);
+  static TIniFileStorage * __fastcall CreateNul();
   virtual __fastcall ~TIniFileStorage();
 
   virtual void __fastcall Flush();

commit b1121ad72f689704241de5cefcbd5593260c7158

Memory leak
Caused by 71ea2a97

Source commit: cc00c4f0789476839c1a7be9ca8e26c0f28e8b9b

source/core/HierarchicalStorage.cpp
@@ -1637,8 +1637,15 @@ TIniFileStorage * __fastcall TIniFileStorage::CreateNul()
 __fastcall TIniFileStorage::TIniFileStorage(const UnicodeString & AStorage, TCustomIniFile * IniFile):
   TCustomIniFileStorage(AStorage, IniFile)
 {
-  FOriginal = new TStringList();
-  dynamic_cast<TMemIniFile *>(FIniFile)->GetStrings(FOriginal);
+  if (!FIniFile->FileName.IsEmpty())
+  {
+    FOriginal = new TStringList();
+    dynamic_cast<TMemIniFile *>(FIniFile)->GetStrings(FOriginal);
+  }
+  else
+  {
+    FOriginal = NULL;
+  }
   ApplyOverrides();
 }
 //---------------------------------------------------------------------------
@@ -1648,6 +1655,7 @@ void __fastcall TIniFileStorage::Flush()
   {
     FMasterStorage->Flush();
   }
+  // This does not seem correct, if storage is written after it is flushed (though we probably never do that currently)
   if ((FOriginal != NULL) && !FIniFile->FileName.IsEmpty())
   {
     std::unique_ptr<TStrings> Strings(new TStringList);