winscp/winscp

commit 069aa20fb38048c3518b30aed5051ef4eabeefa3

Bug 1948: Debug information is printed at the end of the scripting session when debug logging level is set even if logging is turned off
https://winscp.net/tracker/1948

Source commit: c46326e83e52349ccc6f037c18a59a85d855e099

source/core/Script.cpp
@@ -503,7 +503,7 @@ void __fastcall TScript::Command(UnicodeString Cmd)
         UnicodeString LogCmd = GetLogCmd(FullCmd, Command, Cmd);
         Log(llInput, LogCmd);
 
-        if (Configuration->LogProtocol >= 1)
+        if (Configuration->ActualLogProtocol >= 1)
         {
           UnicodeString DummyLogCmd;
           if (DebugAlwaysTrue(CutToken(LogCmd, DummyLogCmd)))

source/core/SessionInfo.cpp
@@ -1177,7 +1177,7 @@ void __fastcall TSessionLog::DoAddStartupInfo(TSessionData * Data)
     ADF(L"Process ID: %d", (int(GetCurrentProcessId())));
     ADF(L"Parent process: %s", (GetAncestorProcessName()));
     ADF(L"Command-line: %s", (GetCmdLineLog()));
-    if (FConfiguration->LogProtocol >= 1)
+    if (FConfiguration->ActualLogProtocol >= 1)
     {
       AddOptions(GetGlobalOptions());
     }

source/windows/ConsoleRunner.cpp
@@ -657,12 +657,12 @@ void __fastcall TExternalConsole::SendEvent(int Timeout)
 {
   SetEvent(FRequestEvent);
   unsigned int Start = 0; // shut up
-  if (Configuration->LogProtocol >= 1)
+  if (Configuration->ActualLogProtocol >= 1)
   {
     Start = GetTickCount();
   }
   unsigned int Result = WaitForSingleObject(FResponseEvent, Timeout);
-  if (Configuration->LogProtocol >= 1)
+  if (Configuration->ActualLogProtocol >= 1)
   {
     unsigned int End = GetTickCount();
     unsigned int Duration = End - Start;
@@ -2202,7 +2202,7 @@ int __fastcall TConsoleRunner::Run(const UnicodeString Session, TOptions * Optio
     {
       UnicodeString ExitCodeMessage = FORMAT(L"Exit code: %d", (ExitCode));
       FScript->Log(llMessage, ExitCodeMessage);
-      if (Configuration->LogProtocol >= 1)
+      if (Configuration->ActualLogProtocol >= 1)
       {
         FConsole->Print(ExitCodeMessage + L"\n");
         UnicodeString LogMessage = FConsole->FinalLogMessage();

commit 7f7427a23643b66c4e572748e490fd05432eba0f

Bug fix: With debug logging enabled, the echoed script commands and XML log group names miss the command name
Caused by 069aa20f

Source commit: 2692655a4bf1261b907b346c3c8d0179c70c0bd0

source/core/Script.cpp
@@ -507,10 +507,11 @@ void __fastcall TScript::Command(UnicodeString Cmd)
 
         if (Configuration->ActualLogProtocol >= 1)
         {
+          UnicodeString LogCmdParams = LogCmd;
           UnicodeString DummyLogCmd;
-          if (DebugAlwaysTrue(CutToken(LogCmd, DummyLogCmd)))
+          if (DebugAlwaysTrue(CutToken(LogCmdParams, DummyLogCmd)))
           {
-            std::unique_ptr<TScriptProcParams> Parameters(new TScriptProcParams(FCommands->ResolveCommand(Cmd), LogCmd));
+            std::unique_ptr<TScriptProcParams> Parameters(new TScriptProcParams(FCommands->ResolveCommand(Cmd), LogCmdParams));
             Parameters->LogOptions(LogOption);
           }
         }