winscp/winscp

commit 9ffd61b6df871127b45e424f0602da7cece858c7

Bug fix: Potential failure when opening unencrypted HTTP/WebDAV connection
Caused by Bug 2034

Source commit: b93f34c4e00ed58793e788815e55ddf0b07707f9

source/core/NeonIntf.cpp
@@ -110,15 +110,6 @@ void InitNeonSession(ne_session * Session, TProxyMethod ProxyMethod, const Unico
 
   ne_redirect_register(Session);
   ne_set_useragent(Session, StrToNeon(FORMAT(L"%s/%s", (AppNameString(), Configuration->Version))));
-  UnicodeString CertificateStorage = Configuration->CertificateStorageExpanded;
-  if (!CertificateStorage.IsEmpty())
-  {
-    ne_ssl_set_certificates_storage(Session, StrToNeon(CertificateStorage));
-    if (Terminal != NULL)
-    {
-      Terminal->LogEvent(FORMAT(L"Using certificate store \"%s\"", (CertificateStorage)));
-    }
-  }
 
   if (Terminal != NULL)
   {
@@ -260,9 +251,31 @@ void ne_init_ssl_session(struct ssl_st * Ssl, ne_session * Session)
 //---------------------------------------------------------------------------
 void SetNeonTlsInit(ne_session * Session, TNeonTlsInit OnNeonTlsInit)
 {
+  // As the OnNeonTlsInit always only calls SetupSsl, we can simplify this with one shared implementation
   TMethod & Method = *(TMethod*)&OnNeonTlsInit;
   ne_set_session_private(Session, SESSION_TLS_INIT_KEY, Method.Code);
   ne_set_session_private(Session, SESSION_TLS_INIT_DATA_KEY, Method.Data);
+}
+//---------------------------------------------------------------------------
+void InitNeonTls(
+  ne_session * Session, TNeonTlsInit OnNeonTlsInit, ne_ssl_verify_fn VerifyCallback, void * VerifyContext,
+  TTerminal * Terminal)
+{
+  UnicodeString CertificateStorage = Configuration->CertificateStorageExpanded;
+  if (!CertificateStorage.IsEmpty())
+  {
+    ne_ssl_set_certificates_storage(Session, StrToNeon(CertificateStorage));
+    if (Terminal != NULL)
+    {
+      Terminal->LogEvent(FORMAT(L"Using certificate store \"%s\"", (CertificateStorage)));
+    }
+  }
+
+  SetNeonTlsInit(Session, OnNeonTlsInit);
+
+  ne_ssl_set_verify(Session, VerifyCallback, VerifyContext);
+
+  ne_ssl_trust_default_ca(Session);
 }
 //---------------------------------------------------------------------------
 AnsiString NeonExportCertificate(const ne_ssl_certificate * Certificate)

source/core/NeonIntf.h
@@ -38,6 +38,9 @@ UnicodeString GetNeonRedirectUrl(ne_session * Session);
 void CheckRedirectLoop(const UnicodeString & RedirectUrl, TStrings * AttemptedUrls);
 typedef void (__closure* TNeonTlsInit)(struct ssl_st * Ssl, ne_session * Session);
 void SetNeonTlsInit(ne_session * Session, TNeonTlsInit OnNeonTlsInit);
+void InitNeonTls(
+  ne_session * Session, TNeonTlsInit OnNeonTlsInit, ne_ssl_verify_fn VerifyCallback, void * VerifyContext,
+  TTerminal * Terminal);
 AnsiString NeonExportCertificate(const ne_ssl_certificate * Certificate);
 bool NeonWindowsValidateCertificate(int & Failures, const AnsiString & AsciiCert, UnicodeString & Error);
 bool NeonWindowsValidateCertificateWithMessage(TNeonCertificateData & Data, UnicodeString & Message);

source/core/Http.cpp
@@ -73,11 +73,7 @@ void THttp::SendRequest(const char * Method, const UnicodeString & Request)
 
       if (IsTls)
       {
-        SetNeonTlsInit(NeonSession, InitSslSession);
-
-        ne_ssl_set_verify(NeonSession, NeonServerSSLCallback, this);
-
-        ne_ssl_trust_default_ca(NeonSession);
+        InitNeonTls(NeonSession, InitSslSession, NeonServerSSLCallback, this, NULL);
       }
 
       ne_request_s * NeonRequest = ne_request_create(NeonSession, Method, StrToNeon(Uri));

source/core/WebDAVFileSystem.cpp
@@ -306,15 +306,8 @@ void TWebDAVFileSystem::NeonClientOpenSessionInternal(UnicodeString & CorrectedU
 //---------------------------------------------------------------------------
 void __fastcall TWebDAVFileSystem::SetSessionTls(TSessionContext * SessionContext, ne_session_s * Session, bool Aux)
 {
-  SetNeonTlsInit(Session, InitSslSession);
-
-  // When the CA certificate or server certificate has
-  // verification problems, neon will call our verify function before
-  // outright rejection of the connection.
   ne_ssl_verify_fn Callback = Aux ? NeonServerSSLCallbackAux : NeonServerSSLCallbackMain;
-  ne_ssl_set_verify(Session, Callback, SessionContext);
-
-  ne_ssl_trust_default_ca(Session);
+  InitNeonTls(Session, InitSslSession, Callback, SessionContext, FTerminal);
 }
 //---------------------------------------------------------------------------
 void __fastcall TWebDAVFileSystem::InitSession(TSessionContext * SessionContext, ne_session_s * Session)

commit b1c505cadba984acaea066c55050667b1dce8a52

Custom certificate store stopped working for S3
Caused by 9ffd61b6

Source commit: d77e91b0ce88205b0a075ea04c52ec88217a4e56

source/core/NeonIntf.cpp
@@ -249,17 +249,7 @@ void ne_init_ssl_session(struct ssl_st * Ssl, ne_session * Session)
 
 } // extern "C"
 //---------------------------------------------------------------------------
-void SetNeonTlsInit(ne_session * Session, TNeonTlsInit OnNeonTlsInit)
-{
-  // As the OnNeonTlsInit always only calls SetupSsl, we can simplify this with one shared implementation
-  TMethod & Method = *(TMethod*)&OnNeonTlsInit;
-  ne_set_session_private(Session, SESSION_TLS_INIT_KEY, Method.Code);
-  ne_set_session_private(Session, SESSION_TLS_INIT_DATA_KEY, Method.Data);
-}
-//---------------------------------------------------------------------------
-void InitNeonTls(
-  ne_session * Session, TNeonTlsInit OnNeonTlsInit, ne_ssl_verify_fn VerifyCallback, void * VerifyContext,
-  TTerminal * Terminal)
+void SetNeonTlsInit(ne_session * Session, TNeonTlsInit OnNeonTlsInit, TTerminal * Terminal)
 {
   UnicodeString CertificateStorage = Configuration->CertificateStorageExpanded;
   if (!CertificateStorage.IsEmpty())
@@ -271,7 +261,17 @@ void InitNeonTls(
     }
   }
 
-  SetNeonTlsInit(Session, OnNeonTlsInit);
+  // As the OnNeonTlsInit always only calls SetupSsl, we can simplify this with one shared implementation
+  TMethod & Method = *(TMethod*)&OnNeonTlsInit;
+  ne_set_session_private(Session, SESSION_TLS_INIT_KEY, Method.Code);
+  ne_set_session_private(Session, SESSION_TLS_INIT_DATA_KEY, Method.Data);
+}
+//---------------------------------------------------------------------------
+void InitNeonTls(
+  ne_session * Session, TNeonTlsInit OnNeonTlsInit, ne_ssl_verify_fn VerifyCallback, void * VerifyContext,
+  TTerminal * Terminal)
+{
+  SetNeonTlsInit(Session, OnNeonTlsInit, Terminal);
 
   ne_ssl_set_verify(Session, VerifyCallback, VerifyContext);
 

source/core/NeonIntf.h
@@ -37,7 +37,7 @@ void CheckNeonStatus(ne_session * Session, int NeonStatus,
 UnicodeString GetNeonRedirectUrl(ne_session * Session);
 void CheckRedirectLoop(const UnicodeString & RedirectUrl, TStrings * AttemptedUrls);
 typedef void (__closure* TNeonTlsInit)(struct ssl_st * Ssl, ne_session * Session);
-void SetNeonTlsInit(ne_session * Session, TNeonTlsInit OnNeonTlsInit);
+void SetNeonTlsInit(ne_session * Session, TNeonTlsInit OnNeonTlsInit, TTerminal * Terminal);
 void InitNeonTls(
   ne_session * Session, TNeonTlsInit OnNeonTlsInit, ne_ssl_verify_fn VerifyCallback, void * VerifyContext,
   TTerminal * Terminal);