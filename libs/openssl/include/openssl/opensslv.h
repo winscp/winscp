e07b103582d23479d703dba98f54056740a6a6d4
Martin Prikryl <martin@winscp.net>
2023-10-25 14:01:29 +0000

    Bug fix: OpenSSL version in About dialog was not up to date
    Source commit: 37ff9e442083010aef4f272f4d0486b4675a81e5

--- libs/openssl/include/openssl/opensslv.h
+++ libs/openssl/include/openssl/opensslv.h
@@ -29,7 +29,7 @@ extern "C" {
  */
 # define OPENSSL_VERSION_MAJOR  3
 # define OPENSSL_VERSION_MINOR  1
-# define OPENSSL_VERSION_PATCH  0
+# define OPENSSL_VERSION_PATCH  4
 
 /*
  * Additional version information
@@ -74,21 +74,21 @@ extern "C" {
  * longer variant with OPENSSL_VERSION_PRE_RELEASE_STR and
  * OPENSSL_VERSION_BUILD_METADATA_STR appended.
  */
-# define OPENSSL_VERSION_STR "3.1.0"
-# define OPENSSL_FULL_VERSION_STR "3.1.0"
+# define OPENSSL_VERSION_STR "3.1.4"
+# define OPENSSL_FULL_VERSION_STR "3.1.4"
 
 /*
  * SECTION 3: ADDITIONAL METADATA
  *
  * These strings are defined separately to allow them to be parsable.
  */
-# define OPENSSL_RELEASE_DATE "14 Mar 2023"
+# define OPENSSL_RELEASE_DATE "24 Oct 2023"
 
 /*
  * SECTION 4: BACKWARD COMPATIBILITY
  */
 
-# define OPENSSL_VERSION_TEXT "OpenSSL 3.1.0 14 Mar 2023"
+# define OPENSSL_VERSION_TEXT "OpenSSL 3.1.4 24 Oct 2023"
 
 /* Synthesize OPENSSL_VERSION_NUMBER with the layout 0xMNN00PPSL */
 # ifdef OPENSSL_VERSION_PRE_RELEASE
