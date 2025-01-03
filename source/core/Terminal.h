4cbc82066163b2a29f76f0421c24ba4127536d3a
Martin Prikryl <martin@winscp.net>
2025-01-03 14:30:18 +0000

    Issue 2335 – Tunneled session password is not remembered
    https://winscp.net/tracker/2335
    (cherry picked from commit ff0a744b58cc7e248ecb7708880153d68b9e67d6)
    
    Source commit: 9131b259c2e14f8de479c42a5a92b63d2a6a0bc1

--- source/core/Terminal.h
+++ source/core/Terminal.h
@@ -161,6 +161,7 @@ friend class TParallelOperation;
 
 private:
   TSessionData * FSessionData;
+  UnicodeString FPasswordEncryptionKey;
   TSessionLog * FLog;
   TActionLog * FActionLog;
   bool FActionLogOwned;
