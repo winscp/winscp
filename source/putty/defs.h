cfd8d4fb6b8d27fe953dfa8ff26d49b70fea7cb6
Martin Prikryl <martin@winscp.net>
2025-02-08 19:40:30 +0000

    PuTTY 0.83
    Source commit: b0090a95261e1444074c54571fefe51629b4204b

--- source/putty/defs.h
+++ source/putty/defs.h
@@ -53,6 +53,11 @@ uintmax_t strtoumax(const char *nptr, char **endptr, int base);
 #define SIZEu "zu"
 #endif
 
+#if !HAVE_WMEMCHR
+/* Work around lack of wmemchr in older MSVC */
+wchar_t *wmemchr(const wchar_t *s, wchar_t c, size_t n);
+#endif
+
 #if defined __GNUC__ || defined __clang__
 /*
  * On MinGW, the correct compiler format checking for vsnprintf() etc
