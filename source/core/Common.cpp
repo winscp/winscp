0d64cc12784d79a0aa213ef3c1ded17a37821057
Martin Prikryl <martin@winscp.net>
2025-02-28 13:24:45 +0000

    Issue 2352 – Remember empty session password
    https://winscp.net/tracker/2352
    
    Source commit: 8deb7a84540894f0ce1a0b3c0ff08ebffdaa44df

--- source/core/Common.cpp
+++ source/core/Common.cpp
@@ -89,6 +89,7 @@ void __fastcall DoShred(T & Str)
 {
   if (!Str.IsEmpty())
   {
+    // Should instead test for (StringRefCount(Str) == 1) to prevent Unique making yet another copy
     Str.Unique();
     memset(Str.c_str(), 0, Str.Length() * sizeof(*Str.c_str()));
     Str = L"";
