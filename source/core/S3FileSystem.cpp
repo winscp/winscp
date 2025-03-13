winscp/winscp

commit 4019b56ffb425dcb17cc521cfcee7c2bea27ffaf

Code errors identified by Clang
Notably:
Bug fix: Monospace font is not remembered if SSH banners are administratively forced
Change: Bug fix: In XML log, filename tag was included in stat tag, although explicitly documented not to

Source commit: 693ecd9b083f1460dc4d1ae1ff49e625989f8a50

source/core/S3FileSystem.cpp
@@ -1858,7 +1858,8 @@ static _di_IXMLNode S3NeedNode(const _di_IXMLNodeList & NodeList, const UnicodeS
 //---------------------------------------------------------------------------
 #define COPY_BUCKET_CONTEXT(BucketContext) \
   { BucketContext.hostName, BucketContext.bucketName, BucketContext.protocol, BucketContext.uriStyle, \
-    BucketContext.accessKeyId, BucketContext.secretAccessKey, BucketContext.securityToken, BucketContext.authRegion }
+    BucketContext.accessKeyId, BucketContext.secretAccessKey, BucketContext.securityToken, BucketContext.authRegion, \
+    BucketContext.service }
 //---------------------------------------------------------------------------
 bool TS3FileSystem::DoLoadFileProperties(
   const UnicodeString & AFileName, const TRemoteFile * File, TS3FileProperties & Properties, bool LoadTags)
@@ -2155,7 +2156,7 @@ void __fastcall TS3FileSystem::LoadFileProperties(const UnicodeString AFileName,
   Data.Result = DoLoadFileProperties(AFileName, File, Properties, Data.LoadTags);
   if (Data.Result)
   {
-    bool AdditionalRights;
+    bool AdditionalRights = false;
     unsigned short Permissions = 0;
     for (int Index = 0; Index < Properties.AclGrantCount; Index++)
     {

source/core/Common.cpp
@@ -34,7 +34,7 @@ const UnicodeString AnyMask = L"*.*";
 const wchar_t EngShortMonthNames[12][4] =
   {L"Jan", L"Feb", L"Mar", L"Apr", L"May", L"Jun",
    L"Jul", L"Aug", L"Sep", L"Oct", L"Nov", L"Dec"};
-const char Bom[3] = "\xEF\xBB\xBF";
+const char Bom[4] = "\xEF\xBB\xBF";
 const UnicodeString XmlDeclaration(TraceInitStr(L"<?xml version=\"1.0\" encoding=\"UTF-8\"?>"));
 const wchar_t TokenPrefix = L'%';
 const wchar_t NoReplacement = wchar_t(false);

source/core/Common.h
@@ -23,7 +23,7 @@
 //---------------------------------------------------------------------------
 extern const UnicodeString AnyMask;
 extern const wchar_t EngShortMonthNames[12][4];
-extern const char Bom[3];
+extern const char Bom[4];
 extern const UnicodeString XmlDeclaration;
 extern const wchar_t TokenPrefix;
 extern const wchar_t NoReplacement;

source/core/FileBuffer.cpp
@@ -105,7 +105,7 @@ void __fastcall TFileBuffer::Convert(char * Source, char * Dest, int Params,
   DebugAssert(strlen(Dest) <= 2);
 
   if (FLAGSET(Params, cpRemoveBOM) && (Size >= 3) &&
-      (memcmp(Data, Bom, sizeof(Bom)) == 0))
+      (memcmp(Data, Bom, strlen(Bom)) == 0))
   {
     Delete(0, 3);
   }

source/core/FtpFileSystem.cpp
@@ -2314,7 +2314,7 @@ void __fastcall TFTPFileSystem::AutoDetectTimeDifference(
       // do we need FTimeDifference for the operation?
       // (tmAutomatic - AsciiFileMask can theoretically include time constraints, while it is unlikely)
       (!FLAGSET(Params, cpNoConfirmation) ||
-       CopyParam->NewerOnly || (!CopyParam->TransferMode == tmAutomatic) || !CopyParam->IncludeFileMask.Masks.IsEmpty()))
+       CopyParam->NewerOnly || (CopyParam->TransferMode == tmAutomatic) || !CopyParam->IncludeFileMask.Masks.IsEmpty()))
   {
     FTerminal->LogEvent(L"Retrieving listing to detect timezone difference");
     DummyReadDirectory(Directory);

source/core/HierarchicalStorage.cpp
@@ -69,9 +69,9 @@ UnicodeString __fastcall UnMungeStr(const UnicodeString & Str)
   RawByteString Dest(sb->s);
   strbuf_free(sb);
   UnicodeString Result;
-  if (Dest.SubString(1, LENOF(Bom)) == Bom)
+  if (Dest.SubString(1, strlen(Bom)) == Bom)
   {
-    Dest.Delete(1, LENOF(Bom));
+    Dest.Delete(1, strlen(Bom));
     Result = UTF8ToString(Dest);
   }
   else

source/core/ScpFileSystem.cpp
@@ -2616,8 +2616,8 @@ void __fastcall TSCPFileSystem::SCPSink(const UnicodeString TargetDir,
         if (CopyParam->SkipTransfer(FullFileName, Dir))
         {
           SkipConfirmed = true;
-          SCPError(L"", false);
           OperationProgress->AddSkippedFileSize(MaskParams.Size);
+          SCPError(L"", false);
         }
 
         FTerminal->LogFileDetails(FileName, FileData.Modification, MaskParams.Size);

source/core/Script.cpp
@@ -2752,6 +2752,7 @@ void __fastcall TManagementScript::Connect(const UnicodeString Session,
       {
         Data = Data->Clone();
       }
+      DefaultsOnly = false;
     }
     else
     {

commit 51cdde2dc03e6d5fb1c8278e294805ded5d2656b

S3 was broken by 4019b56f
Source commit: dd76e818563976018d01d53340225fd6fde6efd7

source/core/S3FileSystem.cpp
@@ -1103,6 +1103,7 @@ TLibS3BucketContext TS3FileSystem::GetBucketContext(const UnicodeString & Bucket
       Result.AuthRegionBuf.SetLength(S3_MAX_REGION_LENGTH);
     }
     Result.authRegion = Result.AuthRegionBuf.c_str();
+    Result.service = NULL;
 
     if (Retry)
     {