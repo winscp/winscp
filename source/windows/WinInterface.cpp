3c621f0b0f26cf447e7a5b21698ea63f9c5dd592
Martin Prikryl <martin@winscp.net>
2025-05-21 14:18:49 +0000

    Buttons are dark only when system theme is dark, so pick drop down arrow color accordingly
    Source commit: 5abb3e6c9a33a25f3451a41693b65be877a1cf2e

--- source/windows/WinInterface.cpp
+++ source/windows/WinInterface.cpp
@@ -1120,7 +1120,8 @@ static void __fastcall MenuButtonRescale(TComponent * Sender, TObject * /*Token*
 void __fastcall MenuButton(TButton * Button)
 {
   SetMenuButtonImages(Button);
-  Button->ImageIndex = UseDarkModeForControl(Button) ? 2 : 0;
+  // buttons are dark only when system theme is dark
+  Button->ImageIndex = (GetSysDarkTheme() && UseDarkModeForControl(Button)) ? 2 : 0;
   Button->DisabledImageIndex = 1;
   Button->ImageAlignment = iaRight;
   SetRescaleFunction(Button, MenuButtonRescale);
