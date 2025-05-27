winscp/winscp

commit 9485cb3f7d5ad4b4df19c65e00c869e4d5a3f514

Switching to Segoe UI font with slightly larger size (9) as recommended for Windows Vista and  newer + All windows are now slightly larger accordingly and their layout was unified
Reverts e55f27da
Fixing some anchors and tab order on the way
Dialogs (and controls with specific width or height) are now about 10% wider

Note that main form already used the Segoe UI:9 by explicitly using UseDesktopFont for most/all of its controls

Layout rules
- follow IDE design guidelines in general
- controls start at 8x8, but at 9x9 in group boxes and 22v if group has title
- right and bottom padding in groupbox is the same as left padding
-for checkboxes the bottom padding of the checkmark is the same as left padding = 10
- buttons are 80x25 by default, with +14v with dropdown
- checkboxes/radiobuttons are +2h
- labels are 15 high, textboxes/comboxes 23
- indentation is controls under checkboxes is +16h

Source commit: 09a558e92af838e142d3624cb433cc052782881c

source/forms/Authenticate.dfm
@@ -7,33 +7,29 @@ object AuthenticateForm: TAuthenticateForm
   BorderStyle = bsDialog
   Caption = 'AuthenticateForm'
   ClientHeight = 380
-  ClientWidth = 375
+  ClientWidth = 416
   Color = clBtnFace
   Constraints.MinHeight = 200
   Constraints.MinWidth = 280
-  Font.Charset = DEFAULT_CHARSET
-  Font.Color = clWindowText
-  Font.Height = -11
-  Font.Name = 'Tahoma'
-  Font.Style = []
+  ParentFont = True
   Position = poOwnerFormCenter
   OnAfterMonitorDpiChanged = FormAfterMonitorDpiChanged
   OnResize = FormResize
   OnShow = FormShow
-  TextHeight = 13
+  TextHeight = 15
   object TopPanel: TPanel
     Left = 0
     Top = 0
-    Width = 375
-    Height = 65
+    Width = 416
+    Height = 63
     Align = alClient
     BevelOuter = bvNone
     TabOrder = 0
     object LogView: TListBox
       Left = 48
       Top = 0
-      Width = 327
-      Height = 65
+      Width = 368
+      Height = 63
       Style = lbOwnerDrawVariable
       Align = alClient
       BevelInner = bvNone
@@ -52,7 +48,7 @@ object AuthenticateForm: TAuthenticateForm
       Left = 0
       Top = 0
       Width = 48
-      Height = 65
+      Height = 63
       Align = alLeft
       BevelOuter = bvNone
       Color = clWindow
@@ -68,9 +64,9 @@ object AuthenticateForm: TAuthenticateForm
   end
   object PasswordPanel: TPanel
     Left = 0
-    Top = 65
-    Width = 375
-    Height = 233
+    Top = 63
+    Width = 416
+    Height = 235
     Align = alBottom
     AutoSize = True
     BevelOuter = bvNone
@@ -79,18 +75,18 @@ object AuthenticateForm: TAuthenticateForm
     object PromptEditPanel: TPanel
       Left = 0
       Top = 0
-      Width = 375
-      Height = 139
+      Width = 416
+      Height = 144
       Align = alTop
       BevelOuter = bvNone
       TabOrder = 0
       DesignSize = (
-        375
-        139)
+        416
+        144)
       object InstructionsLabel: TLabel
         Left = 8
-        Top = 8
-        Width = 360
+        Top = 6
+        Width = 400
         Height = 39
         Anchors = [akLeft, akTop, akRight]
         AutoSize = False
@@ -104,8 +100,8 @@ object AuthenticateForm: TAuthenticateForm
       object PromptLabel1: TLabel
         Left = 8
         Top = 56
-        Width = 360
-        Height = 13
+        Width = 401
+        Height = 15
         Anchors = [akLeft, akTop, akRight]
         AutoSize = False
         Caption = '&UsernameX:'
@@ -114,9 +110,9 @@ object AuthenticateForm: TAuthenticateForm
       end
       object PromptLabel2: TLabel
         Left = 8
-        Top = 101
-        Width = 360
-        Height = 13
+        Top = 103
+        Width = 401
+        Height = 15
         Anchors = [akLeft, akTop, akRight]
         AutoSize = False
         Caption = '&PasswordX:'
@@ -125,18 +121,18 @@ object AuthenticateForm: TAuthenticateForm
       end
       object PromptEdit1: TPasswordEdit
         Left = 8
-        Top = 73
-        Width = 361
-        Height = 21
+        Top = 74
+        Width = 400
+        Height = 23
         Anchors = [akLeft, akTop, akRight]
         MaxLength = 250
         TabOrder = 0
       end
       object PromptEdit2: TPasswordEdit
         Left = 8
-        Top = 118
-        Width = 361
-        Height = 21
+        Top = 121
+        Width = 400
+        Height = 23
         Anchors = [akLeft, akTop, akRight]
         MaxLength = 250
         TabOrder = 1
@@ -144,8 +140,8 @@ object AuthenticateForm: TAuthenticateForm
     end
     object SavePasswordPanel: TPanel
       Left = 0
-      Top = 164
-      Width = 375
+      Top = 169
+      Width = 416
       Height = 25
       Align = alTop
       BevelOuter = bvNone
@@ -154,7 +150,7 @@ object AuthenticateForm: TAuthenticateForm
         Left = 14
         Top = 6
         Width = 275
-        Height = 17
+        Height = 19
         Caption = '&Change stored password to this one'
         Checked = True
         State = cbChecked
@@ -163,19 +159,19 @@ object AuthenticateForm: TAuthenticateForm
     end
     object ButtonsPanel: TPanel
       Left = 0
-      Top = 189
-      Width = 375
-      Height = 44
+      Top = 194
+      Width = 416
+      Height = 41
       Align = alTop
       BevelOuter = bvNone
       TabOrder = 3
       DesignSize = (
-        375
-        44)
+        416
+        41)
       object PasswordOKButton: TButton
-        Left = 118
-        Top = 8
-        Width = 75
+        Left = 154
+        Top = 6
+        Width = 80
         Height = 25
         Anchors = [akTop, akRight]
         Caption = 'OK'
@@ -183,9 +179,9 @@ object AuthenticateForm: TAuthenticateForm
         TabOrder = 0
       end
       object PasswordCancelButton: TButton
-        Left = 206
+        Left = 242
         Top = 8
-        Width = 75
+        Width = 80
         Height = 25
         Anchors = [akTop, akRight]
         Caption = 'Cancel'
@@ -193,9 +189,9 @@ object AuthenticateForm: TAuthenticateForm
         TabOrder = 1
       end
       object PasswordHelpButton: TButton
-        Left = 294
+        Left = 328
         Top = 8
-        Width = 75
+        Width = 80
         Height = 25
         Anchors = [akTop, akRight]
         Caption = '&Help'
@@ -205,8 +201,8 @@ object AuthenticateForm: TAuthenticateForm
     end
     object SessionRememberPasswordPanel: TPanel
       Left = 0
-      Top = 139
-      Width = 375
+      Top = 144
+      Width = 416
       Height = 25
       Align = alTop
       BevelOuter = bvNone
@@ -215,7 +211,7 @@ object AuthenticateForm: TAuthenticateForm
         Left = 14
         Top = 6
         Width = 275
-        Height = 17
+        Height = 19
         Caption = '&Remember password for this session'
         Checked = True
         State = cbChecked
@@ -226,20 +222,20 @@ object AuthenticateForm: TAuthenticateForm
   object BannerPanel: TPanel
     Left = 0
     Top = 298
-    Width = 375
+    Width = 416
     Height = 82
     Align = alBottom
     BevelOuter = bvNone
     TabOrder = 2
     Visible = False
     DesignSize = (
-      375
+      416
       82)
     object BannerMemo: TMemo
       Left = 8
       Top = 8
-      Width = 360
-      Height = 34
+      Width = 400
+      Height = 35
       Anchors = [akLeft, akTop, akRight, akBottom]
       Color = clBtnFace
       PopupMenu = BannerPopupMenu
@@ -250,18 +246,18 @@ object AuthenticateForm: TAuthenticateForm
       OnContextPopup = BannerMemoContextPopup
     end
     object NeverShowAgainCheck: TCheckBox
-      Left = 15
-      Top = 53
-      Width = 188
+      Left = 14
+      Top = 55
+      Width = 229
       Height = 17
       Anchors = [akLeft, akRight, akBottom]
       Caption = '&Never show this banner again'
       TabOrder = 1
     end
     object BannerCloseButton: TButton
-      Left = 206
-      Top = 47
-      Width = 75
+      Left = 243
+      Top = 49
+      Width = 80
       Height = 25
       Anchors = [akRight, akBottom]
       Caption = 'Continue'
@@ -269,9 +265,9 @@ object AuthenticateForm: TAuthenticateForm
       TabOrder = 2
     end
     object BannerHelpButton: TButton
-      Left = 292
-      Top = 47
-      Width = 75
+      Left = 328
+      Top = 49
+      Width = 80
       Height = 25
       Anchors = [akRight, akBottom]
       Caption = '&Help'

source/WinSCP.cpp
@@ -55,10 +55,6 @@ WINAPI wWinMain(HINSTANCE, HINSTANCE, LPWSTR, int)
     Application->MainFormOnTaskBar = true;
     Application->ModalPopupMode = pmAuto;
     DebugAssert(SameFont(Application->DefaultFont, std::unique_ptr<TFont>(new TFont()).get()));
-    Application->DefaultFont->Name = L"Tahoma";
-    // The default DefaultFont is calculated as -MulDiv(8, ScreenLogPixels, 72)
-    // But form's font is scaled using DPI, what can resumt in slightly different Height
-    Application->DefaultFont->Height = -MulDiv(11, Screen->PixelsPerInch, Screen->DefaultPixelsPerInch);
     SetEnvironmentVariable(L"WINSCP_PATH",
       ExcludeTrailingBackslash(ExtractFilePath(Application->ExeName)).c_str());
     CoreInitialize();

source/forms/About.dfm
@@ -6,26 +6,22 @@ object AboutDialog: TAboutDialog
   BorderIcons = [biSystemMenu, biMinimize, biMaximize, biHelp]
   BorderStyle = bsDialog
   Caption = 'About application'
-  ClientHeight = 526
-  ClientWidth = 444
+  ClientHeight = 532
+  ClientWidth = 455
   Color = clBtnFace
-  Font.Charset = DEFAULT_CHARSET
-  Font.Color = clWindowText
-  Font.Height = -12
-  Font.Name = 'Segoe UI'
-  Font.Style = []
+  ParentFont = True
   KeyPreview = True
   Position = poOwnerFormCenter
   OnAfterMonitorDpiChanged = FormAfterMonitorDpiChanged
   OnKeyDown = FormKeyDown
   DesignSize = (
-    444
-    526)
+    455
+    532)
   TextHeight = 15
   object OKButton: TButton
-    Left = 277
-    Top = 493
-    Width = 75
+    Left = 281
+    Top = 499
+    Width = 80
     Height = 25
     Anchors = [akRight, akBottom]
     Cancel = True
@@ -37,8 +33,8 @@ object AboutDialog: TAboutDialog
   end
   object LicenseButton: TButton
     Left = 62
-    Top = 493
-    Width = 75
+    Top = 499
+    Width = 80
     Height = 25
     Anchors = [akLeft, akBottom]
     Caption = '&License...'
@@ -46,9 +42,9 @@ object AboutDialog: TAboutDialog
     OnClick = LicenseButtonClick
   end
   object HelpButton: TButton
-    Left = 359
-    Top = 493
-    Width = 75
+    Left = 367
+    Top = 499
+    Width = 80
     Height = 25
     Anchors = [akRight, akBottom]
     Caption = '&Help'
@@ -58,16 +54,16 @@ object AboutDialog: TAboutDialog
   object Panel: TPanel
     Left = 0
     Top = 0
-    Width = 444
-    Height = 485
+    Width = 455
+    Height = 491
     Anchors = [akLeft, akTop, akRight, akBottom]
     BevelOuter = bvNone
     Color = clWindow
     ParentBackground = False
     TabOrder = 3
     DesignSize = (
-      444
-      485)
+      455
+      491)
     object ApplicationLabel: TLabel
       Left = 62
       Top = 12
@@ -78,7 +74,7 @@ object AboutDialog: TAboutDialog
     end
     object VersionLabel: TLabel
       Left = 62
-      Top = 28
+      Top = 30
       Width = 135
       Height = 15
       Caption = 'Version 2.0.0 (Build 12) XX'
@@ -86,7 +82,7 @@ object AboutDialog: TAboutDialog
     end
     object WinSCPCopyrightLabel: TLabel
       Left = 62
-      Top = 56
+      Top = 58
       Width = 197
       Height = 15
       Caption = 'Copyright '#169' 2000-2003 Martin Prikryl'
@@ -94,15 +90,15 @@ object AboutDialog: TAboutDialog
     end
     object ProductSpecificMessageLabel: TLabel
       Left = 62
-      Top = 100
-      Width = 304
+      Top = 104
+      Width = 305
       Height = 15
       Caption = 'To send comments and report bugs use support forum at:'
       ShowAccelChar = False
     end
     object Label3: TLabel
       Left = 62
-      Top = 259
+      Top = 265
       Width = 101
       Height = 15
       Caption = 'Portions copyright:'
@@ -110,8 +106,8 @@ object AboutDialog: TAboutDialog
     end
     object RegistrationLabel: TLabel
       Left = 62
-      Top = 144
-      Width = 140
+      Top = 150
+      Width = 141
       Height = 15
       Caption = 'This product is licensed to:'
       ShowAccelChar = False
@@ -125,7 +121,7 @@ object AboutDialog: TAboutDialog
     end
     object HomepageLabel: TStaticText
       Left = 62
-      Top = 72
+      Top = 76
       Width = 143
       Height = 19
       Caption = 'http://XXXXXXwinscp.net/'
@@ -134,7 +130,7 @@ object AboutDialog: TAboutDialog
     end
     object ForumUrlLabel: TStaticText
       Left = 62
-      Top = 116
+      Top = 122
       Width = 167
       Height = 19
       Caption = 'http://XXXXwinscp.net/forum/'
@@ -142,9 +138,9 @@ object AboutDialog: TAboutDialog
       TabStop = True
     end
     object ThirdPartyPanel: TPanel
-      Left = 62
-      Top = 277
-      Width = 372
+      Left = 64
+      Top = 283
+      Width = 383
       Height = 193
       Anchors = [akLeft, akTop, akRight]
       BevelKind = bkTile
@@ -154,8 +150,8 @@ object AboutDialog: TAboutDialog
     end
     object RegistrationBox: TPanel
       Left = 62
-      Top = 162
-      Width = 372
+      Top = 168
+      Width = 385
       Height = 89
       Anchors = [akLeft, akTop, akRight]
       BevelKind = bkTile
@@ -164,12 +160,12 @@ object AboutDialog: TAboutDialog
       ParentColor = True
       TabOrder = 3
       DesignSize = (
-        368
+        381
         85)
       object RegistrationSubjectLabel: TLabel
         Left = 8
         Top = 8
-        Width = 275
+        Width = 288
         Height = 65
         Anchors = [akLeft, akTop, akRight]
         AutoSize = False
@@ -188,7 +184,7 @@ object AboutDialog: TAboutDialog
       object RegistrationProductIdLabel: TStaticText
         Left = 8
         Top = 65
-        Width = 154
+        Width = 141
         Height = 19
         Caption = 'Product ID: xxxx-xxxx-xxxxx'
         ShowAccelChar = False

source/forms/Cleanup.dfm
@@ -6,25 +6,21 @@ object CleanupDialog: TCleanupDialog
   BorderIcons = [biSystemMenu, biMinimize, biMaximize, biHelp]
   BorderStyle = bsDialog
   Caption = 'Cleanup application data'
-  ClientHeight = 299
+  ClientHeight = 323
   ClientWidth = 489
   Color = clBtnFace
-  Font.Charset = DEFAULT_CHARSET
-  Font.Color = clWindowText
-  Font.Height = -11
-  Font.Name = 'Tahoma'
-  Font.Style = []
+  ParentFont = True
   Position = poOwnerFormCenter
   OnShow = FormShow
   DesignSize = (
     489
-    299)
-  TextHeight = 13
+    323)
+  TextHeight = 15
   object Label1: TLabel
     Left = 8
     Top = 8
-    Width = 475
-    Height = 97
+    Width = 473
+    Height = 108
     Anchors = [akLeft, akTop, akRight]
     AutoSize = False
     Caption = 
@@ -37,9 +33,9 @@ object CleanupDialog: TCleanupDialog
     WordWrap = True
   end
   object OKButton: TButton
-    Left = 249
-    Top = 268
-    Width = 75
+    Left = 231
+    Top = 290
+    Width = 80
     Height = 25
     Anchors = [akRight, akBottom]
     Caption = 'OK'
@@ -48,9 +44,9 @@ object CleanupDialog: TCleanupDialog
     TabOrder = 2
   end
   object CancelButton: TButton
-    Left = 329
-    Top = 268
-    Width = 75
+    Left = 317
+    Top = 290
+    Width = 80
     Height = 25
     Anchors = [akRight, akBottom]
     Cancel = True
@@ -60,9 +56,9 @@ object CleanupDialog: TCleanupDialog
   end
   object DataListView: TListView
     Left = 8
-    Top = 104
-    Width = 475
-    Height = 156
+    Top = 117
+    Width = 473
+    Height = 167
     Anchors = [akLeft, akTop, akRight, akBottom]
     Checkboxes = True
     Columns = <
@@ -90,8 +86,8 @@ object CleanupDialog: TCleanupDialog
   end
   object CheckAllButton: TButton
     Left = 8
-    Top = 266
-    Width = 113
+    Top = 290
+    Width = 120
     Height = 25
     Anchors = [akLeft, akBottom]
     Caption = 'Un/check &all'
@@ -99,9 +95,9 @@ object CleanupDialog: TCleanupDialog
     OnClick = CheckAllButtonClick
   end
   object HelpButton: TButton
-    Left = 409
-    Top = 268
-    Width = 75
+    Left = 401
+    Top = 290
+    Width = 80
     Height = 25
     Anchors = [akRight, akBottom]
     Caption = '&Help'

source/forms/Console.dfm
@@ -10,11 +10,7 @@ object ConsoleDialog: TConsoleDialog
   Color = clBtnFace
   Constraints.MinHeight = 250
   Constraints.MinWidth = 420
-  Font.Charset = DEFAULT_CHARSET
-  Font.Color = clWindowText
-  Font.Height = -11
-  Font.Name = 'Tahoma'
-  Font.Style = []
+  ParentFont = True
   Icon.Data = {
     000001000700404000000100200028420000760000003030000001002000A825
     00009E4200002828000001002000681A0000466800002020000001002000A810
@@ -1365,28 +1361,28 @@ object ConsoleDialog: TConsoleDialog
   DesignSize = (
     551
     393)
-  TextHeight = 13
+  TextHeight = 15
   object Bevel1: TBevel
     Left = 0
     Top = 0
     Width = 551
-    Height = 78
+    Height = 80
     Align = alTop
     Shape = bsBottomLine
   end
   object Label1: TLabel
     Left = 51
-    Top = 13
-    Width = 78
-    Height = 13
+    Top = 11
+    Width = 88
+    Height = 15
     Caption = 'Enter &command:'
     FocusControl = CommandEdit
   end
   object Label2: TLabel
     Left = 51
     Top = 56
-    Width = 87
-    Height = 13
+    Width = 93
+    Height = 15
     Caption = 'Current directory:'
     ShowAccelChar = False
   end
@@ -1401,10 +1397,10 @@ object ConsoleDialog: TConsoleDialog
     ShowAccelChar = False
   end
   object DirectoryLabel: TPathLabel
-    Left = 158
+    Left = 163
     Top = 56
-    Width = 299
-    Height = 13
+    Width = 298
+    Height = 15
     UnixPath = True
     IndentHorizontal = 0
     IndentVertical = 0
@@ -1421,9 +1417,9 @@ object ConsoleDialog: TConsoleDialog
   end
   object OutputMemo: TMemo
     Left = 0
-    Top = 78
+    Top = 80
     Width = 551
-    Height = 315
+    Height = 313
     TabStop = False
     Align = alClient
     Color = clBtnFace
@@ -1435,9 +1431,9 @@ object ConsoleDialog: TConsoleDialog
     OnContextPopup = OutputMemoContextPopup
   end
   object CancelBtn: TButton
-    Left = 472
+    Left = 463
     Top = 7
-    Width = 75
+    Width = 80
     Height = 25
     Anchors = [akTop, akRight]
     Cancel = True
@@ -1446,10 +1442,10 @@ object ConsoleDialog: TConsoleDialog
     TabOrder = 2
   end
   object CommandEdit: THistoryComboBox
-    Left = 158
-    Top = 9
-    Width = 213
-    Height = 21
+    Left = 163
+    Top = 8
+    Width = 208
+    Height = 23
     AutoComplete = False
     Anchors = [akLeft, akTop, akRight]
     MaxLength = 2048
@@ -1457,9 +1453,9 @@ object ConsoleDialog: TConsoleDialog
     OnChange = CommandEditChange
   end
   object ExecuteButton: TButton
-    Left = 384
+    Left = 377
     Top = 7
-    Width = 75
+    Width = 80
     Height = 25
     Anchors = [akTop, akRight]
     Caption = '&Execute'
@@ -1468,9 +1464,9 @@ object ConsoleDialog: TConsoleDialog
     OnClick = ExecuteButtonClick
   end
   object HelpButton: TButton
-    Left = 472
+    Left = 463
     Top = 42
-    Width = 75
+    Width = 80
     Height = 25
     Anchors = [akTop, akRight]
     Caption = '&Help'

source/forms/Copy.dfm
@@ -6,22 +6,18 @@ object CopyDialog: TCopyDialog
   BorderIcons = [biSystemMenu, biMinimize, biMaximize, biHelp]
   BorderStyle = bsDialog
   Caption = 'CopyDialog'
-  ClientHeight = 225
-  ClientWidth = 511
+  ClientHeight = 235
+  ClientWidth = 567
   Color = clBtnFace
-  Font.Charset = DEFAULT_CHARSET
-  Font.Color = clWindowText
-  Font.Height = -11
-  Font.Name = 'Tahoma'
-  Font.Style = []
+  ParentFont = True
   Position = poOwnerFormCenter
   OnAfterMonitorDpiChanged = FormAfterMonitorDpiChanged
   OnCloseQuery = FormCloseQuery
   OnShow = FormShow
   DesignSize = (
-    511
-    225)
-  TextHeight = 13
+    567
+    235)
+  TextHeight = 15
   object Image: TImage
     Left = 8
     Top = 11
@@ -32,15 +28,15 @@ object CopyDialog: TCopyDialog
   object DirectoryLabel: TLabel
     Left = 46
     Top = 8
-    Width = 195
-    Height = 13
+    Width = 212
+    Height = 15
     Caption = 'Copy 2 selected files to remote directory'
   end
   object LocalDirectoryEdit: THistoryComboBox
     Left = 46
-    Top = 25
-    Width = 372
-    Height = 21
+    Top = 26
+    Width = 427
+    Height = 23
     AutoComplete = False
     Anchors = [akLeft, akTop, akRight]
     DropDownCount = 16
@@ -51,9 +47,9 @@ object CopyDialog: TCopyDialog
   end
   object RemoteDirectoryEdit: THistoryComboBox
     Left = 46
-    Top = 25
-    Width = 456
-    Height = 21
+    Top = 26
+    Width = 513
+    Height = 23
     AutoComplete = False
     Anchors = [akLeft, akTop, akRight]
     DropDownCount = 16
@@ -63,9 +59,9 @@ object CopyDialog: TCopyDialog
     OnChange = ControlChange
   end
   object OkButton: TButton
-    Left = 260
-    Top = 136
-    Width = 75
+    Left = 307
+    Top = 143
+    Width = 80
     Height = 25
     Anchors = [akTop, akRight]
     Caption = 'OK'
@@ -75,9 +71,9 @@ object CopyDialog: TCopyDialog
     OnDropDownClick = OkButtonDropDownClick
   end
   object CancelButton: TButton
-    Left = 343
-    Top = 136
-    Width = 75
+    Left = 393
+    Top = 143
+    Width = 80
     Height = 25
     Anchors = [akTop, akRight]
     Cancel = True
@@ -86,17 +82,18 @@ object CopyDialog: TCopyDialog
     TabOrder = 7
   end
   object LocalDirectoryBrowseButton: TButton
-    Left = 427
-    Top = 23
-    Width = 75
+    Left = 479
+    Top = 25
+    Width = 80
     Height = 25
+    Anchors = [akTop, akRight]
     Caption = 'B&rowse...'
     TabOrder = 1
     OnClick = LocalDirectoryBrowseButtonClick
   end
   object QueueCheck2: TCheckBox
-    Left = 12
-    Top = 112
+    Left = 10
+    Top = 120
     Width = 317
     Height = 17
     Caption = 'Transfer on background (add to transfer &queue) X'
@@ -104,9 +101,9 @@ object CopyDialog: TCopyDialog
     OnClick = ControlChange
   end
   object HelpButton: TButton
-    Left = 427
-    Top = 136
-    Width = 75
+    Left = 479
+    Top = 143
+    Width = 80
     Height = 25
     Anchors = [akTop, akRight]
     Caption = '&Help'
@@ -114,8 +111,8 @@ object CopyDialog: TCopyDialog
     OnClick = HelpButtonClick
   end
   object NeverShowAgainCheck: TCheckBox
-    Left = 12
-    Top = 167
+    Left = 10
+    Top = 174
     Width = 253
     Height = 17
     Caption = '&Do not show this dialog box again'
@@ -124,8 +121,8 @@ object CopyDialog: TCopyDialog
   end
   object TransferSettingsButton: TButton
     Left = 8
-    Top = 136
-    Width = 161
+    Top = 143
+    Width = 175
     Height = 25
     Caption = 'Transfer settin&gs...'
     Style = bsSplitButton
@@ -135,21 +132,22 @@ object CopyDialog: TCopyDialog
   end
   object CopyParamGroup: TGroupBox
     Left = 8
-    Top = 53
-    Width = 496
-    Height = 50
+    Top = 55
+    Width = 551
+    Height = 59
+    Anchors = [akLeft, akTop, akRight]
     Caption = 'Transfer settings'
     TabOrder = 3
     OnClick = CopyParamGroupClick
     OnContextPopup = CopyParamGroupContextPopup
     DesignSize = (
-      496
-      50)
+      551
+      59)
     object CopyParamLabel: TLabel
-      Left = 7
-      Top = 15
-      Width = 482
-      Height = 26
+      Left = 9
+      Top = 20
+      Width = 533
+      Height = 35
       Anchors = [akLeft, akTop, akRight, akBottom]
       AutoSize = False
       Caption = 'CopyParamLabel'
@@ -160,18 +158,22 @@ object CopyDialog: TCopyDialog
   end
   object ShortCutHintPanel: TPanel
     Left = 0
-    Top = 191
-    Width = 511
-    Height = 34
+    Top = 197
+    Width = 567
+    Height = 38
     Align = alBottom
     BevelOuter = bvNone
     ParentBackground = False
     TabOrder = 9
+    DesignSize = (
+      567
+      38)
     object ShortCutHintLabel: TLabel
-      Left = 12
+      Left = 8
       Top = 3
-      Width = 490
-      Height = 28
+      Width = 551
+      Height = 32
+      Anchors = [akLeft, akTop, akRight, akBottom]
       AutoSize = False
       Caption = 
         'In Commander interface the keyboard shortcut F5 is used to trans' +

source/forms/CopyLocal.dfm
@@ -6,22 +6,18 @@ object CopyLocalDialog: TCopyLocalDialog
   BorderIcons = [biSystemMenu, biMinimize, biMaximize, biHelp]
   BorderStyle = bsDialog
   Caption = 'CopyLocalDialog'
-  ClientHeight = 121
+  ClientHeight = 126
   ClientWidth = 511
   Color = clBtnFace
-  Font.Charset = DEFAULT_CHARSET
-  Font.Color = clWindowText
-  Font.Height = -11
-  Font.Name = 'Tahoma'
-  Font.Style = []
+  ParentFont = True
   Position = poOwnerFormCenter
   OnAfterMonitorDpiChanged = FormAfterMonitorDpiChanged
   OnCloseQuery = FormCloseQuery
   OnShow = FormShow
   DesignSize = (
     511
-    121)
-  TextHeight = 13
+    126)
+  TextHeight = 15
   object Image: TImage
     Left = 8
     Top = 11
@@ -32,16 +28,16 @@ object CopyLocalDialog: TCopyLocalDialog
   object DirectoryLabel: TLabel
     Left = 46
     Top = 8
-    Width = 61
-    Height = 13
+    Width = 63
+    Height = 15
     Caption = '&Target path:'
     FocusControl = DirectoryEdit
   end
   object DirectoryEdit: THistoryComboBox
     Left = 46
-    Top = 25
-    Width = 372
-    Height = 21
+    Top = 26
+    Width = 371
+    Height = 23
     AutoComplete = False
     Anchors = [akLeft, akTop, akRight]
     DropDownCount = 16
@@ -50,9 +46,9 @@ object CopyLocalDialog: TCopyLocalDialog
     OnExit = DirectoryEditExit
   end
   object OkButton: TButton
-    Left = 260
+    Left = 251
     Top = 55
-    Width = 75
+    Width = 80
     Height = 25
     Anchors = [akTop, akRight]
     Caption = 'OK'
@@ -61,9 +57,9 @@ object CopyLocalDialog: TCopyLocalDialog
     TabOrder = 3
   end
   object CancelButton: TButton
-    Left = 343
+    Left = 337
     Top = 55
-    Width = 75
+    Width = 80
     Height = 25
     Anchors = [akTop, akRight]
     Cancel = True
@@ -72,18 +68,18 @@ object CopyLocalDialog: TCopyLocalDialog
     TabOrder = 4
   end
   object LocalDirectoryBrowseButton: TButton
-    Left = 427
-    Top = 23
-    Width = 75
+    Left = 423
+    Top = 25
+    Width = 80
     Height = 25
     Caption = 'B&rowse...'
     TabOrder = 1
     OnClick = LocalDirectoryBrowseButtonClick
   end
   object HelpButton: TButton
-    Left = 427
+    Left = 423
     Top = 55
-    Width = 75
+    Width = 80
     Height = 25
     Anchors = [akTop, akRight]
     Caption = '&Help'
@@ -91,27 +87,31 @@ object CopyLocalDialog: TCopyLocalDialog
     OnClick = HelpButtonClick
   end
   object NeverShowAgainCheck: TCheckBox
-    Left = 12
-    Top = 58
-    Width = 242
+    Left = 10
+    Top = 59
+    Width = 237
     Height = 17
     Caption = '&Do not show this dialog box again'
     TabOrder = 2
   end
   object ShortCutHintPanel: TPanel
     Left = 0
-    Top = 87
+    Top = 88
     Width = 511
-    Height = 34
+    Height = 38
     Align = alBottom
     BevelOuter = bvNone
     ParentBackground = False
     TabOrder = 6
+    DesignSize = (
+      511
+      38)
     object ShortCutHintLabel: TLabel
-      Left = 12
+      Left = 8
       Top = 3
-      Width = 490
-      Height = 28
+      Width = 495
+      Height = 32
+      Anchors = [akLeft, akTop, akRight, akBottom]
       AutoSize = False
       Caption = 
         'In Commander interface the keyboard shortcut F5 is used to trans' +

source/forms/CopyParamCustom.dfm
@@ -6,24 +6,20 @@ object CopyParamCustomDialog: TCopyParamCustomDialog
   BorderIcons = [biSystemMenu, biMinimize, biMaximize, biHelp]
   BorderStyle = bsDialog
   Caption = 'Transfer settings'
-  ClientHeight = 515
-  ClientWidth = 420
+  ClientHeight = 511
+  ClientWidth = 466
   Color = clBtnFace
-  Font.Charset = DEFAULT_CHARSET
-  Font.Color = clWindowText
-  Font.Height = -11
-  Font.Name = 'Tahoma'
-  Font.Style = []
+  ParentFont = True
   Position = poOwnerFormCenter
   OnCloseQuery = FormCloseQuery
   DesignSize = (
-    420
-    515)
-  TextHeight = 13
+    466
+    511)
+  TextHeight = 15
   object OkButton: TButton
-    Left = 168
-    Top = 482
-    Width = 75
+    Left = 206
+    Top = 478
+    Width = 80
     Height = 25
     Anchors = [akRight, akBottom]
     Caption = 'OK'
@@ -32,9 +28,9 @@ object CopyParamCustomDialog: TCopyParamCustomDialog
     TabOrder = 1
   end
   object CancelButton: TButton
-    Left = 252
-    Top = 482
-    Width = 75
+    Left = 292
+    Top = 478
+    Width = 80
     Height = 25
     Anchors = [akRight, akBottom]
     Cancel = True
@@ -43,17 +39,17 @@ object CopyParamCustomDialog: TCopyParamCustomDialog
     TabOrder = 2
   end
   inline CopyParamsFrame: TCopyParamsFrame
-    Left = 0
-    Top = 0
-    Width = 420
-    Height = 477
+    Left = 5
+    Top = 5
+    Width = 456
+    Height = 471
     HelpType = htKeyword
     TabOrder = 0
   end
   object HelpButton: TButton
-    Left = 336
-    Top = 482
-    Width = 75
+    Left = 378
+    Top = 478
+    Width = 80
     Height = 25
     Anchors = [akRight, akBottom]
     Caption = 'Help'

commit 0b6147e59905f33c2400cdcdf90e642ffc4daa6b

Fixing button alignment broken by 9485cb3f
Source commit: 63412acba9f9c86398451b3eb8120414d6acf58e

source/forms/Authenticate.dfm
@@ -175,8 +175,8 @@ object AuthenticateForm: TAuthenticateForm
         416
         41)
       object PasswordOKButton: TButton
-        Left = 154
-        Top = 6
+        Left = 156
+        Top = 8
         Width = 80
         Height = 25
         Anchors = [akTop, akRight]