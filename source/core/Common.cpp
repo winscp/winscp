9fefb2536c9ed5434a468bfcf097d2fd57b51c0c
Martin Prikryl <martin@winscp.net>
2025-07-06 21:04:19 +0000

    Bug fix: Failure when reading empty certificate authority configuration
    Source commit: 7afc6c48816470716314023fc3598213799564ac

--- source/core/Common.cpp
+++ source/core/Common.cpp
@@ -510,9 +510,14 @@ UnicodeString EncodeStrToBase64(const RawByteString & Str)
 RawByteString DecodeBase64ToStr(const UnicodeString & Str)
 {
   TBytes Bytes = DecodeBase64(Str);
-  // This might be the same as TEncoding::ASCII->GetString.
-  // const_cast: The operator[] const is (badly?) implemented to return by value
-  return RawByteString(reinterpret_cast<const char *>(&const_cast<TBytes &>(Bytes)[0]), Bytes.Length);
+  RawByteString Result;
+  if (Bytes.Length > 0)
+  {
+    // This might be the same as TEncoding::ASCII->GetString.
+    // const_cast: The operator[] const is (badly?) implemented to return by value
+    Result = RawByteString(reinterpret_cast<const char *>(&const_cast<TBytes &>(Bytes)[0]), Bytes.Length);
+  }
+  return Result;
 }
 //---------------------------------------------------------------------------
 UnicodeString Base64ToUrlSafe(const UnicodeString & S)
