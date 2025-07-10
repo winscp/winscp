93ca4d8eeae1be6c1e7d68f6c8f8da81209820f2
Martin Prikryl <martin@winscp.net>
2025-07-10 10:54:42 +0000

    Issue 2385 – Incorrect scaling of navigation tree on Login and Preferences dialogs
    https://winscp.net/tracker/2385
    
    Scaling of anchored TTreeView in VCL is somehow broken. Placing the tree as client-aligned on anchored panel fixes the problem.
    
    (cherry picked from commit c0b24b7703f5925c2f2786877a9eeb73ac0b639c)
    
    # Conflicts:
    #	source/forms/Preferences.h
    
    Source commit: c72f0fcdaa301e5db7108c7ecf0f51885c79d6af

--- source/forms/Preferences.h
+++ source/forms/Preferences.h
@@ -359,6 +359,7 @@ __published:
   TComboEdit *SearchEdit;
   TTabSheet *SearchSheet;
   TGroupBox *SearchGroup;
+  TPanel *NavigationPanel;
   void __fastcall FormShow(TObject *Sender);
   void __fastcall ControlChange(TObject *Sender);
   void __fastcall EditorFontButtonClick(TObject *Sender);
