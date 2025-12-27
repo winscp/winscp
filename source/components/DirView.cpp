winscp/winscp

commit c562d3d4c4c0b104e7a529df4278d632e67024a9

Getting rid of TDictionary as it crashes 64-bit linker
https://winscp.net/tracker/1016

Source commit: 02d79c8672c60c572c9b722a93dea8b5030ea1fe

source/components/DirView.cpp
@@ -2,6 +2,7 @@
 #include <vcl.h>
 #pragma hdrstop
 
+#include <Common.h>
 #include "DirView.h"
 #include "IEDriveInfo.h"
 //---------------------------------------------------------------------------
@@ -11,4 +12,108 @@ __fastcall TDirView::TDirView(TComponent * AOwner) :
   TDirViewInt(AOwner)
 {
   DriveInfoRequire();
+}
+//---------------------------------------------------------------------------
+void __fastcall TDirView::IconUpdateClear()
+{
+  FIconUpdateQueue = TIconUpdateQueue();
+  FIconUpdateQueueDeferred = TIconUpdateQueue();
+  FIconUpdateSet.clear();
+}
+//---------------------------------------------------------------------------
+void __fastcall TDirView::IconUpdateEnqueue(TListItem * ListItem)
+{
+  if (FIconUpdateSet.find(ListItem->Index) == FIconUpdateSet.end())
+  {
+    TIconUpdateSchedule Schedule { .Index = ListItem->Index };
+    FIconUpdateQueue.push(Schedule);
+    DebugAssert(FIconUpdateSet.size() == FIconUpdateQueue.size() + FIconUpdateQueueDeferred.size());
+  }
+  StartIconUpdateThread();
+}
+//---------------------------------------------------------------------------
+int __fastcall TDirView::IconUpdatePeek()
+{
+  int Result;
+  if (!FIconUpdateQueue.empty())
+  {
+    Result = FIconUpdateQueue.front().Index;
+  }
+  else if (!FIconUpdateQueueDeferred.empty())
+  {
+    Result = FIconUpdateQueueDeferred.front().Index;
+  }
+  else
+  {
+    Result = -1;
+  }
+  return Result;
+}
+//---------------------------------------------------------------------------
+bool __fastcall TDirView::IconUpdateDeprioritize(Dirviewint::TFileRec * ItemData, int Index)
+{
+  bool Result = !FIconUpdateQueue.empty();
+  if (Result)
+  {
+    DebugAssert(!ThumbnailNeeded(ItemData));
+    if (ItemData->IconEmpty)
+    {
+      TIconUpdateSchedule Schedule = FIconUpdateQueue.front();
+      FIconUpdateQueue.pop();
+      FIconUpdateQueueDeferred.push(Schedule);
+    }
+    else
+    {
+      IconUpdateDequeue(Index);
+    }
+  }
+  return Result;
+}
+//---------------------------------------------------------------------------
+void __fastcall TDirView::IconUpdateDequeue(int Index)
+{
+  if (!FIconUpdateQueue.empty())
+  {
+    FIconUpdateQueue.pop();
+  }
+  else
+  {
+    FIconUpdateQueueDeferred.pop();
+  }
+  FIconUpdateSet.erase(Index);
+  DebugAssert(FIconUpdateSet.size() == FIconUpdateQueue.size() + FIconUpdateQueueDeferred.size());
+}
+//---------------------------------------------------------------------------
+void __fastcall TDirView::PathChanged()
+{
+  TDirViewInt::PathChanged();
+  // make sure to use PathName as Path maybe just X: what
+  // ExpandFileName resolves to current working directory
+  // on the drive, not to root path
+  UnicodeString Expanded = ExpandFileName(PathName);
+  FLastPaths[DriveInfo->GetDriveKey(Expanded)] = Expanded;
+}
+//---------------------------------------------------------------------------
+bool __fastcall TDirView::TryGetLastPath(UnicodeString Drive, UnicodeString & Path)
+{
+  auto I = FLastPaths.find(Drive);
+  bool Result = (I != FLastPaths.end());
+  if (Result)
+  {
+    Path = I->second;
+    #ifndef DESIGN_ONLY
+    if (!DirectoryExists(ApiPath(Path)))
+    {
+      if (DriveInfo->IsRealDrive(Drive))
+      {
+        Path = FORMAT(L"%s:", (Drive));
+      }
+      else
+      {
+        Path = Drive;
+      }
+    }
+    #endif
+  }
+  return Result;
 }

source/DScpComp.cbproj
@@ -68,7 +68,7 @@
         <RunBCCOutOfProcess>true</RunBCCOutOfProcess>
         <SanitizedProjectName>DScpComp</SanitizedProjectName>
         <SubProcessesNumber>8</SubProcessesNumber>
-        <TLIB_PageSize>64</TLIB_PageSize>
+        <TLIB_PageSize>128</TLIB_PageSize>
         <VerInfo_Keys>CompanyName=;FileDescription=;FileVersion=1.0.0.0;InternalName=;LegalCopyright=;LegalTrademarks=;OriginalFilename=;ProductName=;ProductVersion=1.0.0.0;Comments=</VerInfo_Keys>
         <VerInfo_Locale>1033</VerInfo_Locale>
     </PropertyGroup>

source/components/DirView.h
@@ -3,11 +3,30 @@
 #define DirViewH
 //---------------------------------------------------------------------------
 #include "DirViewInt.hpp"
+#include <set>
+#include <queue>
+#include <map>
 //---------------------------------------------------------------------------
 class TDirView : public TDirViewInt
 {
 public:
   __fastcall TDirView(TComponent * AOwner);
+
+protected:
+  virtual void __fastcall IconUpdateClear();
+  virtual void __fastcall IconUpdateEnqueue(TListItem * ListItem);
+  virtual int __fastcall IconUpdatePeek();
+  virtual bool __fastcall IconUpdateDeprioritize(Dirviewint::TFileRec * ItemData, int Index);
+  virtual void __fastcall IconUpdateDequeue(int Index);
+  virtual void __fastcall PathChanged();
+  virtual bool __fastcall TryGetLastPath(UnicodeString Drive, UnicodeString & Path);
+
+private:
+  std::set<int> FIconUpdateSet;
+  typedef std::queue<TIconUpdateSchedule> TIconUpdateQueue;
+  TIconUpdateQueue FIconUpdateQueue;
+  TIconUpdateQueue FIconUpdateQueueDeferred;
+  std::map<UnicodeString, UnicodeString> FLastPaths;
 };
 //---------------------------------------------------------------------------
 #endif

source/components/DriveView.cpp
@@ -2,6 +2,7 @@
 #include <vcl.h>
 #pragma hdrstop
 
+#include <Common.h>
 #include "DriveView.h"
 #include "IEDriveInfo.h"
 //---------------------------------------------------------------------------
@@ -11,4 +12,47 @@ __fastcall TDriveView::TDriveView(TComponent * AOwner) :
   TDriveViewInt(AOwner)
 {
   DriveInfoRequire();
+
+  FDriveStatus.reset(new TStringList());
+  FDriveStatus->Sorted = true;
+  for (TRealDrive Drive = FirstDrive; Drive <= LastDrive; Drive++)
+  {
+    FDriveStatus->AddObject(Drive, CreateDriveStatus());
+  }
+}
+//---------------------------------------------------------------------------
+TDriveStatus * __fastcall TDriveView::GetDriveStatus(UnicodeString Drive)
+{
+  int Index = FDriveStatus->IndexOf(Drive);
+  TDriveStatus * Result;
+  if (Index < 0)
+  {
+    Result = CreateDriveStatus();
+    FDriveStatus->AddObject(Drive, Result);
+  }
+  else
+  {
+    Result = GetDriveStatus(Index);
+  }
+  return Result;
+}
+//---------------------------------------------------------------------------
+TDriveStatus * TDriveView::GetDriveStatus(int Index)
+{
+  return DebugNotNull(dynamic_cast<TDriveStatus *>(FDriveStatus->Objects[Index]));
+}
+//---------------------------------------------------------------------------
+bool __fastcall TDriveView::GetNextDriveStatus(int & Iterator, UnicodeString * Drive, TDriveStatus *& Status)
+{
+  bool Result = (Iterator < FDriveStatus->Count);
+  if (Result)
+  {
+    if (Drive != nullptr)
+    {
+      *Drive = FDriveStatus->Strings[Iterator];
+    }
+    Status = GetDriveStatus(Iterator);
+    ++Iterator;
+  }
+  return Result;
 }

source/components/DriveView.h
@@ -8,6 +8,18 @@ class TDriveView : public TDriveViewInt
 {
 public:
   __fastcall TDriveView(TComponent * AOwner);
+
+  virtual TDriveStatus * __fastcall GetDriveStatus(UnicodeString Drive);
+
+protected:
+  // Using TStringList as it's easy to iterate with int iterator for GetNextDriveStatus.
+  // Once all code is moved here, we can replace it easily with std::map.
+  std::unique_ptr<TStringList> FDriveStatus;
+
+  virtual bool __fastcall GetNextDriveStatus(int & Iterator, UnicodeString * Drive, TDriveStatus *& Status);
+
+private:
+  TDriveStatus * GetDriveStatus(int Index);
 };
 //---------------------------------------------------------------------------
 #endif

source/components/IEDriveInfo.cpp
@@ -2,7 +2,9 @@
 #include <vcl.h>
 #pragma hdrstop
 
+#include <Common.h>
 #include "IEDriveInfo.h"
+#include "PasTools.hpp"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 //---------------------------------------------------------------------------
@@ -17,4 +19,67 @@ void DriveInfoRequire()
 __fastcall TDriveInfo::TDriveInfo() :
   TDriveInfoInt()
 {
+}
+//---------------------------------------------------------------------------
+TDriveInfoRec * __fastcall TDriveInfo::AddDrive(UnicodeString Drive)
+{
+  TDriveInfoRec * Result = new TDriveInfoRec();
+  FData.insert(std::make_pair(Drive, Result));
+  ResetDrive(Drive);
+  if (IsFixedDrive(Drive) || !IsRealDrive(Drive)) // not floppy
+  {
+    DoReadDriveStatus(Drive, dsSynchronous);
+  }
+  else
+  {
+    ReadDriveBasicStatus(Drive);
+  }
+  return Result;
+}
+//---------------------------------------------------------------------------
+TDriveInfoRec * __fastcall TDriveInfo::GetInternal(UnicodeString Drive)
+{
+  auto I = FData.find(Drive);
+  DebugAssert(I != FData.end());
+  return I->second.get();
+}
+//---------------------------------------------------------------------------
+TDriveInfoRec * __fastcall TDriveInfo::Get(UnicodeString Drive)
+{
+  NeedData();
+
+  // We might want to wait for FReadyDrives to be empty before returning
+  // (or even better do that only in DriveReady getter)
+  TDriveInfoRec * Result;
+  auto I = FData.find(Drive);
+  if (I == FData.end())
+  {
+    DebugAssert(IsUncPath(Drive));
+    Result = AddDrive(Drive);
+    DriveRefresh();
+  }
+  else
+  {
+    Result = I->second.get();
+  }
+  return Result;
+}
+//---------------------------------------------------------------------------
+void __fastcall TDriveInfo::DriveRemoving(THandle DeviceHandle)
+{
+  for (auto I = FData.begin(); I != FData.end(); ++I)
+  {
+    if (I->second->DriveHandle == DeviceHandle)
+    {
+      TDriveInfoInt::DriveRemoving(I->first);
+    }
+  }
+}
+//---------------------------------------------------------------------------
+void __fastcall TDriveInfo::UpdateDrivesNotifications()
+{
+  for (auto I = FData.begin(); I != FData.end(); ++I)
+  {
+    UpdateDriveNotifications(I->first);
+  }
 }

source/components/IEDriveInfo.h
@@ -3,11 +3,22 @@
 #define IEDriveInfoH
 //---------------------------------------------------------------------------
 #include "IEDriveInfoInt.hpp"
+#include <map>
 //---------------------------------------------------------------------------
 class TDriveInfo : public TDriveInfoInt
 {
 public:
   __fastcall TDriveInfo();
+
+protected:
+  virtual TDriveInfoRec * __fastcall AddDrive(UnicodeString Drive);
+  virtual TDriveInfoRec * __fastcall GetInternal(UnicodeString Drive);
+  virtual TDriveInfoRec * __fastcall Get(UnicodeString Drive);
+  virtual void __fastcall DriveRemoving(THandle DeviceHandle);
+  virtual void __fastcall UpdateDrivesNotifications();
+
+private:
+  std::map<UnicodeString, std::unique_ptr<TDriveInfoRec>> FData;
 };
 //---------------------------------------------------------------------------
 void DriveInfoRequire();

source/packages/filemng/DirViewInt.pas
@@ -121,7 +121,6 @@ type
     FParentFolder: IShellFolder;
     FDesktopFolder: IShellFolder;
     FDirOK: Boolean;
-    FPath: string;
     FHiddenCount: Integer;
     FFilteredCount: Integer;
     FNotRelative: Boolean;
@@ -131,9 +130,6 @@ type
     // Additional thread components:
     FIconUpdateThread: TIconUpdateThread;
     // only ever accessed by GUI thread
-    FIconUpdateSet: TDictionary<Integer, Boolean>;
-    FIconUpdateQueue: TQueue<TIconUpdateSchedule>;
-    FIconUpdateQueueDeferred: TQueue<TIconUpdateSchedule>;
     FDiscMonitor: TDiscMonitor;
     FHomeDirectory: string;
 
@@ -143,7 +139,6 @@ type
     iRecycleFolder: iShellFolder;
     PIDLRecycle: PItemIDList;
 
-    FLastPath: TDictionary<string, string>;
     FTimeoutShellIconRetrieval: Boolean;
     FAnyCut: Boolean;
 
@@ -157,6 +152,8 @@ type
     procedure SetDirColProperties(Value: TDirViewColProperties);
 
   protected
+    FPath: string;
+
     function NewColProperties: TCustomListViewColProperties; override;
     function SortAscendingByDefault(Index: Integer): Boolean; override;
     procedure Notification(AComponent: TComponent; Operation: TOperation); override;
@@ -183,7 +180,6 @@ type
     procedure SetLoadEnabled(Value: Boolean); override;
     function GetPath: string; override;
     procedure SetPath(Value: string); override;
-    procedure PathChanged; override;
     procedure SetItemImageIndex(Item: TListItem; Index: Integer); override;
     procedure ChangeDetected(Sender: TObject; const Directory: string;
       var SubdirsChanged: Boolean);
@@ -214,9 +210,9 @@ type
     procedure DoFetchIconUpdate;
     procedure DoUpdateIcon;
     procedure StartFileDeleteThread;
-    procedure IconUpdateEnqueue(ListItem: TListItem);
-    function IconUpdatePeek: Integer;
-    procedure IconUpdateDequeue;
+    procedure IconUpdateEnqueue(ListItem: TListItem); virtual; abstract;
+    function IconUpdatePeek: Integer; virtual; abstract;
+    procedure IconUpdateDequeue(Index: Integer); virtual; abstract;
     procedure WMDestroy(var Msg: TWMDestroy); message WM_DESTROY;
     procedure Load(DoFocusSomething: Boolean); override;
     procedure DoFetchIcon(
@@ -226,6 +222,9 @@ type
       dwFileAttributes: DWORD; var psfi: TSHFileInfoW; uFlags: UINT): DWORD_PTR;
     function DoCopyToClipboard(Focused: Boolean; Cut: Boolean; Operation: TClipBoardOperation): Boolean;
     procedure ClearCutState;
+    procedure IconUpdateClear; virtual; abstract;
+    function IconUpdateDeprioritize(ItemData: PFileRec; Index: Integer): Boolean; virtual; abstract;
+    function TryGetLastPath(Drive: string; var Path: string): Boolean; virtual; abstract;
 
     function HiddenCount: Integer; override;
     function FilteredCount: Integer; override;
@@ -687,22 +686,12 @@ begin
   DragDropFilesEx.ShellExtensions.DragDropHandler := True;
   DragDropFilesEx.ShellExtensions.DropHandler := True;
 
-  FIconUpdateSet := TDictionary<Integer, Boolean>.Create;
-  FIconUpdateQueue := TQueue<TIconUpdateSchedule>.Create;
-  FIconUpdateQueueDeferred := TQueue<TIconUpdateSchedule>.Create;
-
-  FLastPath := nil;
 end;
 
 destructor TDirViewInt.Destroy;
 begin
-  FreeAndNil(FIconUpdateQueue);
-  FreeAndNil(FIconUpdateQueueDeferred);
-  FreeAndNil(FIconUpdateSet);
-
   if Assigned(PIDLRecycle) then FreePIDL(PIDLRecycle);
 
-  FLastPath.Free;
   FFileOperator.Free;
   FChangeTimer.Free;
 
@@ -751,23 +740,6 @@ end;
 function TDirViewInt.GetPath: string;
 begin
   Result := FPath;
-end;
-
-procedure TDirViewInt.PathChanged;
-var
-  Expanded: string;
-begin
-  inherited;
-
-  // make sure to use PathName as Path maybe just X: what
-  // ExpandFileName resolves to current working directory
-  // on the drive, not to root path
-  Expanded := ExpandFileName(PathName);
-  if not Assigned(FLastPath) then
-  begin
-    FLastPath := TDictionary<string, string>.Create;
-  end;
-  FLastPath.AddOrSetValue(DriveInfo.GetDriveKey(Expanded), Expanded);
 end;
 
 procedure TDirViewInt.SetPath(Value: string);
@@ -1191,9 +1163,7 @@ procedure TDirViewInt.Load(DoFocusSomething: Boolean);
 begin
   try
     StopIconUpdateThread;
-    FIconUpdateQueue.Clear;
-    FIconUpdateQueueDeferred.Clear;
-    FIconUpdateSet.Clear;
+    IconUpdateClear();
 
     StopWatchThread;
 
@@ -2372,40 +2342,6 @@ begin
   end;
 end;
 
-procedure TDirViewInt.IconUpdateEnqueue(ListItem: TListItem);
-var
-  Schedule: TIconUpdateSchedule;
-begin
-  if not FIconUpdateSet.ContainsKey(ListItem.Index) then
-  begin
-    FIconUpdateSet.Add(ListItem.Index, False);
-    Schedule.Index := ListItem.Index;
-    FIconUpdateQueue.Enqueue(Schedule);
-    Assert(FIconUpdateSet.Count = FIconUpdateQueue.Count + FIconUpdateQueueDeferred.Count);
-  end;
-  StartIconUpdateThread;
-end;
-
-function TDirViewInt.IconUpdatePeek: Integer;
-begin
-  if FIconUpdateQueue.Count > 0 then
-    Result := FIconUpdateQueue.Peek.Index
-  else if FIconUpdateQueueDeferred.Count > 0 then
-    Result := FIconUpdateQueueDeferred.Peek.Index
-  else
-    Result := -1;
-end;
-
-procedure TDirViewInt.IconUpdateDequeue;
-begin
-  if FIconUpdateQueue.Count > 0 then
-    FIconUpdateQueue.Dequeue
-  else
-    FIconUpdateQueueDeferred.Dequeue;
-  FIconUpdateSet.Remove(FIconUpdateThread.FCurrentIndex);
-  Assert(FIconUpdateSet.Count = FIconUpdateQueue.Count + FIconUpdateQueueDeferred.Count);
-end;
-
 function TDirViewInt.ThumbnailNeeded(ItemData: PFileRec): Boolean;
 begin
   Result := (not Assigned(ItemData.Thumbnail)) and (ItemData.ThumbnailSize <> ThumbnailNotNeeded);
@@ -2428,7 +2364,7 @@ begin
     if FIconUpdateThread.FCurrentIndex >= Items.Count then
     begin
       FIconUpdateThread.FCurrentIndex := -1;
-      IconUpdateDequeue;
+      IconUpdateDequeue(-1);
     end
       else
     begin
@@ -2448,18 +2384,8 @@ begin
       // Deprioritizing unvisible item taken from priority queue.
       // As we ask Window to cache the image index (LVIF_DI_SETITEM), we won't get asked again,
       // so we have to retrieve the image eventually (but not thumbnail)
-      if (FIconUpdateQueue.Count > 0) and
-         Invisible then
+      if Invisible and IconUpdateDeprioritize(ItemData, FIconUpdateThread.FCurrentIndex) then
       begin
-        Assert(not ThumbnailNeeded(ItemData));
-        if ItemData.IconEmpty then
-        begin
-          FIconUpdateQueueDeferred.Enqueue(FIconUpdateQueue.Dequeue);
-        end
-          else
-        begin
-          IconUpdateDequeue;
-        end;
         FIconUpdateThread.FCurrentIndex := -1;
       end
         else
@@ -2472,7 +2398,7 @@ begin
         end
           else
         begin
-          IconUpdateDequeue;
+          IconUpdateDequeue(FIconUpdateThread.FCurrentIndex);
           FIconUpdateThread.FCurrentIndex := -1;
         end;
       end;
@@ -2492,7 +2418,7 @@ begin
      (not FIconUpdateThread.Terminated) and
      (FIconUpdateThread.FCurrentIndex = IconUpdatePeek) then
   begin
-    IconUpdateDequeue;
+    IconUpdateDequeue(FIconUpdateThread.FCurrentIndex);
 
     if (FIconUpdateThread.FCurrentIndex >= 0) and
        (FIconUpdateThread.FCurrentIndex < Items.Count) then
@@ -2749,18 +2675,7 @@ var
   APath: string;
   DriveRoot: string;
 begin
-  if Assigned(FLastPath) and FLastPath.ContainsKey(Drive) then
-  begin
-    APath := FLastPath[Drive];
-    if not DirectoryExists(ApiPath(APath)) then
-    begin
-      if DriveInfo.IsRealDrive(Drive) then
-        APath := Format('%s:', [Drive])
-      else
-        APath := Drive;
-    end;
-  end
-    else
+  if not TryGetLastPath(Drive, APath) then
   begin
     if DriveInfo.IsRealDrive(Drive) then
     begin

commit 674c6958eca2abf6de897d501d09f9c0e46bfbd1

Fixing local thumbnail bug introduced in c562d3d
Source commit: 31a0e0f30397a79acfac9cc9e20d62c792928716

source/components/DirView.cpp
@@ -25,6 +25,7 @@ void __fastcall TDirView::IconUpdateEnqueue(TListItem * ListItem)
 {
   if (FIconUpdateSet.find(ListItem->Index) == FIconUpdateSet.end())
   {
+    FIconUpdateSet.insert(ListItem->Index);
     TIconUpdateSchedule Schedule { .Index = ListItem->Index };
     FIconUpdateQueue.push(Schedule);
     DebugAssert(FIconUpdateSet.size() == FIconUpdateQueue.size() + FIconUpdateQueueDeferred.size());