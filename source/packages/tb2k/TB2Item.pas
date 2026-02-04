winscp/winscp

commit a2119a7eac5444748df64fae0937fc49a6bf21c8

Types and casts safe in respect to pointer size for forward 64-bit compatibility
Also GetWindowLongPtr and GWLP_USERDATA

https://winscp.net/tracker/618

Source commit: c85fdf15e50ce8a6f35b1c4d2a0125a0b9c4f1f0

source/packages/tb2k/TB2Item.pas
@@ -6216,7 +6216,7 @@ begin
       end;
       case Msg.message of
         WM_LBUTTONDOWN, WM_RBUTTONDOWN: begin
-            P := SmallPointToPoint(TSmallPoint(Msg.lParam));
+            P := Point(LOWORD(Msg.lParam), HIWORD(Msg.lParam));
             Windows.ClientToScreen(Msg.hwnd, P);
             Ctl := FindDragTarget(P, True);
             { Was the mouse not clicked on a popup, or was it clicked on a
@@ -6337,7 +6337,7 @@ begin
             WM_MOUSEWHEEL:
               if GetSelectedViewer(View, Viewer) then begin
                 P := Viewer.ScreenToClient(Msg.pt);
-                Viewer.MouseWheel(Smallint(LongRec(Msg.wParam).Hi), P.X, P.Y);
+                Viewer.MouseWheel(HIWORD(Msg.wParam), P.X, P.Y);
               end;
             WM_LBUTTONDOWN, WM_LBUTTONDBLCLK, WM_RBUTTONDOWN:
               if (Msg.message <> WM_RBUTTONDOWN) or TrackRightButton then begin

commit f4e0e5809125c96bc2aec0fd73f9cc64f6ded5ae

Bug fixes: Some menus were not working on displays to the left or above the primary display + Mouse wheel downwards scrolling did not work on toolbar drop down lists
Caused by a2119a7e

Source commit: 63352780bc26375231b44b4be493a29a26048d6a

source/packages/tb2k/TB2Item.pas
@@ -6213,7 +6213,7 @@ begin
       end;
       case Msg.message of
         WM_LBUTTONDOWN, WM_RBUTTONDOWN: begin
-            P := Point(LOWORD(Msg.lParam), HIWORD(Msg.lParam));
+            P := Point(SmallInt(LOWORD(NativeUInt(Msg.lParam))), SmallInt(HIWORD(NativeUInt(Msg.lParam))));
             Windows.ClientToScreen(Msg.hwnd, P);
             Ctl := FindDragTarget(P, True);
             { Was the mouse not clicked on a popup, or was it clicked on a
@@ -6334,7 +6334,7 @@ begin
             WM_MOUSEWHEEL:
               if GetSelectedViewer(View, Viewer) then begin
                 P := Viewer.ScreenToClient(Msg.pt);
-                Viewer.MouseWheel(HIWORD(Msg.wParam), P.X, P.Y);
+                Viewer.MouseWheel(Smallint(LongRec(Msg.wParam).Hi), P.X, P.Y);
               end;
             WM_LBUTTONDOWN, WM_LBUTTONDBLCLK, WM_RBUTTONDOWN:
               if (Msg.message <> WM_RBUTTONDOWN) or TrackRightButton then begin