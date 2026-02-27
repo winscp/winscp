winscp/winscp

commit 3a887624f6ff273310ab5649162749a5191c7bae

Properly drawing background of selectable value displays on tab controls on Windows 11
(would also help on Windows XP, but we do not support that anymore)

Source commit: 23db1a3f17f5cd240650c5005fa33702ff7d7a2a

source/windows/VCLCommon.cpp
@@ -307,6 +307,45 @@ static void __fastcall ReadOnlyEditContextPopup(void * /*Data*/, TObject * Sende
   }
 }
 //---------------------------------------------------------------------------
+class TPublicWinControl : public TWinControl
+{
+friend TWndMethod __fastcall ControlWndProc(TWinControl * Control);
+friend void __fastcall InstallPathWordBreakProc(TWinControl * Control);
+};
+//---------------------------------------------------------------------------
+TWndMethod __fastcall ControlWndProc(TWinControl * Control)
+{
+  TPublicWinControl * PublicWinControl = static_cast<TPublicWinControl *>(Control);
+  return &PublicWinControl->WndProc;
+}
+//---------------------------------------------------------------------------
+static void __fastcall ReadOnlyEditWindowProc(void * Data, TMessage & Message)
+{
+  TCustomEdit * Edit = static_cast<TCustomEdit *>(Data);
+  TCustomStyleServices * AStyleServices;
+  if ((Message.Msg == CN_CTLCOLORSTATIC) && Edit->ReadOnly && (AStyleServices = StyleServices(Edit))->Enabled)
+  {
+    // VCL_COPY Based on TCustomStaticText.CNCtlColorStatic
+
+    // Pure Win32 alternative can be seen at:
+    // https://stackoverflow.com/a/75764544/850848
+    // (see my comment to the answer)
+
+    HDC ControlDC = reinterpret_cast<HDC>(Message.WParam);
+    HWND ControlHandle = reinterpret_cast<HWND>(Message.LParam);
+    DebugAssert(ControlHandle == Edit->Handle);
+
+    SetBkMode(ControlDC, TRANSPARENT);
+    AStyleServices->DrawParentBackground(ControlHandle, ControlDC, NULL, false);
+
+    Message.Result = reinterpret_cast<LRESULT>(GetStockObject(NULL_BRUSH));
+  }
+  else
+  {
+    ControlWndProc(Edit)(Message);
+  }
+}
+//---------------------------------------------------------------------------
 void __fastcall DoReadOnlyControl(TControl * Control, bool ReadOnly, bool Color)
 {
   if (dynamic_cast<TCustomEdit *>(Control) != NULL)
@@ -314,6 +353,23 @@ void __fastcall DoReadOnlyControl(TControl * Control, bool ReadOnly, bool Color)
     TEdit * Edit = static_cast<TEdit *>(Control);
     Edit->ReadOnly = ReadOnly;
     TMemo * Memo = dynamic_cast<TMemo *>(Control);
+
+    TWinControl * Parent = Edit->Parent;
+    while (Parent != NULL)
+    {
+      // Not necessary, just to limit impact and conflicts
+      if (dynamic_cast<TTabSheet *>(Parent) != NULL)
+      {
+        TWndMethod WindowProc = MakeMethod<TWndMethod>(Edit, ReadOnlyEditWindowProc);
+        if ((Edit->WindowProc != WindowProc) && DebugAlwaysTrue(Edit->WindowProc == ControlWndProc(Edit)))
+        {
+          Edit->WindowProc = WindowProc;
+        }
+        break;
+      }
+      Parent = Parent->Parent;
+    }
+
     if (ReadOnly)
     {
       if (Color)
@@ -465,18 +521,6 @@ UnicodeString __fastcall FormatFormCaption(
     Result = FormatMainFormCaption(Result, SessionName);
   }
   return Result;
-}
-//---------------------------------------------------------------------------
-class TPublicWinControl : public TWinControl
-{
-friend TWndMethod __fastcall ControlWndProc(TWinControl * Control);
-friend void __fastcall InstallPathWordBreakProc(TWinControl * Control);
-};
-//---------------------------------------------------------------------------
-TWndMethod __fastcall ControlWndProc(TWinControl * Control)
-{
-  TPublicWinControl * PublicWinControl = static_cast<TPublicWinControl *>(Control);
-  return &PublicWinControl->WndProc;
 }
 //---------------------------------------------------------------------
 class TPublicControl : public TControl

commit 4136d3e545bc97141fefbc4f78e1041cd8056ddd

Bug fix: Protocol additional information scrolling was broken
Caused by 3a887624

Implementing handy functions for managing internal components

Source commit: 701335de9b626f7f96daa34a4dc4f2dff10461f9

source/windows/GUITools.cpp
@@ -924,19 +924,13 @@ UnicodeString __fastcall UniqTempDir(const UnicodeString BaseDir, const UnicodeS
 class TSessionColors : public TComponent
 {
 public:
-  __fastcall TSessionColors(TComponent * Owner) : TComponent(Owner)
+  __fastcall TSessionColors() : TComponent(nullptr)
   {
-    Name = QualifiedClassName();
   }
 
   static TSessionColors * __fastcall Retrieve(TComponent * Component)
   {
-    TSessionColors * SessionColors = dynamic_cast<TSessionColors *>(Component->FindComponent(QualifiedClassName()));
-    if (SessionColors == NULL)
-    {
-      SessionColors = new TSessionColors(Component);
-    }
-    return SessionColors;
+    return GetComponentInstance<TSessionColors>(Component);
   }
 
   typedef std::map<TColor, int> TColorMap;

source/windows/UserInterface.cpp
@@ -855,8 +855,7 @@ __fastcall TColorChangeData::TColorChangeData(
 TColorChangeData * __fastcall TColorChangeData::Retrieve(TObject * Object)
 {
   TComponent * Component = DebugNotNull(dynamic_cast<TComponent *>(Object));
-  TComponent * ColorChangeDataComponent = Component->FindComponent(QualifiedClassName());
-  return DebugNotNull(dynamic_cast<TColorChangeData *>(ColorChangeDataComponent));
+  return FindComponentInstance<TColorChangeData>(Component);
 }
 //---------------------------------------------------------------------------
 static void SaveCustomColors(bool SessionColors, const UnicodeString & Colors)

source/windows/VCLCommon.cpp
@@ -39,15 +39,7 @@ public:
 //---------------------------------------------------------------------------
 static TFormCustomizationComponent * GetFormCustomizationComponent(TCustomForm * Form)
 {
-  TFormCustomizationComponent * FormCustomizationComponent =
-    dynamic_cast<TFormCustomizationComponent *>(Form->FindComponent(TFormCustomizationComponent::QualifiedClassName()));
-  if (FormCustomizationComponent == NULL)
-  {
-    FormCustomizationComponent = new TFormCustomizationComponent();
-    FormCustomizationComponent->Name = TFormCustomizationComponent::QualifiedClassName();
-    Form->InsertComponent(FormCustomizationComponent);
-  }
-  return FormCustomizationComponent;
+  return GetComponentInstance<TFormCustomizationComponent>(Form);
 }
 //---------------------------------------------------------------------------
 void __fastcall FixListColumnWidth(TListView * TListView, int Index)
@@ -320,26 +312,93 @@ TWndMethod __fastcall ControlWndProc(TWinControl * Control)
   return &PublicWinControl->WndProc;
 }
 //---------------------------------------------------------------------------
+class TBrushComponent : public TComponent
+{
+public:
+  __fastcall TBrushComponent() : TComponent(nullptr)
+  {
+    Brush = 0;
+  }
+
+  void ReleaseBrush()
+  {
+    if (Brush != 0)
+    {
+      DeleteObject(Brush);
+      Brush = 0;
+    }
+  }
+
+  virtual __fastcall ~TBrushComponent()
+  {
+    ReleaseBrush();
+  }
+
+  HBRUSH Brush;
+};
+//---------------------------------------------------------------------------
 static void __fastcall ReadOnlyEditWindowProc(void * Data, TMessage & Message)
 {
   TCustomEdit * Edit = static_cast<TCustomEdit *>(Data);
-  TCustomStyleServices * AStyleServices;
-  if ((Message.Msg == CN_CTLCOLORSTATIC) && Edit->ReadOnly && (AStyleServices = StyleServices(Edit))->Enabled)
+  if (((Message.Msg == WM_WINDOWPOSCHANGED) || (Message.Msg == CN_CTLCOLORSTATIC)) &&
+      Edit->ReadOnly)
   {
-    // VCL_COPY Based on TCustomStaticText.CNCtlColorStatic
-
-    // Pure Win32 alternative can be seen at:
     // https://stackoverflow.com/q/75759034/850848#75764544
     // (see my comment to the answer)
 
-    HDC ControlDC = reinterpret_cast<HDC>(Message.WParam);
-    HWND ControlHandle = reinterpret_cast<HWND>(Message.LParam);
-    DebugAssert(ControlHandle == Edit->Handle);
+    auto BrushComponent = GetComponentInstance<TBrushComponent>(Edit);
 
-    SetBkMode(ControlDC, TRANSPARENT);
-    AStyleServices->DrawParentBackground(ControlHandle, ControlDC, NULL, false);
+    if (Message.Msg == WM_WINDOWPOSCHANGED)
+    {
+      BrushComponent->ReleaseBrush();
+      ControlWndProc(Edit)(Message);
+    }
+    else
+    {
+      HWND ControlHandle = reinterpret_cast<HWND>(Message.LParam);
+      DebugAssert(ControlHandle == Edit->Handle);
 
-    Message.Result = reinterpret_cast<LRESULT>(GetStockObject(NULL_BRUSH));
+      TWinControl * Parent = Edit->Parent;
+      while (Parent != NULL)
+      {
+        if (dynamic_cast<TPageControl *>(Parent) != nullptr)
+        {
+          break;
+        }
+        Parent = Parent->Parent;
+      }
+
+      if (Parent != nullptr)
+      {
+        if (BrushComponent->Brush == 0)
+        {
+          HDC DC = GetDC(Parent->Handle);
+          HDC DCNew = CreateCompatibleDC(DC);
+          HBITMAP Bitmap = CreateCompatibleBitmap(DC, Parent->Width, Parent->Height);
+          HBITMAP BitmapOld = static_cast<HBITMAP>(SelectObject(DCNew, Bitmap));
+
+          Parent->Perform(WM_PRINTCLIENT, reinterpret_cast<WPARAM>(DCNew), static_cast<LPARAM>(PRF_ERASEBKGND | PRF_CLIENT | PRF_NONCLIENT));
+          BrushComponent->Brush = CreatePatternBrush(Bitmap);
+          SelectObject(DCNew, BitmapOld);
+
+          DeleteObject(Bitmap);
+          DeleteDC(DCNew);
+          ReleaseDC(Parent->Handle, DC);
+        }
+
+        HDC EditDC = reinterpret_cast<HDC>(Message.WParam);
+        SetBkMode(EditDC, TRANSPARENT);
+
+        TPoint P = Edit->ClientToParent(TPoint(), Parent);
+        SetBrushOrgEx(EditDC, -P.X, -P.Y, nullptr);
+
+        Message.Result = reinterpret_cast<LRESULT>(BrushComponent->Brush);
+      }
+      else
+      {
+        ControlWndProc(Edit)(Message);
+      }
+    }
   }
   else
   {
@@ -359,7 +418,6 @@ void __fastcall DoReadOnlyControl(TControl * Control, bool ReadOnly, bool Color)
     TWinControl * Parent = Edit->Parent;
     while (Parent != NULL)
     {
-      // Not necessary, just to limit impact and conflicts
       if (dynamic_cast<TTabSheet *>(Parent) != NULL)
       {
         TWndMethod WindowProc = MakeMethod<TWndMethod>(Edit, ReadOnlyEditWindowProc);
@@ -581,8 +639,7 @@ void __fastcall SetRescaleFunction(
   TComponent * Component, TRescaleEvent OnRescale, TObject * Token, bool OwnsToken)
 {
   TRescaleComponent * RescaleComponent = new TRescaleComponent(OnRescale, Token, OwnsToken);
-  RescaleComponent->Name = TRescaleComponent::QualifiedClassName();
-  Component->InsertComponent(RescaleComponent);
+  InsertComponentInstance(Component, RescaleComponent);
 }
 //---------------------------------------------------------------------------
 static void __fastcall ChangeControlScale(TControl * Control)
@@ -608,8 +665,7 @@ static void __fastcall ChangeControlScale(TControl * Control)
       ChangeControlScale(ChildControl);
     }
 
-    TRescaleComponent * RescaleComponent =
-      dynamic_cast<TRescaleComponent *>(Component->FindComponent(TRescaleComponent::QualifiedClassName()));
+    TRescaleComponent * RescaleComponent = FindComponentInstance<TRescaleComponent>(Component);
     if (RescaleComponent != NULL)
     {
       RescaleComponent->OnRescale(Component, RescaleComponent->Token);

source/windows/VCLCommon.h
@@ -108,5 +108,28 @@ void AutoSizeLabel(TLabel * Label);
 void AutoSizeLabel(TStaticText * Label);
 void SetAccessibleName(TWinControl * Control, const UnicodeString & Name);
 void SetAccessibleRole(TWinControl * Control, int Role);
+template<class ComponentType>
+ComponentType * FindComponentInstance(TComponent * Owner)
+{
+  TComponent * Component = Owner->FindComponent(ComponentType::QualifiedClassName());
+  return (Component == nullptr) ? nullptr : DebugNotNull(dynamic_cast<ComponentType *>(Component));
+}
+template<class ComponentType>
+void InsertComponentInstance(TComponent * Owner, ComponentType * Component)
+{
+  Component->Name = ComponentType::QualifiedClassName();
+  Owner->InsertComponent(Component);
+}
+template<class ComponentType>
+ComponentType * GetComponentInstance(TComponent * Owner)
+{
+  auto Component = FindComponentInstance<ComponentType>(Owner);
+  if (Component == nullptr)
+  {
+    Component = new ComponentType();
+    InsertComponentInstance(Owner, Component);
+  }
+  return Component;
+}
 //---------------------------------------------------------------------------
 #endif  // VCLCommonH