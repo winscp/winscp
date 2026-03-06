winscp/winscp

commit 1413e93add14786c2e72b79a93ee14551097afa7

Bug fix: Master password prompt was not added to task bar when opening session from commandline
Source commit: aee96444b70c678c682a592174fcef43ab2e5714

source/windows/UserInterface.cpp
@@ -1246,8 +1246,9 @@ bool __fastcall IsCustomShortCut(TShortCut ShortCut)
 class TMasterPasswordDialog : public TCustomDialog
 {
 public:
-  __fastcall TMasterPasswordDialog(bool Current);
+  __fastcall TMasterPasswordDialog(TComponent * AOwner);
 
+  void Init(bool Current);
   bool __fastcall Execute(UnicodeString & CurrentPassword, UnicodeString & NewPassword);
 
 protected:
@@ -1260,9 +1261,15 @@ private:
   TPasswordEdit * ConfirmEdit;
 };
 //---------------------------------------------------------------------------
-__fastcall TMasterPasswordDialog::TMasterPasswordDialog(bool Current) :
-  TCustomDialog(Current ? HELP_MASTER_PASSWORD_CURRENT : HELP_MASTER_PASSWORD_CHANGE)
+// Need to have an Owner argument for SafeFormCreate
+__fastcall TMasterPasswordDialog::TMasterPasswordDialog(TComponent *) :
+  TCustomDialog(EmptyStr)
 {
+}
+//---------------------------------------------------------------------------
+void TMasterPasswordDialog::Init(bool Current)
+{
+  HelpKeyword = Current ? HELP_MASTER_PASSWORD_CURRENT : HELP_MASTER_PASSWORD_CHANGE;
   Caption = LoadStr(MASTER_PASSWORD_CAPTION);
 
   CurrentEdit = new TPasswordEdit(this);
@@ -1359,9 +1366,11 @@ static bool __fastcall DoMasterPasswordDialog(bool Current,
   UnicodeString & NewPassword)
 {
   bool Result;
-  TMasterPasswordDialog * Dialog = new TMasterPasswordDialog(Current);
+  // This can be a standalone dialog when opening session from commandline
+  TMasterPasswordDialog * Dialog = SafeFormCreate<TMasterPasswordDialog>();
   try
   {
+    Dialog->Init(Current);
     UnicodeString CurrentPassword;
     Result = Dialog->Execute(CurrentPassword, NewPassword);
     if (Result)

commit 3aeab26186950b9bbb1dcbf9ae413f93a59f123c

Bug fix: Master password dialog was missing Help button
Caused by 1413e93a

Source commit: 2d66178b23d7e0c33d91023ae86d590cb9c48ae6

source/windows/UserInterface.cpp
@@ -1276,7 +1276,9 @@ private:
 //---------------------------------------------------------------------------
 // Need to have an Owner argument for SafeFormCreate
 __fastcall TMasterPasswordDialog::TMasterPasswordDialog(TComponent *) :
-  TCustomDialog(EmptyStr)
+  // HelpKeyword is provisional, and will be overridein in Init.
+  // It's provided here only to enable the Help button.
+  TCustomDialog(HELP_MASTER_PASSWORD_CURRENT)
 {
 }
 //---------------------------------------------------------------------------