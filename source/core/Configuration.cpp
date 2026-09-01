11ce2859c0f9b32af3c56c117aa18157fc0b3355
Martin Prikryl <martin@winscp.net>
2026-09-01 10:03:36 +0000

    Links update
    Source commit: a99ad766ce70165b2aa13cc27bb88f8bd4a57893

--- source/core/Configuration.cpp
+++ source/core/Configuration.cpp
@@ -14,7 +14,7 @@
 const wchar_t * AutoSwitchNames = L"On;Off;Auto";
 const wchar_t * NotAutoSwitchNames = L"Off;On;Auto";
 //---------------------------------------------------------------------------
-// See https://www.iana.org/assignments/hash-function-text-names/hash-function-text-names.xhtml
+// See https://www.iana.org/assignments/hash-function-text-names
 const UnicodeString Sha1ChecksumAlg(L"sha-1");
 const UnicodeString Sha224ChecksumAlg(L"sha-224");
 const UnicodeString Sha256ChecksumAlg(L"sha-256");
